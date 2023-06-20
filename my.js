859d85615da377ed2e3d3ba0635696a71ce45aae
Jhonny <gonzodzen@mail.ru>
2023-06-20 22:07:40 +0300

    Add Two Numbers

--- my.js
+++ my.js
@@ -1,4 +1,18 @@
-const reverse=(x,t=+[...Math.abs(x)+''].reverse().join``)=>x<0&&-t>(-2)**31?-t:x>0&&t<2**31-1?t:0
+function ListNode(val, next){
+    this.val=(val===undefined ? 0 : val)
+    this.next=(next===undefined ? null : next)
+ }
 
-console.log(reverse(123))
-console.log(reverse(-123))
+ const f=(x,a=[])=>{
+    while(x){
+        a.push(x.val)
+        x=x.next
+    }
+    return BigInt(a.reverse().join``)
+}
+const r=(x,n=null)=>{
+    return x.length===1?new ListNode(x.pop(),n):r(x,new ListNode(x.pop(),n))
+}
+const addTwoNumbers=(l1,l2)=>r([...(f(l1)+f(l2))+''].map(Number).reverse())
+
+console.log(addTwoNumbers(r([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 ,0,0,0,0,0,1]),r([5,6,4])))
