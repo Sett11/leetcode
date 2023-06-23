4dd3cdaa5bb63457d50204806bda2185d43562de
Jhonny <gonzodzen@mail.ru>
2023-06-23 15:03:02 +0300

    Contains Duplicate III

--- my.js
+++ my.js
@@ -1,1 +1,7 @@
-findMin=a=>Math.min(...a)
+containsNearbyAlmostDuplicate=(a,d,k)=>{
+    for(let i=-1;++i<a.length;)for(let j=i;++j<a.length;)if(Math.abs(i-j)<=d&&Math.abs(a[i]-a[j])<=k)return true
+    return false
+}
+
+console.log(containsNearbyAlmostDuplicate([1,2,3,1],3,0))
+console.log(containsNearbyAlmostDuplicate([1,5,9,1,5,9],2,3))
