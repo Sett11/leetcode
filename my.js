b3c0aedc463ed7e76a222f611204c03988eb71a9
Jhonny <gonzodzen@mail.ru>
2023-06-23 15:29:29 +0300

    Number of Digit One

--- my.js
+++ my.js
@@ -1,7 +1,16 @@
-containsNearbyAlmostDuplicate=(a,d,k)=>{
-    for(let i=-1;++i<a.length;)for(let j=i;++j<a.length;)if(Math.abs(i-j)<=d&&Math.abs(a[i]-a[j])<=k)return true
-    return false
+class M{
+    constructor(){
+        this.s=''
+        this.k=0
+    }
+    c(n){
+        let t=this.k+1
+        while(t<=n)this.s+=(t++ +'').replace(/[^1]/g,''),this.k=t
+        return this.s.length
+    }
 }
+const r=new M()
+countDigitOne=n=>r.c(n)
 
-console.log(containsNearbyAlmostDuplicate([1,2,3,1],3,0))
-console.log(containsNearbyAlmostDuplicate([1,5,9,1,5,9],2,3))
+console.log(countDigitOne(13))
+console.log(countDigitOne(824883294))
