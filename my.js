a66fcb1be2056ddfb07d7547c9447b22430ee41d
Jhonny <gonzodzen@mail.ru>
2023-06-23 12:50:51 +0300

    Find Minimum in Rotated Sorted Array II

--- my.js
+++ my.js
@@ -1,15 +1,1 @@
-isValid=(s,a=[0,0,0])=>{
-    for(let i=-1;++i<s.length;){
-        if(s[i]==='(')a[0]++
-        if(s[i]==='[')a[1]++
-        if(s[i]==='{')a[2]++
-        if(s[i]===')')a[0]--
-        if(s[i]===']')a[1]--
-        if(s[i]==='}')a[2]--
-        if(a.some(e=>e<0))return false
-    }
-    return a.every(e=>!e)&&!s.match(/\[\)|\[\}\]|\{\]/)&&s!=='[([]])'
-}
-
-console.log(isValid('()[]{}'))
-console.log(isValid('([)]'))
+findMin=a=>Math.min(...a)
