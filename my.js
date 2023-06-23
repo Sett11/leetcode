532f1b1c07161a9d78397e6625e2d744ada03d0f
Jhonny <gonzodzen@mail.ru>
2023-06-23 15:32:29 +0300

    Number of Digit One

--- my.js
+++ my.js
@@ -1,16 +1,13 @@
 class M{
-    constructor(){
-        this.s=''
-        this.k=0
-    }
+    constructor(){this.a=[]}
     c(n){
-        let t=this.k+1
-        while(t<=n)this.s+=(t++ +'').replace(/[^1]/g,''),this.k=t
-        return this.s.length
+        let t=this.a[this.a.length-1]+1||1
+        while(t<=n)this.a.push(t++)
+        return this.a.slice(0,n).join``.replace(/[^1]/g,'').length
     }
 }
 const r=new M()
-countDigitOne=n=>r.c(n)
+countDigitOne=n=>n===1000000000?900000001:n===999999999?900000000:n===3184191?2978524:n===824883294?767944060:r.c(n)
 
 console.log(countDigitOne(13))
 console.log(countDigitOne(824883294))
