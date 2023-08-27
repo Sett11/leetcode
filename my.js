5542d1244f999d6a4f9de3645ac4a73988674ad3
Jhonny <gonzodzen@mail.ru>
2023-08-27 17:07:11 +0300

    Two Sum II - Input Array Is Sorted

--- my.js
+++ my.js
@@ -1,21 +1,5 @@
-lengthOfLongestSubstring=s=>{
-    if(s.length===[...new Set(s)].length)return s.length
-    let a=[]
-    for(let i=-1;++i<s.length;){
-        for(let j=i;++j<=s.length+1;){
-            let t=s.slice(i,j)
-            if(t.length!==[...new Set(t)].length){
-                a.push(t.slice(0,-1))
-                break
-            }
-            if(j===s.length){
-                a.push(t.slice(0,j))
-                break
-            }
-        }
-    }
-    return !s||a.length==0?0:a.sort((a,b)=>b.length-a.length)[0].length
+twoSum=(a,k)=>{
+    for(let i=-1;++i<a.length;)for(let j=i;++j<a.length;)if(a[i]+a[j]===k)return [i+1,j+1]
 }
 
-console.log(lengthOfLongestSubstring('abcabcbb'))
-console.log(lengthOfLongestSubstring('aab'))
+console.log(twoSum([ 2 , 7 ,11,15],9))
