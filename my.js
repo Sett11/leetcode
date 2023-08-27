223d97400025808f1efedf85c2f57669aa9ff254
Jhonny <gonzodzen@mail.ru>
2023-08-27 16:10:10 +0300

    Longest Substring Without Repeating Characters

--- my.js
+++ my.js
@@ -1,5 +1,21 @@
-twoSum=(a,b)=>{
-    for(let i=-1;++i<a.length;)for(let j=-1;++j<a.length;)if(a[i]+a[j]===b&&i!==j)return [i,j]
+lengthOfLongestSubstring=s=>{
+    if(s.length===[...new Set(s)].length)return s.length
+    let a=[]
+    for(let i=-1;++i<s.length;){
+        for(let j=i;++j<=s.length+1;){
+            let t=s.slice(i,j)
+            if(t.length!==[...new Set(t)].length){
+                a.push(t.slice(0,-1))
+                break
+            }
+            if(j===s.length){
+                a.push(t.slice(0,j))
+                break
+            }
+        }
+    }
+    return !s||a.length==0?0:a.sort((a,b)=>b.length-a.length)[0].length
 }
 
-console.log(twoSum([2,7,11,15],9))
+console.log(lengthOfLongestSubstring('abcabcbb'))
+console.log(lengthOfLongestSubstring('aab'))
