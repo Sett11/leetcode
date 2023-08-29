57be1e68e842feccfd3852d47d58c94f1a02c57b
Jhonny <gonzodzen@mail.ru>
2023-08-29 13:21:11 +0300

    Two Sum

--- my.py
+++ my.py
@@ -1,18 +1,10 @@
 class Solution:
-    def applyOperations(self,a):
-        i,r=0,[]
-        while i<len(a)-1:
-            if a[i]==a[i+1] and a[i]:
-                a[i]*=2
-                a.pop(i+1)
-                r.append(0)
-            if not a[i]:
-                r.append(0)
-                a.pop(i)
-                i-=1
-            i+=1
-        return a+r
+    def twoSum(self,a,n):
+        for i in range(len(a)):
+            for j in range(i+1,len(a)):
+                if a[i]+a[j]==n:
+                    return [i,j]
     
 s=Solution()
 
-print(s.applyOperations([847,847,0,0,0,399,416,416,879,879,206,206,206,272]))
+print(s.twoSum([2,7,11,15],9))
