250ada45d8625860e86f97f89850dedcb54ea1b7
Jhonny <gonzodzen@mail.ru>
2023-08-29 13:40:33 +0300

    Find the Kth Largest Integer in the Array

--- my.py
+++ my.py
@@ -1,10 +1,7 @@
 class Solution:
-    def twoSum(self,a,n):
-        for i in range(len(a)):
-            for j in range(i+1,len(a)):
-                if a[i]+a[j]==n:
-                    return [i,j]
+    def kthLargestNumber(self,a,n):
+        return str(sorted([int(i) for i in a],reverse=True)[n-1])
     
 s=Solution()
 
-print(s.twoSum([2,7,11,15],9))
+print(s.kthLargestNumber(["2","21","12","1"],3))
