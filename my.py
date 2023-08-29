a15909a27108a24fdbe2d2a2176d926a1971c3d0
Jhonny <gonzodzen@mail.ru>
2023-08-29 21:43:50 +0300

    Most Frequent Number Following Key In an Array

--- my.py
+++ my.py
@@ -1,8 +1,13 @@
 class Solution:
-    def frequencySort(self,a):
-        return sorted(a,key=lambda e:(a.count(e),-e))
+    def mostFrequent(self,a,n):
+        b=[a[i+1] for i,j in enumerate(a) if j==n and i!=len(a)-1]
+        s,o,m=set(b),{},0
+        for i in s:
+            t=b.count(i)
+            o[t],m=i,max(t,m)
+        return o[m]
     
 s=Solution()
 
-print(s.frequencySort([1,1,2,2,2,3]))
-print(s.frequencySort([2,3,1,3,2]))
+print(s.mostFrequent([1,100,200,1,100],1))
+print(s.mostFrequent([2,2,2,2,3],2))
