0429056b767182a4f3cb7b607c9bdd0d74d62424
Jhonny <gonzodzen@mail.ru>
2023-08-30 01:29:04 +0300

    Percentage of Letter in String

--- my.py
+++ my.py
@@ -1,12 +1,7 @@
 class Solution:
-    def rearrangeCharacters(self,s,t):
-        if [i for i in t if i not in s]:
-            return 0
-        return int(min([s.count(i)/t.count(i) for i in set(s) if i in t]))
+    def percentageLetter(self,s,t):
+        return int(len([i for i in s if i==t])/len(s)*100)
     
 s=Solution()
 
-print(s.rearrangeCharacters('ilovecodingonleetcode','code'))
-print(s.rearrangeCharacters('abcba','abc'))
-print(s.rearrangeCharacters('abbaccaddaeea','aaaaa'))
-print(s.rearrangeCharacters('codecodecodecode','codecode'))
+print(s.percentageLetter('foobar','o'))
