79646b79676fb250ce2a04334b22c8203292c0e7
Jhonny <gonzodzen@mail.ru>
2023-08-29 23:31:41 +0300

    Top K Frequent Words

--- my.py
+++ my.py
@@ -1,17 +1,15 @@
 class Solution:
-    def numberOfPairs(self,a):
-        c,q=0,[]
-        while len(set(a))!=len(a):
-            t=a.pop(0)
-            if t not in a:
-                q.append(t)
-            else:
-                a.pop(a.index(t))
-                c+=1
-        return [c,len(a+q)]
+    def topKFrequent(self,a,n):
+        s=set(a)
+        a=sorted(a)
+        if len(s)==len(a):
+            return a[:n]
+        return [j[1] for j in sorted([[a.count(i),i] for i in s],key=lambda e:(e[0],-ord(e[1][0]),-ord((e[1][0] if len(e[1])<2 else e[1][1])),-len(e)),reverse=True)[:n]]
             
     
 s=Solution()
 
-print(s.numberOfPairs([1,3,2,1,3,2,2]))
-print(s.numberOfPairs([4,98,80,3,89,14,38,98]))
+print(s.topKFrequent(["i","love","leetcode","i","love","coding"],2))
+print(s.topKFrequent(["the","day","is","sunny","the","the","the","sunny","is","is"],4))
+print(s.topKFrequent(["aaa",'aa',"a"],2))
+print(s.topKFrequent(["rmrypv","zgsedk","jlmetsplg","wnfbo","hnnftqf","bxlr","sviavwoxss","jdbgvc","zddeno","rxgw","hnnftqf","hdmvplne","rlmdt","jlmetsplg","ous","rmrypv","fwxulnpit","dmhuq","rxgw","ledleb","bxlr","indbvb","ckqqibnx","cub","ijww","ehd","hfhlfqzkcn","sviavwoxss","rxgw","bxjxpfp","mgqj","oic","ptk","fwxulnpit","ijww","sviavwoxss","bgfvfa","zfkgsudxq","alkq","dmhuq","zddeno","rxgw","zgsedk","amarxpg","bgfvfa","wnfbo","sviavwoxss","sviavwoxss","alkq","nmclxk","zgsedk","bwowfvira","ous","bxlr","zddeno","rxgw","ous","wnfbo","rmrypv","sviavwoxss","ehd","zgsedk","jlmetsplg","abxvhyehv","ledleb","wnfbo","bgfvfa","bwowfvira","amarxpg","wnfbo","bwowfvira","dmhuq","ouy","bxlr","rxgw","oic","hnnftqf","ledleb","rlmdt","oldainprua","ous","ckqqibnx","dmhuq","hnnftqf","oic","jlmetsplg","ppn","amarxpg","jlgfgwb","bxlr","bwowfvira","hdmvplne","oic","ledleb","bwowfvira","oic","ptk","dmhuq","abxvhyehv","ckqqibnx","indbvb","ypzfk","rmrypv","bxjxpfp","amarxpg","dmhuq","sviavwoxss","bwowfvira","zfkgsudxq","wnfbo"],25))
