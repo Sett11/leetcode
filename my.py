e0ff9dc52b8f32b35dc3a75dab26f3e1020f75d9
Jhonny <gonzodzen@mail.ru>
2023-08-29 20:06:24 +0300

    Can Place Flowers

--- my.py
+++ my.py
@@ -1,10 +1,21 @@
 class Solution:
-    def rotate(self,a,k):
-        for i in range(k):
-            a.insert(0,a.pop())
-        return a
+    def canPlaceFlowers(self,a,n):
+        c,l=0,len(a)
+        if l==1 and not a[0]:
+            return l>=n
+        if l>1 and not a[0] and not a[1]:
+            c=1
+            a[0]=1
+        if l>1 and not a[-1] and not a[-2]:
+            c+=1
+            a[-1]=1
+        for i in range(1,l-1):
+            if all([not i for i in [a[i-1],a[i],a[i+1]]]):
+                a[i]=1
+                c+=1
+        return c>=n
     
 s=Solution()
 
-print(s.rotate([1,2,3,4,5,6,7],3))
-print(s.rotate([-1,-100,3,99],2))
+print(s.canPlaceFlowers([0,0,1,0,1],1))
+print(s.canPlaceFlowers([1,0,0,0,1],2))
