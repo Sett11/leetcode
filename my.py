5b06703d6a095b001375656480f6d17beb12efdc
Jhonny <gonzodzen@mail.ru>
2023-08-29 16:36:09 +0300

    Find the Index of the First Occurrence in a String

--- my.py
+++ my.py
@@ -1,9 +1,9 @@
-from itertools import product as p
 class Solution:
-    def letterCombinations(self,s):
-        o={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
-        return [''.join(j) for j in p(*[o[i] for i in s]) if j]
-
+    def strStr(self,s,t):
+        return s.find(t)
+    
 s=Solution()
 
-print(s.letterCombinations(''))
+print(s.strStr('sadbutsad','sad'))
+print(s.strStr('leetcode','leeto'))
+print(s.strStr('hello','ll'))
