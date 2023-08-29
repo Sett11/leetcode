9049af77d7bc37f7ba7566a51f037c4ca7fa18ab
Jhonny <gonzodzen@mail.ru>
2023-08-29 18:40:45 +0300

    Shortest Palindrome

--- my.py
+++ my.py
@@ -1,9 +1,12 @@
 class Solution:
-    def strStr(self,s,t):
-        return s.find(t)
+    def shortestPalindrome(self,s):
+        l,t=len(s)-1,''
+        while t+s!=(t+s)[::-1]:
+            t=s[l:][::-1]
+            l-=1
+        return t+s
     
 s=Solution()
 
-print(s.strStr('sadbutsad','sad'))
-print(s.strStr('leetcode','leeto'))
-print(s.strStr('hello','ll'))
+print(s.shortestPalindrome('aacecaaa'))
+print(s.shortestPalindrome('abcd'))
