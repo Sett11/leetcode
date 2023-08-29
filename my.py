0506d1018cdfe765bfffee769b77eeb0536cd14b
Jhonny <gonzodzen@mail.ru>
2023-08-29 13:05:37 +0300

    Apply Operations to an Array

--- my.py
+++ my.py
@@ -1,14 +1,18 @@
 class Solution:
-    def searchInsert(self,a,n):
-        if n in a:
-            return a.index(n)
-        for i in range(len(a)):
-            if a[i]>n:
-                return i
-        return len(a)
+    def applyOperations(self,a):
+        i,r=0,[]
+        while i<len(a)-1:
+            if a[i]==a[i+1] and a[i]:
+                a[i]*=2
+                a.pop(i+1)
+                r.append(0)
+            if not a[i]:
+                r.append(0)
+                a.pop(i)
+                i-=1
+            i+=1
+        return a+r
     
 s=Solution()
 
-print(s.searchInsert([1,3,5,6],5))
-print(s.searchInsert([1,3,5,6],2))
-print(s.searchInsert([1,3,5,6],7))
+print(s.applyOperations([847,847,0,0,0,399,416,416,879,879,206,206,206,272]))
