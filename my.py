aeba059a9e3cb203c3af41f990196ea91f85887b
Jhonny <gonzodzen@mail.ru>
2023-08-29 09:40:59 +0300

    Remove Duplicates from Sorted Array

--- my.py
+++ my.py
@@ -1,8 +1,14 @@
-from statistics import median as m
 class Solution:
-    def findMedianSortedArrays(self,a,b):
-        return m(sorted(a+b))
+    def removeDuplicates(self,a):
+        i=0
+        while i<len(a):
+            if a.count(a[i])>1:
+                a.pop(i)
+                i-=1
+            i+=1
+        return len(a)
     
 s=Solution()
 
-print(s.findMedianSortedArrays([1,2],[3,4]))
+print(s.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+print(s.removeDuplicates([1,1,1,1]))
