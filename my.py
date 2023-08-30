a800da605828095f38fc659d8ea82b248a9d8039
Jhonny <gonzodzen@mail.ru>
2023-08-30 15:12:37 +0300

    Find Minimum in Rotated Sorted Array

--- my.py
+++ my.py
@@ -1,9 +1,7 @@
 class Solution:
-    def search(self,a,n):
-        for i in range(len(a)):
-            if a[i]==n:
-                return i
-        return -1
+    def findMin(self,a):
+        return min(a)
+    
 s=Solution()
 
-print(s.search([4,5,6,7,0,1,2],0))
+print(s.findMin([4,5,6,7,0,1,2]))
