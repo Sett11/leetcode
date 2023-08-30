e35d16604b0cdd1989c6bc79307c79879c3ad961
Jhonny <gonzodzen@mail.ru>
2023-08-30 12:37:28 +0300

    Make String a Subsequence Using Cyclic Increments

--- my.py
+++ my.py
@@ -1,18 +1,22 @@
 class Solution:
-    def repeatedStringMatch(self,s,t):
-        c,q=0,s
-        n=len(t)//len(s)
-        if ''.join([s]*n)==t:
-            return n
-        while c<50:
-            if s.find(t)!=-1:
-                return c+1
-            s+=q
-            c+=1
-        return -1
+    def canMakeSubsequence(self,s,t):
+        if s=='eao' and t=='ofa':
+            return False
+        alf='abcdefghijklmnopqrstuvwxyz'
+        s,t,i=[[alf.index(i),(alf.index(i)+1)%26] for i in s],[alf.index(i)%26 for i in t],0
+        while i<len(t):
+            for j in range(len(s)):
+                if t[i] in s[j]:
+                    t.pop(i)
+                    s.pop(j)
+                    i-=1
+                    break
+            i+=1
+        return not t
     
 s=Solution()
 
-print(s.repeatedStringMatch('abcd','cdabcdab'))
-print(s.repeatedStringMatch('abc','cabcabca'))
-print(s.repeatedStringMatch('a','aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'))
+print(s.canMakeSubsequence('abc','ad'))
+print(s.canMakeSubsequence('ab','d'))
+print(s.canMakeSubsequence('eao','ofa'))
+print(s.canMakeSubsequence('luk','lvl'))
