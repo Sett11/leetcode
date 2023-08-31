d9a36fd1fdb5cc448930c5c75e66b5b821f27b5a
Jhonny <gonzodzen@mail.ru>
2023-09-01 00:25:13 +0300

    Two Out of Three

--- my.py
+++ my.py
@@ -1,7 +1,12 @@
 class Solution:
-    def transpose(self,m):
-        return [list(i) for i in zip(*m)]
+    def twoOutOfThree(self,a,b,c):
+        s=set(a+b+c)
+        r=[]
+        for i in s:
+            if len([j for j in [i in a, i in b, i in c] if j])>1:
+                r.append(i)
+        return r
     
 s=Solution()
 
-print(s.transpose([[1,2,3],[4,5,6],[7,8,9]]))
+print(s.twoOutOfThree([1,1,3,2],[2,3],[3]))
