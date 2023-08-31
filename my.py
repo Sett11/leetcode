ee1832d9d848006ca99bd463de6f643bd18887a7
Jhonny <gonzodzen@mail.ru>
2023-08-31 11:42:56 +0300

    Sum of Digits of String After Convert

--- my.py
+++ my.py
@@ -1,17 +1,12 @@
+def f(s,n):
+    return s if not n else f(''.join([str(j) for j in str(sum([int(i) for i in s]))]),n-1)
+
 class Solution:
-    def numSquares(self,n):
-        d=[float('inf') for _ in range(n+1)]
-        d[0]=0
-        p=[i**2 for i in range(1,int(n**.5)+1)]
-        for i in range(n+1):
-            for j in p:
-                if i+j<=n:
-                    d[i+j]=min(d[i]+1,d[i + j])
-                else:
-                    break
-        return d[n]
+    def getLucky(self,s,n):
+        return int(f(''.join([str(ord(i)-96) for i in s]),n))
+        
     
 s=Solution()
 
-print(s.numSquares(22))
-print(s.numSquares(2897))
+print(s.getLucky('zbax',2))
+print(s.getLucky('iiii',1))
