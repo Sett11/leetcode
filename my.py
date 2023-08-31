8d2fecbfaf0e7c47a660456a5d0eb1671982195e
Jhonny <gonzodzen@mail.ru>
2023-08-31 08:46:59 +0300

    Number of 1 Bits

--- my.py
+++ my.py
@@ -1,11 +1,8 @@
 from re import sub
-def f(x):
-    return all(x%i for i in range(2,int(x**.5)+1)) and x>1
-
 class Solution:
-    def countPrimeSetBits(self,a,b):
-        return len([i for i in range(a,b+1) if f(len(sub(r'[^1]','',bin(i))))])
+    def hammingWeight(self,n):
+        return len(sub(r'[^1]','',bin(n)))
     
 s=Solution()
 
-print(s.countPrimeSetBits(10,15))
+print(s.hammingWeight(0b00000000000000000000000000001011))
