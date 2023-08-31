7237d9580fd802f6b806e8786e80469191f74305
Jhonny <gonzodzen@mail.ru>
2023-08-31 23:58:02 +0300

    Game of Life

--- my.py
+++ my.py
@@ -1,29 +1,24 @@
 class Solution:
-    def setZeroes(self,m):
-        a=[q for q in [[[j,i] for i,k in enumerate(h) if not k] for j,h in enumerate(m)] if q]
-        if not a:
-            return m
-        q=[]
-        [q.extend(i) for i in a]
-        a=q
-        w=m.copy()
+    def gameOfLife(self,m):
+        a=[]
+        for i in range(len(m)):
+            for j in range(len(m[i])):
+                t=[[i+1,j],[i-1,j],[i,j+1],[i,j-1],[i-1,j-1],[i+1,j+1],[i-1,j+1],[i+1,j-1]]
+                t=[m[k[0]][k[1]] for k in t if k[0]>=0 and k[0]<len(m) and k[1]>=0 and k[1]<len(m[i])]
+                c=t.count(1)
+                a.append([[i,j],m[i][j],c])
         for i in a:
-            w[i[0]]=[0]*len(w[i[0]])
-        w=[list(i) for i in zip(*w)]
-
-        for i in a:
-            w[i[1]]=[0]*len(w[i[1]])
-        
-        w=[list(i) for i in zip(*w)]
-        m.clear()
-        for i in w:
-            m.append(i)
-        
+            if i[1]==1:
+                if i[2]<2 or i[2]>3:
+                    m[i[0][0]][i[0][1]]=0
+            if i[1]==0 and i[2]==3:
+                m[i[0][0]][i[0][1]]=1
         return m
-
+    
 s=Solution()
 
-print(s.setZeroes([[1,2,3,4],[5,0,7,8],[0,10,11,12],[13,14,15,0]]))
-print(s.setZeroes([[1],[2],[3],[0]]))
-print(s.setZeroes([[1,1,1],[1,0,1],[1,1,1]]))
-print(s.setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]]))
+print(s.gameOfLife([
+    [0,1,0],
+    [0,0,1],
+    [1,1,1],
+    [0,0,0]]))
