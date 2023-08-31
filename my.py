bacfecccf2467948b2ba9ea087c94ceeaafd73ec
Jhonny <gonzodzen@mail.ru>
2023-08-31 16:17:41 +0300

    Length of Last Word

--- my.py
+++ my.py
@@ -1,39 +1,7 @@
-def f(a,q):
-    if len(a) <= 1:
-        q.extend(a[0])
-        return q
-
-    q.extend(a.pop(0))
-
-    if not a or not a[0]:
-        return q
-
-    for i in range(len(a)):
-        q.append(a[i].pop(len(a[i])-1))
-    
-    if not a or not a[0]:
-        return q
-
-    q.extend(a.pop(-1)[::-1])
-
-    if not a or not a[0]:
-        return q
-    
-    for i in range(len(a)-1, -1, -1):
-        q.append(a[i].pop(0))
-
-    if not a or not a[0]:
-        return q
-
-    return f(a, q)
-
-
 class Solution:
-    def spiralOrder(self, a):
-        return f(a,[])
-
+    def lengthOfLastWord(self,s):
+        return len([i for i in s.split(' ') if i][-1])
 
 s = Solution()
 
-print(s.spiralOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]))
-print(s.spiralOrder([[3],[2],[1]]))
+print(s.lengthOfLastWord("   fly me   to   the moon  "))
