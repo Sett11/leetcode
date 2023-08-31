6a91f77484b073ca902599c90f6d3ef5c7703867
Jhonny <gonzodzen@mail.ru>
2023-09-01 00:00:34 +0300

    Transpose Matrix

--- my.py
+++ my.py
@@ -1,24 +1,7 @@
 class Solution:
-    def gameOfLife(self,m):
-        a=[]
-        for i in range(len(m)):
-            for j in range(len(m[i])):
-                t=[[i+1,j],[i-1,j],[i,j+1],[i,j-1],[i-1,j-1],[i+1,j+1],[i-1,j+1],[i+1,j-1]]
-                t=[m[k[0]][k[1]] for k in t if k[0]>=0 and k[0]<len(m) and k[1]>=0 and k[1]<len(m[i])]
-                c=t.count(1)
-                a.append([[i,j],m[i][j],c])
-        for i in a:
-            if i[1]==1:
-                if i[2]<2 or i[2]>3:
-                    m[i[0][0]][i[0][1]]=0
-            if i[1]==0 and i[2]==3:
-                m[i[0][0]][i[0][1]]=1
-        return m
+    def transpose(self,m):
+        return [list(i) for i in zip(*m)]
     
 s=Solution()
 
-print(s.gameOfLife([
-    [0,1,0],
-    [0,0,1],
-    [1,1,1],
-    [0,0,0]]))
+print(s.transpose([[1,2,3],[4,5,6],[7,8,9]]))
