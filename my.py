54aa43b415cf9b59c4eda27b187ca79f2943bbe2
Jhonny <gonzodzen@mail.ru>
2023-08-31 19:15:37 +0300

    Merge Intervals

--- my.py
+++ my.py
@@ -1,15 +1,22 @@
 class Solution:
-    def isPrefixString(self,s,a):
-        t=a.pop(0)
-        while a:
-            if t==s:
-                return True
-            if not s.startswith(t):
-                return False
-            t+=a.pop(0)
-        return t==s
+    def merge(self,a):
+        a=sorted(a,key=lambda e:(e[0],e[1]))
+        i=0
+        while i<len(a)-1:
+            if a[i][0]<=a[i+1][0] and a[i][1]<=a[i+1][1] and a[i][1]>=a[i+1][0]:
+                a[i][1]=a[i+1][1]
+                a.pop(i+1)
+                i-=1
+            if len(a)==1:
+                break
+            if a[i][0]<=a[i+1][0] and a[i][1]>=a[i+1][1]:
+                a.pop(i+1)
+                i-=1
+            i+=1
+        return a
+    
+s=Solution()
 
-s = Solution()
-
-print(s.isPrefixString('iloveleetcode',["i","love","leetcode","apples"]))
-print(s.isPrefixString('z',['z']))
+print(s.merge([[1,3],[2,6],[8,10],[15,18]]))
+print(s.merge([[1,4],[2,3]]))
+print(s.merge([[1,4],[1,5]]))
