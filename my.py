b177f28fa09960336eb34338a4f6f9a32ea26de0
Jhonny <gonzodzen@mail.ru>
2023-08-31 17:08:37 +0300

    Check If String Is a Prefix of Array

--- my.py
+++ my.py
@@ -1,7 +1,15 @@
 class Solution:
-    def countPrefixes(self,a,p):
-        return len([i for i in a if p.startswith(i)])
+    def isPrefixString(self,s,a):
+        t=a.pop(0)
+        while a:
+            if t==s:
+                return True
+            if not s.startswith(t):
+                return False
+            t+=a.pop(0)
+        return t==s
 
 s = Solution()
 
-print(s.countPrefixes(["a","b","c","ab","bc","abc"],'abc'))
+print(s.isPrefixString('iloveleetcode',["i","love","leetcode","apples"]))
+print(s.isPrefixString('z',['z']))
