8304d01db7bce51d932ab683faf8546cc0b27b8f
Jhonny <gonzodzen@mail.ru>
2023-08-31 19:34:42 +0300

    Insert Interval

--- my.py
+++ my.py
@@ -1,5 +1,4 @@
-class Solution:
-    def merge(self,a):
+def merge(a):
         a=sorted(a,key=lambda e:(e[0],e[1]))
         i=0
         while i<len(a)-1:
@@ -14,9 +13,21 @@ class Solution:
                 i-=1
             i+=1
         return a
+
+class Solution:
+    def insert(self,a,b):
+         if not a:
+              return [b]
+         i=0
+         while i<len(a):
+              if a[i][1]>=b[0]:
+                   a.insert(i+1,b)
+                   break
+              i+=1
+         if b not in a:
+              a.append(b)
+         return merge(a)
     
 s=Solution()
 
-print(s.merge([[1,3],[2,6],[8,10],[15,18]]))
-print(s.merge([[1,4],[2,3]]))
-print(s.merge([[1,4],[1,5]]))
+print(s.insert([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8]))
