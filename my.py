6d6ce74840cf8056bc3487ca12fe83445faec587
Jhonny <gonzodzen@mail.ru>
2023-08-31 13:09:25 +0300

    Difference Between Element Sum and Digit Sum of an Array

--- my.py
+++ my.py
@@ -1,14 +1,7 @@
-from itertools import permutations
 class Solution:
-    def minimumSum(self,n):
-        a=list(str(n))
-        p=[int(j[0])+int(j[1]) for j in [[a[0]+a[1],a[2]+a[3]],[a[0]+a[1],a[3]+a[2]],[a[1]+a[0],a[3]+a[2]],[a[1]+a[0],a[2]+a[3]],[a[0]+a[2],a[1]+a[3]],[a[2]+a[0],a[3]+a[1]],[a[0]+a[3],a[1]+a[2]],[a[3]+a[0],a[2]+a[1]]]]
-        for i in range(len(a)):
-            q=a.pop(0)
-            p.extend(sum([[int(q)+int(''.join(j))] for j in permutations(a)],[]))
-            a.append(q)
-        return min(p)
+    def differenceOfSum(self,a):
+        return abs(sum(a)-sum([int(i) for i in ''.join([str(j) for j in a])]))
     
 s=Solution()
 
-print(s.minimumSum(2436))
+print(s.differenceOfSum([1,15,6,3]))
