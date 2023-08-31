31d09bbd6047af53c92aa051d806c0a9f535cd88
Jhonny <gonzodzen@mail.ru>
2023-08-31 13:04:41 +0300

    Minimum Sum of Four Digit Number After Splitting Digits

--- my.py
+++ my.py
@@ -1,7 +1,14 @@
+from itertools import permutations
 class Solution:
-    def alternateDigitSum(self,n):
-        return eval('+'.join(['(+'+i+')' if j%2==0 else '(-'+i+')' for j,i in enumerate(list(str(n)))]))
+    def minimumSum(self,n):
+        a=list(str(n))
+        p=[int(j[0])+int(j[1]) for j in [[a[0]+a[1],a[2]+a[3]],[a[0]+a[1],a[3]+a[2]],[a[1]+a[0],a[3]+a[2]],[a[1]+a[0],a[2]+a[3]],[a[0]+a[2],a[1]+a[3]],[a[2]+a[0],a[3]+a[1]],[a[0]+a[3],a[1]+a[2]],[a[3]+a[0],a[2]+a[1]]]]
+        for i in range(len(a)):
+            q=a.pop(0)
+            p.extend(sum([[int(q)+int(''.join(j))] for j in permutations(a)],[]))
+            a.append(q)
+        return min(p)
     
 s=Solution()
 
-print(s.alternateDigitSum(886996))
+print(s.minimumSum(2436))
