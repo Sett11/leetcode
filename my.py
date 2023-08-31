7eeb28dcaaeadb2b78b8693c5eea7c4b6851ef35
Jhonny <gonzodzen@mail.ru>
2023-08-31 14:57:43 +0300

    Valid Anagram

--- my.py
+++ my.py
@@ -1,19 +1,7 @@
 class Solution:
-    def groupAnagrams(self,a):
-        s,r,i=sorted([[sorted(i),i] for i in a])+[[[''],'']],[],0
-        t=[]
-        while i<len(s)-1:
-            if s[i][0]==s[i+1][0]:
-                t.append(s.pop(i+1)[1])
-                i-=1
-            else:
-                t.append(s.pop(i)[1])
-                r.append(t)
-                t=[]
-                i-=1
-            i+=1
-        return r
+    def isAnagram(self,s,t):
+        return sorted(s)==sorted(t)
     
 s=Solution()
 
-print(s.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print(s.isAnagram('abc','cab'))
