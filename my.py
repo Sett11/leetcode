2044ad93799eb712bfa38f95dc0fa7f23bfc587b
Jhonny <gonzodzen@mail.ru>
2023-08-31 11:18:24 +0300

    Perfect Squares

--- my.py
+++ my.py
@@ -1,7 +1,17 @@
 class Solution:
-    def isPowerOfTwo(self,n):
-        return n>0 and (n&(n-1))==0
+    def numSquares(self,n):
+        d=[float('inf') for _ in range(n+1)]
+        d[0]=0
+        p=[i**2 for i in range(1,int(n**.5)+1)]
+        for i in range(n+1):
+            for j in p:
+                if i+j<=n:
+                    d[i+j]=min(d[i]+1,d[i + j])
+                else:
+                    break
+        return d[n]
     
 s=Solution()
 
-print(s.isPowerOfTwo(16))
+print(s.numSquares(22))
+print(s.numSquares(2897))
