123c4c39e4917f01939428a97447f9cb7ef649c8
Jhonny <gonzodzen@mail.ru>
2023-09-01 19:29:34 +0300

    Check If All 1's Are at Least Length K Places Away

--- my.py
+++ my.py
@@ -1,7 +1,9 @@
 class Solution:
-    def search(self,a,n):
-        return n in a
+    def kLengthApart(self, nums: list[int], k: int) -> bool:
+        a=[i+k for i,j in enumerate(nums) if j==1]
+        return all(abs(a[i]-a[i+1])>k for i in range(len(a)-1))
     
 s=Solution()
 
-print(s.search([1,2,2,1],2))
+print(s.kLengthApart([1,0,0,0,1,0,0,1],2))
+print(s.kLengthApart([1,0,0,1,0,1],2))
