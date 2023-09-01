c434bd5f8b400c4f6ebef60a7eff331695eab434
Jhonny <gonzodzen@mail.ru>
2023-09-01 22:55:31 +0300

    Largest Number

--- my.js
+++ my.js
@@ -1,13 +1,4 @@
-longestPalindrome=s=>{
-    let a=[]
-    for(let i=-1;++i<s.length;){
-        for(let j=i;++j<=s.length;){
-            let t=s.slice(i,j)
-            if(t===[...t].reverse().join``)a.push(t)
-        }
-    }
-    return a.sort((a,b)=>b.length-a.length)[0]
-}
+largestNumber=a=>a.sort((a,b)=>(b+''+a)-(a+''+b)).join``.replace(/^0+$/,'0')
 
-console.log(longestPalindrome('babad'))
-console.log(longestPalindrome('cbbd'))
+console.log(largestNumber([0,0]))
+console.log(largestNumber([1000,999,808,89700]))
--- my.py
+++ my.py
@@ -1,13 +1,8 @@
-from math import factorial as f
-from sys import set_int_max_str_digits as s
-from re import sub
-s(100000)
-
 class Solution:
-    def trailingZeroes(self,n):
-        r=sub(r'0+$',lambda e:'&'+e.group(),str(f(n))).split('&')[-1]
-        return 0 if '0' not in r else len(r)
-    
+    def largestNumber(self,a):
+        return sorted([str(i) for i in a],reverse=True)
+
 s=Solution()
 
-print(s.trailingZeroes(1223))
+print(s.largestNumber([3,30,34,5,9]))
+print(s.largestNumber([111311,1113]))
