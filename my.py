1a34908a7c37fb02ce28eea4c24e87eeaab94e87
Jhonny <gonzodzen@mail.ru>
2023-09-01 22:12:56 +0300

    Reverse Words in a String

--- my.py
+++ my.py
@@ -1,14 +1,7 @@
 class Solution:
-    def divisorSubstrings(self,n,k):
-        s=list(str(n))
-        a=[int(j) for j in [''.join(s[i:i+k]) for i in range(0,len(s))] if len(j)==k]
-        c=0
-        for i in a:
-            if i and n%i==0:
-                c+=1
-        return c
+    def reverseWords(self,s):
+        return ' '.join([i for i in s.split(' ') if i][::-1])
     
 s=Solution()
 
-print(s.divisorSubstrings(430043,2))
-print(s.divisorSubstrings(240,2))
+print(s.reverseWords("a good   example"))
