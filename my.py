89938fb240f0d783b03002abfd08a2e1f34b81c3
Jhonny <gonzodzen@mail.ru>
2023-09-01 21:15:10 +0300

    Min Max Game

--- my.py
+++ my.py
@@ -1,12 +1,21 @@
-def f(s,k):
-    if len(s)<=k:
-        return s
-    return f(''.join([str(sum([int(j) for j in s[i:i+k]])) for i in range(0,len(s),k)]),k)
+def f(a):
+    if len(a)==1:
+        return a[0]
+    c=0
+    r=[]
+    for i in range(0,len(a),2):
+        if c%2==0:
+            r.append(min(a[i],a[i+1]))
+            c+=1
+        else:
+            r.append(max(a[i],a[i+1]))
+            c+=1
+    return f(r)
 
 class Solution:
-    def digitSum(self,s,k):
-        return f(s,k)
+    def minMaxGame(self,a):
+        return f(a)
     
 s=Solution()
 
-print(s.digitSum('11111222223',3))
+print(s.minMaxGame([1,3,5,2,4,8,2,2]))
