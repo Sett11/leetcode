540b1e4b4f3eddfdd47ffe5509ec6a197c3fba5c
Jhonny <gonzodzen@mail.ru>
2023-09-01 22:23:22 +0300

    Factorial Trailing Zeroes

--- my.py
+++ my.py
@@ -1,7 +1,13 @@
+from math import factorial as f
+from sys import set_int_max_str_digits as s
+from re import sub
+s(100000)
+
 class Solution:
-    def reverseWords(self,s):
-        return ' '.join([i for i in s.split(' ') if i][::-1])
+    def trailingZeroes(self,n):
+        r=sub(r'0+$',lambda e:'&'+e.group(),str(f(n))).split('&')[-1]
+        return 0 if '0' not in r else len(r)
     
 s=Solution()
 
-print(s.reverseWords("a good   example"))
+print(s.trailingZeroes(1223))
