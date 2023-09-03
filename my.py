7972d273822137a08a2bbf08431eb6b0f7eb5e57
Jhonny <gonzodzen@mail.ru>
2023-09-03 23:05:35 +0300

    H-Index

--- my.py
+++ my.py
@@ -1,37 +1,13 @@
-def summaryRanges(l):
-        r=[]
-        a=sorted(set(l))
-        while a:
-            t=[a.pop(0)]
-            while a:
-                if a[0]-t[-1]==1:
-                    t.append(a.pop(0))
-                else:
-                    break
-            r.append([t[0],t[-1]])
-        return r
+class Solution:
+    def hIndex(self,a):
+        m=max(a)
+        while m:
+            t=[i for i in a if i>=m]
+            if len(t)>=m:
+                return m
+            m-=1
+        return m
+    
+s=Solution()
 
-class SummaryRanges:
-    def __init__(self):
-        self.a=[]
-
-    def addNum(self,v):
-        self.a.append(v)
-
-    def getIntervals(self):
-        return summaryRanges(self.a)
-
-s=SummaryRanges()
-
-s.addNum(1)
-print(s.getIntervals())
-s.addNum(3)
-print(s.getIntervals())
-s.addNum(7)
-print(s.getIntervals())
-s.addNum(2)
-print(s.getIntervals())
-s.addNum(6)
-s.addNum(6)
-s.addNum(6)
-print(s.getIntervals())
+print(s.hIndex([3,0,6,1,5]))
