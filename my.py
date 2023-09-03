5eca643abb682a4ebabb4e34098ff0664c822561
Jhonny <gonzodzen@mail.ru>
2023-09-03 17:33:38 +0300

    Surrounded Regions

--- my.py
+++ my.py
@@ -1,11 +1,45 @@
 class Solution:
-    def maxProfit(self,a):
-        if len(a)==1:
-            return 0
-        return max(a)
-    
+    def solve(self,a):
+        if not a:
+            return
+        m,n=len(a),len(a[0])
+        def dfs(i,j):
+            if i<0 or i>=m or j>=n or a[i][j]!='O':
+                return
+            a[i][j]='&'
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+        for i in range(m):
+            dfs(i,0)
+            dfs(i,n-1)
+        for j in range(n):
+            dfs(0,j)
+            dfs(m-1,j)
+        for i in range(m):
+            for j in range(n):
+                if a[i][j]=='O':
+                    a[i][j]='X'
+                if a[i][j]=='&':
+                    a[i][j]='O'
+        return a
+
 s=Solution()
 
-print(s.maxProfit([7,1,5,3,6,4]))
-print(s.maxProfit([7,6,4,3,1]))
-print(s.maxProfit([2,4,1]))
+print(s.solve([["X","X","X","X"],["X","O","O","X"],["X","X", "O","X"],["X","O","X","X"]]))
+print(s.solve([["O","X","O","O","O","X"],
+               ["O","O","X","X","X","O"],
+               ["X","X","X","X","X","O"],
+               ["O","O","O","O","X","X"],
+               ["X","X","O","O","X","O"],
+               ["O","O","X","X","X","X"]]))
+# print(s.solve([["O","X","O","O","O","O","O","O","O"],
+#                ["O","O","O","X","O","O","O","O","X"],
+#                ["O","X","O","X","O","O","O","O","X"],
+#                ["O","O","O","O","X","O","O","O","O"],
+#                ["X","O","O","O","O","O","O","O","X"],
+#                ["X","X","O","O","X","O","X","O","X"],
+#                ["O","O","O","X","O","O","O","O","O"],
+#                ["O","O","O","X","O","O","O","O","O"],
+#                ["O","O","O","O","O","X","X","O","O"]]))
