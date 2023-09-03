b2a24de701d911aaf72062b59949e76a6cbad2f1
Jhonny <gonzodzen@mail.ru>
2023-09-04 01:15:21 +0300

    Reverse Vowels of a String

--- my.py
+++ my.py
@@ -1,10 +1,13 @@
+import re
+
 class Solution:
-    def reverseString(self,s):
-        s.reverse()
+    def reverseVowels(self,s):
+        a=[i for i in s if i.lower() in 'aeoiu'][::-1]
+        s=re.sub(r'[aioue]','&6&',s,flags=re.I)
+        for i in a:
+            s=s.replace('&6&',i,1)
+        return s
         
 s=Solution()
 
-a=["h","e","l","l","o"]
-
-s.reverseString(a)
-print(a)
+print(s.reverseVowels('leetcode'))
