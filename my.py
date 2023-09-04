204668f35790c82f2f6a3e9a3faab5b74b905300
Jhonny <gonzodzen@mail.ru>
2023-09-05 00:39:17 +0300

    Bulls and Cows

--- my.py
+++ my.py
@@ -1,7 +1,25 @@
 class Solution:
-    def calculate(self,s):
-        return eval(s.replace('/','//'))
-    
+    def getHint(self,s,q):
+        c=x=i=0
+        s,q=list(s),list(q)
+        while i<(min(len(s),len(q))):
+            if s[i]==q[i]:
+                c+=1
+                s.pop(i)
+                q.pop(i)
+                i-=1
+            i+=1
+        i=0
+        while i<len(s):
+            if s[i] in q:
+                x+=1
+                q.pop(q.index(s[i]))
+                s.pop(i)
+                i-=1
+            i+=1
+        return f'{c}A{x}B'
+
 s=Solution()
 
-print(s.calculate('3/2'))
+print(s.getHint('1807','7810'))
+print(s.getHint('1123','0111'))
