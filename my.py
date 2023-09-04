153ade2372eaa8b683244994aa93aba176f37c81
Jhonny <gonzodzen@mail.ru>
2023-09-04 22:27:53 +0300

    Subsets

--- my.py
+++ my.py
@@ -1,6 +1,6 @@
 from itertools import combinations
 class Solution:
-    def subsetsWithDup(self,a):
+    def subsets(self,a):
         r=[]
         for i in range(1,len(a)):
             r.extend(['&'.join([str(k) for k in sorted(j)]) for j in combinations(a,i)])
@@ -8,4 +8,4 @@ class Solution:
     
 s=Solution()
 
-print(s.subsetsWithDup([1,2,2]))
+print(s.subsets([1,2,2]))
