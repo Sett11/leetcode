0b75c8a21805a50b081e1755366fa52ae629ae2f
Jhonny <gonzodzen@mail.ru>
2023-09-05 00:16:16 +0300

    Basic Calculator II

--- my.py
+++ my.py
@@ -1,12 +1,7 @@
-a=[-56,301,-1946]
-
 class Solution:
     def calculate(self,s):
-        try:
-            return eval(s)
-        except:
-            return a.pop(0)
+        return eval(s.replace('/','//'))
     
 s=Solution()
 
-print(s.calculate('(1+(4+5+2)-3)+(6+8)'))
+print(s.calculate('3/2'))
