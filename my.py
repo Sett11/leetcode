7f63677e5045edddb81986a3c81f3772d368c30e
Jhonny <gonzodzen@mail.ru>
2023-09-04 23:41:56 +0300

    Min Stack

--- my.py
+++ my.py
@@ -1,18 +1,28 @@
-from itertools import product
-from re import sub,I
-class Solution:
-    def letterCasePermutation(self,s):
-        ns=sub(r'[a-z]','&',s,flags=I)
-        a=[list(j) for j in product(*[[i.lower(),i.upper()] for i in s if i.isalpha()])]
-        r=[]
-        for i in a:
-            t=ns
-            while i:
-                t=t.replace('&',i.pop(0),1)
-            r.append(t)
-        return r
-    
-s=Solution()
+class MinStack:
 
-print(s.letterCasePermutation('RmR'))
-print(s.letterCasePermutation('1a2b'))
+    def __init__(self):
+        self.stack=[]
+
+    def push(self,v):
+        self.stack.append(v)
+
+    def pop(self):
+        self.stack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return min(self.stack)
+    
+m=MinStack()
+
+m.push(-2)
+m.push(0)
+m.push(-3)
+
+print(m.getMin())
+
+m.pop()
+
+print(m.top())
