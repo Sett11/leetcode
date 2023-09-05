5ee9d0670b94f77bcfea2013664179e0de58fd54
Jhonny <gonzodzen@mail.ru>
2023-09-05 19:40:56 +0300

    Intersection of Two Arrays

--- my.py
+++ my.py
@@ -1,15 +1,7 @@
 class Solution:
-    def coinChange(self,a,n):
-        r=[n+1]*(n+1)
-        r[0]=0
-        a.sort()
-        for i in range(1,n+1):
-            for j in a:
-                if i-j>=0:
-                    r[i]=min(r[i],1+r[i-j])
-        return r[n] if r[n]!=n+1 else -1
+    def intersection(self,a,b):
+        return list(set(a).intersection(set(b)))
     
 s=Solution()
 
-print(s.coinChange([1,2,5],11))
-print(s.coinChange([186,419,83,408],6249))
+print(s.intersection([4,9,5],[9,4,9,8,4]))
