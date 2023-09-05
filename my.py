e493780f7ebb5c02454406b851a6c0457fe49a48
Jhonny <gonzodzen@mail.ru>
2023-09-05 22:11:30 +0300

    Defanging an IP Address

--- my.py
+++ my.py
@@ -1,20 +1,7 @@
 class Solution:
-    def minimumLength(self,s):
-        if len(s)<=1:
-            return len(s)
-        while len(s)>1 and s[0]==s[-1]:
-            i=0
-            while i<len(s)-1 and s[i]==s[i+1]:
-                i+=1
-            j=len(s)-1
-            while j and s[j]==s[j-1] and j!=i:
-                j-=1
-            s=s[i+1:j]
-        return len(s)
+    def defangIPaddr(self,s):
+        return s.replace('.','[.]')
     
 s=Solution()
 
-print(s.minimumLength('ca'))
-print(s.minimumLength('cabaabac'))
-print(s.minimumLength('aabccabba'))
-print(s.minimumLength('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbbccbcbcbccbbabbb'))
+print(s.defangIPaddr("1.1.1.1"))
