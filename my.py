a53890d4356d03080f0ef6ede406cc7d25178990
Jhonny <gonzodzen@mail.ru>
2023-09-05 20:31:47 +0300

    Count Common Words With One Occurrence

--- my.py
+++ my.py
@@ -1,14 +1,10 @@
+def sorted_one_count(x):
+    return [i for i in x if x.count(i)==1]
+
 class Solution:
-    def intersection(self,a):
-        r=set(a.pop(0))
-        a=[set(i) for i in a]
-        for i in range(len(a)):
-            r=r.intersection(a[i])
-        return sorted(r)
-    
+    def countWords(self,a,b):
+        return len(set(sorted_one_count(a)).intersection(set(sorted_one_count(b))))
+
 s=Solution()
 
-print(s.intersection([[1,2,3]]))
-print(s.intersection([[ 3 ,1,2, 4 ,5],[1,2, 3 , 4 ],[ 3 , 4 ,5,6]]))
-print(s.intersection([[73,6,87,26,12,83,19,43,31,63,54,21,70,10,16,97,40,93,65,99,53,61],[43, 74,83,53,67,8,16,72,9,50,45,97,79,88,80,12,26,46,30,70,13,84,100,40,60,4,6, 52],[76,86,40,90,15,44,43,70,61,2,95,10,93,74,6,99,96,47,48,26,82,16,84, 38,85,14]]))
-print(s.intersection([[17,20],[20,25,49],[16,7,20,47,17]]))
+print(s.countWords(["leetcode","is","amazing","as","is"],["amazing","leetcode","is"]))
