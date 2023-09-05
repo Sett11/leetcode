0c6783698aa5f6ca7f7c8446cd7e3eb535d0fa86
Jhonny <gonzodzen@mail.ru>
2023-09-05 19:48:45 +0300

    Find the Difference of Two Arrays

--- my.py
+++ my.py
@@ -1,7 +1,8 @@
 class Solution:
-    def intersection(self,a,b):
-        return list(set(a).intersection(set(b)))
+    def findDifference(self,a,b):
+        a,b=set(a),set(b)
+        return [list(i) for i in [a.difference(b),b.difference(a)]]
     
 s=Solution()
 
-print(s.intersection([4,9,5],[9,4,9,8,4]))
+print(s.findDifference([1,2,3],[2,4,6]))
