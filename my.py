a5b510104adb7f2a053dbfa0ac6e6a6ba474031b
Jhonny <gonzodzen@mail.ru>
2023-09-05 22:09:03 +0300

    Minimum Length of String After Deleting Similar Ends

--- my.py
+++ my.py
@@ -1,9 +1,20 @@
 class Solution:
-    def kthDistinct(self,a,k):
-        r=[i for i in a if a.count(i)==1]
-        return '' if len(r)<k else r[k-1]
-
+    def minimumLength(self,s):
+        if len(s)<=1:
+            return len(s)
+        while len(s)>1 and s[0]==s[-1]:
+            i=0
+            while i<len(s)-1 and s[i]==s[i+1]:
+                i+=1
+            j=len(s)-1
+            while j and s[j]==s[j-1] and j!=i:
+                j-=1
+            s=s[i+1:j]
+        return len(s)
+    
 s=Solution()
 
-print(s.kthDistinct(["d","b","c","b","c","a"],2))
-print(s.kthDistinct(['a','a'],1))
+print(s.minimumLength('ca'))
+print(s.minimumLength('cabaabac'))
+print(s.minimumLength('aabccabba'))
+print(s.minimumLength('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbbccbcbcbccbbabbb'))
