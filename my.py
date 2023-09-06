8dce810461af4def44768489111030be0f75b33c
Jhonny <gonzodzen@mail.ru>
2023-09-07 00:38:32 +0300

    Convert a Number to Hexadecimal

--- my.py
+++ my.py
@@ -1,14 +1,3 @@
-from random import choice
-
 class Solution:
-    def __init__(self,a):
-        self.a=a
-
-    def pick(self,t):
-        return choice([i for i,j in enumerate(self.a) if j==t])
-    
-s=Solution([1, 2, 3, 3, 3])
-
-print(s.pick(3))
-print(s.pick(1))
-print(s.pick(3))
+    def toHex(self,n):
+        return hex(n)[2:] if n>=0 else hex(n+2**32)[2:]
