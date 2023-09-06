b9a305b719b98b8206f47b2a1707b02715039aaa
Jhonny <gonzodzen@mail.ru>
2023-09-06 19:11:48 +0300

    Insert Delete GetRandom O(1)

--- my.py
+++ my.py
@@ -1,14 +1,33 @@
-q=[1,3,292051]
-class Solution:
-    def smallestDistancePair(self,a,k):
-        if k==25000000:
-            return q.pop(0)
-        r=[]
-        for i in range(len(a)):
-            for j in range(i+1,len(a)):
-                r.append(abs(a[i]-a[j]))
-        return sorted(r)[k-1]
-    
-s=Solution()
+from random import choice
 
-print(s.smallestDistancePair([1,3,1,4,7,8,1,1,2],2))
+class RandomizedSet:
+    def __init__(self):
+        self.a=[]
+
+    def insert(self,v):
+        q=v in self.a
+        if not q:
+            self.a.append(v)
+        return not q
+
+    def remove(self,v):
+        q=v in self.a
+        if q:
+            self.a.remove(v)
+        return q
+
+    def getRandom(self):
+        return choice(self.a)
+    
+r=RandomizedSet()
+
+print(r.insert(7))
+print(r.insert(7))
+print(r.insert(1))
+print(r.insert(2))
+print(r.insert(5))
+
+print(r.remove(5))
+
+print(r.getRandom())
+print(r.getRandom())
