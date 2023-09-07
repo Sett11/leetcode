aa596973cd9d05600f3f91b089bf2a54ebb9a678
Jhonny <gonzodzen@mail.ru>
2023-09-07 21:32:39 +0300

    Find Subarrays With Equal Sum

--- my.py
+++ my.py
@@ -1,18 +1,8 @@
-from re import sub
-
 class Solution:
-    def maxNumberOfBalloons(self,s):
-        if not all(i in s for i in 'balon'):
-            return 0
-        a=sub(r'(.)\1*',lambda e:' '+e.group(),''.join(sorted(sub(r'[^balon]','',s)))).split(' ')[1:]
-        m=1e5
-        for i in a:
-            if i[0]=='l' or i[0]=='o':
-                m=min(len(i)//2,m)
-            else:
-                m=min(len(i),m)
-        return m
+    def findSubarrays(self,a):
+        r=[sum([a[i],a[i+1]]) for i in range(len(a)-1)]
+        return len(set(r))!=len(r)
     
 s=Solution()
 
-print(s.maxNumberOfBalloons("loonbalxballpoon"))
+print(s.findSubarrays([4,2,4]))
