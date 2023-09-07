23425db171b6c0c16a091b8a5ac53aed6241a261
Jhonny <gonzodzen@mail.ru>
2023-09-07 21:21:43 +0300

    Maximum Number of Balloons

--- my.py
+++ my.py
@@ -1,15 +1,18 @@
+from re import sub
+
 class Solution:
-    def bestHand(self,a,b):
-        if len(set(b))==1:
-            return 'Flush'
-        if [i for i in a if a.count(i)>2]:
-            return 'Three of a Kind'
-        if [i for i in a if a.count(i)>1]:
-            return 'Pair'
-        return 'High Card'
+    def maxNumberOfBalloons(self,s):
+        if not all(i in s for i in 'balon'):
+            return 0
+        a=sub(r'(.)\1*',lambda e:' '+e.group(),''.join(sorted(sub(r'[^balon]','',s)))).split(' ')[1:]
+        m=1e5
+        for i in a:
+            if i[0]=='l' or i[0]=='o':
+                m=min(len(i)//2,m)
+            else:
+                m=min(len(i),m)
+        return m
     
 s=Solution()
 
-print(s.bestHand([13,2,3,1,9],["a","a","a","a","a"]))
-print(s.bestHand([4,3,2,4,4],["d","a","a","b","c"]))
-print(s.bestHand([10,10,7,7,4],["d","a","a","b","c"]))
+print(s.maxNumberOfBalloons("loonbalxballpoon"))
