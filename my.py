a8317a77df7adb010be47b419f99f37a797e89f9
Jhonny <gonzodzen@mail.ru>
2023-09-08 00:27:52 +0300

    Permutation in String

--- my.py
+++ my.py
@@ -1,13 +1,13 @@
 class Solution:
-    def findAnagrams(self,s,t):
+    def checkInclusion(self,t,s):
         r=[]
         t=sorted(t)
         for i in range(len(s)-len(t)+1):
             if sorted(s[i:i+len(t)])==t:
-                r.append(i)
-        return r
+                return True
+        return False
     
 s=Solution()
 
-print(s.findAnagrams('cbaebabacd','abc'))
-print(s.findAnagrams('abab','ab'))
+print(s.checkInclusion('ab','"eidbaooo"'))
+print(s.checkInclusion('ab','eidboaoo'))
