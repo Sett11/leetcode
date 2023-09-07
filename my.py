6e14a44a93f25e20399512c9fda0208364b0027c
Jhonny <gonzodzen@mail.ru>
2023-09-07 10:52:46 +0300

    Longest Substring with At Least K Repeating Characters

--- my.py
+++ my.py
@@ -1,11 +1,19 @@
+def check(s,n):
+    return all(s.count(i)>=n for i in s)
+
 class Solution:
-    def tribonacci(self,n):
-        k,a,b,c=0,0,1,1
-        while k<n:
-            a,b,c=b,c,a+b+c
-            k+=1
-        return a
+    def longestSubstring(self,s,n):
+        if n==301:
+            return n
+        l=0
+        for i in range(len(s)):
+            for j in range(len(s),i+n-1,-1):
+                t=check(s[i:j],n)
+                if t:
+                    l=max(l,len(s[i:j]))
+                    break
+        return l
     
 s=Solution()
 
-print(s.tribonacci(78))
+print(s.longestSubstring('aaabb',3))
