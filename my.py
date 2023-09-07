290c5326ad7705d139588b6f099078ac7d0923df
Jhonny <gonzodzen@mail.ru>
2023-09-07 20:28:46 +0300

    Find Winner on a Tic Tac Toe Game

--- my.py
+++ my.py
@@ -1,9 +1,25 @@
-class Solution:
-    def categorizeBox(self,l,w,h,m):
-        v=l*w*h>=1e9 or (l>=1e4 or w>=1e4 or h>=1e4)
-        q=m>=100
-        return 'Both' if v and q else 'Neither' if not v and not q else 'Bulky' if v else 'Heavy'
+def wrap(m):
+    a=[[0]*3 for _ in range(3)]
+    for i in range(len(m)):
+        if i&1:
+            a[m[i][0]][m[i][1]]='O'
+        else:
+            a[m[i][0]][m[i][1]]='X'
+    return a
 
+def f(a):
+    r,i=[],0
+    while i<len(a):
+        r.append(a[i][i])
+        i+=1
+    return r
+
+class Solution:
+    def tictactoe(self,m):
+        a=wrap(m)
+        r=[j for j in [list(i) for i in zip(*a)]+a+[f(a)]+[f([i[::-1] for i in a])] if len(set(j))==1 and j[0]]
+        return 'Draw' if not r and len(m)==9 else 'Pending' if not r else 'A' if r[0][0]=='X' else 'B'
+    
 s=Solution()
 
-print(s.categorizeBox(1000,35,700,300))
+print(s.tictactoe([[0,0],[1,1]]))
