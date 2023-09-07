995fe85ad34147980c85c9ba4a27aa2b22efb408
Jhonny <gonzodzen@mail.ru>
2023-09-07 22:16:56 +0300

    Find Two Non-overlapping Sub-arrays Each With Target Sum

--- my.py
+++ my.py
@@ -1,8 +1,21 @@
 class Solution:
-    def findSubarrays(self,a):
-        r=[sum([a[i],a[i+1]]) for i in range(len(a)-1)]
-        return len(set(r))!=len(r)
+    def minSumOfLengths(self,a,t):
+        if t in a and a.count(t)>1:
+             return 2
+        r=[]
+        i=0
+        while i<len(a):
+            q=[a[i]]
+            for j in range(i+1,len(a)):
+                if sum(q)==t and (i>=r[-1][2] if r else j):
+                    r.append([len(q),i,j])
+                    break
+                q.append(a[j])
+            i+=1
+        return [i[0] for i in r]#-1 if len(r)<2 else sum(sorted(r)[:2])
     
 s=Solution()
 
-print(s.findSubarrays([4,2,4]))
+print(s.minSumOfLengths([3,1,1,1,5,1,2,1],3))
+print(s.minSumOfLengths([2,1,3,3,2,3,1],6))
+print(s.minSumOfLengths([1,1,1,2,2,2,4,4],6))
