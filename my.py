27a3bc27780f03448bd949e88d472ff07514cfae
Jhonny <gonzodzen@mail.ru>
2023-09-08 09:16:11 +0300

    Roman to Integer

--- my.py
+++ my.py
@@ -1,13 +1,11 @@
 class Solution:
-    def intToRoman(self,n):
-        o=[[1000,'M'],[900,'CM'],[500,'D'],[400,'CD'],[100,'C'],[90,'XC'],[50,'L'],[40,'XL'],[10,'X'],[9,'IX'],[5,'V'],[4,'IV'],[1,'I']]
-        s=''
-        for i in o:
-            while n>=i[0]:
-                n-=i[0]
-                s+=i[1]
-        return s
-    
+    def romanToInt(self,s):
+        a=[['CM', 900],['M', 1000], ['CD', 400], ['D', 500], ['XC', 90], ['C', 100], ['XL', 40], ['L', 50], ['IX', 9], ['X', 10], ['IV', 4], ['V', 5], ['I', 1]]
+        for i in a:
+            s=s.replace(i[0],' '+str(i[1])+' ')
+        return sum([int(i) for i in s.split(' ')if i])
+
 s=Solution()
 
-print(s.intToRoman(1994))
+print(s.romanToInt('LVIII'))
+print(s.romanToInt('MCMXCIV'))
