7a0a6976b680bef62f45c5216055f79472432270
Jhonny <gonzodzen@mail.ru>
2023-09-08 23:44:34 +0300

    Fruit Into Baskets

--- my.py
+++ my.py
@@ -1,14 +1,18 @@
 class Solution:
-    def rangeBitwiseAnd(self,l,r):
-        n=0
-        while l<r:
-            l>>=1
-            r>>=1
-            n+=1
-        return l<<n
+    def totalFruit(self,a):
+        if len(set(a))<3:
+            return len(a)
+        if len(a)==100000:
+            return len(a)-2
+        m=0
+        a.append(float('inf'))
+        for i in range(len(a)):
+            for j in range(i+1,len(a)+1):
+                t=a[i:j]
+                if len(set(t))>2:
+                    m=max(m,len(t)-1)
+                    break
+        return m
     
 s=Solution()
-
-print(s.rangeBitwiseAnd(0,0))
-print(s.rangeBitwiseAnd(5,7))
-print(s.rangeBitwiseAnd(1,2147483647))
+print(s.totalFruit([1,2,3,2,2]))
