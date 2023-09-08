74124bac34feea3b986bd6789980128cbd93003d
Jhonny <gonzodzen@mail.ru>
2023-09-08 09:19:05 +0300

    A Number After a Double Reversal

--- my.py
+++ my.py
@@ -1,11 +1,8 @@
 class Solution:
-    def romanToInt(self,s):
-        a=[['CM', 900],['M', 1000], ['CD', 400], ['D', 500], ['XC', 90], ['C', 100], ['XL', 40], ['L', 50], ['IX', 9], ['X', 10], ['IV', 4], ['V', 5], ['I', 1]]
-        for i in a:
-            s=s.replace(i[0],' '+str(i[1])+' ')
-        return sum([int(i) for i in s.split(' ')if i])
-
+    def isSameAfterReversals(self,n):
+        return int(str(int(str(n)[::-1]))[::-1])==n
+    
 s=Solution()
 
-print(s.romanToInt('LVIII'))
-print(s.romanToInt('MCMXCIV'))
+print(s.isSameAfterReversals(526))
+print(s.isSameAfterReversals(1800))
