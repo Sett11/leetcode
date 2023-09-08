ef2131093020d0846b816f1abd4a6dc06290d8ee
Jhonny <gonzodzen@mail.ru>
2023-09-08 23:27:52 +0300

    Bitwise AND of Numbers Range

--- my.py
+++ my.py
@@ -1,23 +1,14 @@
-def check(a):
-    for i in range(len(a)):
-        for j in range(i+1,len(a)):
-            if a[i]&a[j]:
-                return False
-    return True
-
 class Solution:
-    def longestNiceSubarray(self,a):
-        m=0
-        a.append(a[-1]+1)
-        for i in range(len(a)):
-            for j in range(i+1,len(a)+1):
-                t=a[i:j]
-                if not check(t):
-                    m=max(m,len(t)-1)
-                    break
-        return m
+    def rangeBitwiseAnd(self,l,r):
+        n=0
+        while l<r:
+            l>>=1
+            r>>=1
+            n+=1
+        return l<<n
     
 s=Solution()
 
-print(s.longestNiceSubarray([1,3,8,48,10]))
-print(s.longestNiceSubarray([135745088,609245787,16,2048,2097152]))
+print(s.rangeBitwiseAnd(0,0))
+print(s.rangeBitwiseAnd(5,7))
+print(s.rangeBitwiseAnd(1,2147483647))
