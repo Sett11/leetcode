f7eb2bea310e1f6082a7bd959bb93378d4ad01fe
Jhonny <gonzodzen@mail.ru>
2023-09-09 00:23:29 +0300

    Detect Capital

--- my.py
+++ my.py
@@ -1,3 +1,7 @@
 class Solution:
-    def toLowerCase(self,s):
-        return s.lower()
+    def detectCapitalUse(self,s):
+        return s.islower() or s.isupper() or (s[0].isupper() and s[1:].islower())
+    
+s=Solution()
+
+print(s.detectCapitalUse('Google'))
