52ecf73d2dd657b120c567378894db6d989b2abf
Jhonny <gonzodzen@mail.ru>
2023-09-09 00:16:12 +0300

    Capitalize the Title

--- my.py
+++ my.py
@@ -1,8 +1,8 @@
 class Solution:
-    def customSortString(self,t,s):
-        return ''.join(map(lambda e:e[1],sorted([[t.index(i) if i in t else s.index(i),i] for i in s])))
+    def capitalizeTitle(self,s):
+        return ' '.join([i.capitalize() if len(i)>2 else i.lower() for i in s.split(' ')])
     
 s=Solution()
 
-print(s.customSortString("cba","abcd"))
-print(s.customSortString("cbafg","abcd"))
+print(s.capitalizeTitle("capiTalIze tHe titLe"))
+print(s.capitalizeTitle("First leTTeR of EACH Word"))
