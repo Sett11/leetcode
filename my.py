703bbde3169d822d13360b0ab2c380f931afa421
Jhonny <gonzodzen@mail.ru>
2023-09-09 00:18:19 +0300

    To Lower Case

--- my.py
+++ my.py
@@ -1,8 +1,3 @@
 class Solution:
-    def capitalizeTitle(self,s):
-        return ' '.join([i.capitalize() if len(i)>2 else i.lower() for i in s.split(' ')])
-    
-s=Solution()
-
-print(s.capitalizeTitle("capiTalIze tHe titLe"))
-print(s.capitalizeTitle("First leTTeR of EACH Word"))
+    def toLowerCase(self,s):
+        return s.lower()
