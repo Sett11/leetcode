49dcde17a170c911dc67872f190c4c28a89c6d29
Jhonny <gonzodzen@mail.ru>
2023-09-08 20:05:14 +0300

    Increasing Triplet Subsequence

--- my.py
+++ my.py
@@ -1,30 +1,16 @@
-class LRUCache:
-    def __init__(self,size):
-        self.size=size
-        self.cache={}
-        self.sorted_cache=[]
+class Solution:
+    def increasingTriplet(self,a):
+        b,c=[float('inf')]*2
+        for i in a:
+            if i<=b:
+                b=i
+            elif i<=c:
+                c=i
+            elif i>c:
+                return True
+        return False
 
-    def get(self,k):
-        if self.cache.get(k,None)!=None:
-            i=self.sorted_cache.index(k)
-            self.sorted_cache.append(self.sorted_cache.pop(i))
-            return self.cache[k]
-        return -1
+s=Solution()
 
-    def put(self,k,v):
-        if len(self.cache)==self.size and k not in self.cache:
-            e=self.sorted_cache.pop(0)
-            del self.cache[e]
-        if k in self.sorted_cache:
-            self.sorted_cache.append(self.sorted_cache.pop(self.sorted_cache.index(k)))
-        else:
-            self.sorted_cache.append(k)    
-        self.cache[k]=v
-    
-l=LRUCache(2)
-
-print(l.put(2,1))
-print(l.put(1,1))
-print(l.put(2,3))
-print(l.put(4,1))
-print(l.get(2))
+print(s.increasingTriplet([2,1,5,0,4,6]))
+print(s.increasingTriplet([20,100,10,12,5,13]))
