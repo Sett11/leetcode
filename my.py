36b5550af6049a232339f2e4e68e8c1333b03e23
Jhonny <gonzodzen@mail.ru>
2023-09-09 16:50:59 +0300

    Find Maximum Number of String Pairs

--- my.py
+++ my.py
@@ -1,64 +1,7 @@
-class AllOne:
-    def __init__(self):
-        self.a={}
-        self.min=100000
-        self.max=0
-        self.minstack=[]
-        self.maxstack=[]
+class Solution:
+    def maximumNumberOfStringPairs(self,s):
+        return len(s)-len(set([''.join(sorted(i)) for i in s]))
+    
+s=Solution()
 
-    def inc(self,k):
-        if k in self.a:
-            self.a[k]+=1
-        else:
-            self.a[k]=1
-        self.maxstack=[]
-        self.minstack=[]
-
-    def dec(self,k):
-        self.a[k]-=1
-        if self.a[k]<=0:
-            del self.a[k]
-        self.maxstack=[]
-        self.minstack=[]
-
-    def getMaxKey(self):
-        if self.maxstack:
-            return self.maxstack[-1]
-        m=v=0
-        for i in self.a:
-            if self.a[i]>m:
-                m=self.a[i]
-                v=i
-        if v and m>=self.max:
-            self.max=m
-            self.maxstack.append(v)
-        return v or ''
-
-    def getMinKey(self):
-        if self.minstack:
-            return self.minstack[-1]
-        m=float('inf')
-        v=0
-        for i in self.a:
-            if self.a[i]<m:
-                m=self.a[i]
-                v=i
-        if v and m<=self.min:
-            self.min=m
-            self.minstack.append(v)
-        return v or ''
-
-a=AllOne()
-
-print(a.inc('hello'))
-print(a.inc('hello'))
-
-print(a.getMaxKey())
-print(a.getMinKey())
-
-print(a.inc('leet'))
-
-print(a.getMaxKey())
-print(a.getMinKey())
-
-print(a.a)
+print(s.maximumNumberOfStringPairs(["cd","ac","dc","ca","zz"]))
