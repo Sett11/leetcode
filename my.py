00f1cdd77d107abf959d51cf82100bc0677d4df1
Jhonny <gonzodzen@mail.ru>
2023-09-09 14:55:11 +0300

    Validate IP Address

--- my.py
+++ my.py
@@ -1,20 +1,22 @@
-from re import sub
-
 class Solution:
-    def strongPasswordCheckerII(self,s):
-        r=[False]*4+[len(s)>7]
-        for i in range(len(s)):
-            if s[i].islower():
-                r[0]=True
-            if s[i].isupper():
-                r[1]=True
-            if s[i].isdigit():
-                r[2]=True
-            if s[i] in '!@#$%^&*()-+':
-                r[3]=True
-        return all(r) and not bool([i for i in sub(r'(.)\1*',lambda e:' '+e.group()+' ',s).split(' ') if len(i)>1])
-        
+    def validIPAddress(self,s):
+        a,b=s.split('.'),s.split(':')
+        if len(a)!=4 and len(b)!=8:
+            return 'Neither'
+        if len(a)==4:
+            try:
+                return (all([int(i)>=0 and int(i)<256 and not (len(i)>1 and i[0]=='0') for i in a]) and 'IPv4') or 'Neither'
+            except:
+                return 'Neither'
+        else:
+            try:
+                return (all([len(i)<=4 for i in b]) and [int(i,16) for i in b] and 'IPv6') or 'Neither'
+            except:
+                return 'Neither'
     
 s=Solution()
 
-print(s.strongPasswordCheckerII('IloveLe3tcode!'))
+print(s.validIPAddress("172.16.254.1"))
+print(s.validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334"))
+print(s.validIPAddress("02001:0db8:85a3:0000:0000:8a2e:0370:7334"))
+print(s.validIPAddress("192.168@1.1"))
