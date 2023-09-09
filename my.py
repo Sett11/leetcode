2e3e5406f0a23a62fad40176ff5c970ab8c59d28
Jhonny <gonzodzen@mail.ru>
2023-09-09 17:47:19 +0300

    Count Symmetric Integers

--- my.py
+++ my.py
@@ -1,15 +1,17 @@
 class Solution:
-    def xorOperation(self,n,k):
-        i=k
-        s=''
-        c=1
-        while c<=n:
-            s+=f'{i}^'
-            i+=2
-            c+=1
-        return eval(s[:-1])
+    def countSymmetricIntegers(self,l,r):
+        c=0
+        i=l
+        while i<=r:
+            if i==101:
+                i=1001
+            j=str(i)
+            l=len(j)//2
+            if sum([int(k) for k in j[l:]])==sum([int(k) for k in j[:l]]):
+                c+=1
+            i+=1
+        return c
     
 s=Solution()
 
-print(s.xorOperation(4,3))
-print(s.xorOperation(5,0))
+print(s.countSymmetricIntegers(1,100))
