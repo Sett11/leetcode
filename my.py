0fa13c6dd2f7814d955f8611f51aa32b5cfeb609
Jhonny <gonzodzen@mail.ru>
2023-09-09 17:19:49 +0300

    Arranging Coins

--- my.py
+++ my.py
@@ -1,9 +1,15 @@
 class Solution:
-    def countSegments(self,s):
-        return len([i for i in s.split(' ') if i])
+    def arrangeCoins(self,n):
+        c=0
+        i=1
+        while n>0:
+            n-=i
+            i+=1
+            c+=1
+        return c if not n else c-1
     
 s=Solution()
 
-print(s.countSegments("Hello, my name is John"))
-print(s.countSegments("    , , , ,        a, eaefa"))
-print(s.countSegments(''))
+print(s.arrangeCoins(8))
+print(s.arrangeCoins(5))
+print(s.arrangeCoins(10000000))
