9c096e687d067519c48be57c00d038feef8b7ee7
Jhonny <gonzodzen@mail.ru>
2023-09-09 17:29:52 +0300

    XOR Operation in an Array

--- my.py
+++ my.py
@@ -1,15 +1,15 @@
 class Solution:
-    def arrangeCoins(self,n):
-        c=0
-        i=1
-        while n>0:
-            n-=i
-            i+=1
+    def xorOperation(self,n,k):
+        i=k
+        s=''
+        c=1
+        while c<=n:
+            s+=f'{i}^'
+            i+=2
             c+=1
-        return c if not n else c-1
+        return eval(s[:-1])
     
 s=Solution()
 
-print(s.arrangeCoins(8))
-print(s.arrangeCoins(5))
-print(s.arrangeCoins(10000000))
+print(s.xorOperation(4,3))
+print(s.xorOperation(5,0))
