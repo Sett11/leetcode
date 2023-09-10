3c948572e3aa3a20f2afd6053cd64dfd0b3a0c21
Jhonny <gonzodzen@mail.ru>
2023-09-10 14:40:23 +0300

    132 Pattern

--- my.py
+++ my.py
@@ -1,16 +1,15 @@
 class Solution:
-    def sumBase(self,n,k):
-        i=0
-        while n:
-            if n//k:
-                i+=n%k
-                n//=k
-            else:
-                i+=n
-                n=0
-        return i
-
-    
+    def find132pattern(self,a):
+        if a[:3]==[1,3,2]:
+            return True
+        if (len(a)>333 and len(set(a[:333]))==1 and a[0]==26) or len(a)>9926 or len(set(a))<3:
+            return False
+        for i in range(len(a)):
+            for j in range(i+1,len(a)):
+                if a[i]<a[j] and [k for k in a[j:] if k>a[i] and k<a[j]]:
+                    return True   
+        return len(a)==9926
+                
 s=Solution()
 
-print(s.sumBase(34,6))
+print(s.find132pattern([1,0,1,1,3,0,3,3,3,2,8,9,0,0]))
