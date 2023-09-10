c8a0eb24e3031851395fffa38fe7c9a8b2358bed
Jhonny <gonzodzen@mail.ru>
2023-09-10 17:58:49 +0300

    Alert Using Same Key-Card Three or More Times in a One Hour Period

--- my.py
+++ my.py
@@ -1,15 +1,21 @@
 class Solution:
-    def find132pattern(self,a):
-        if a[:3]==[1,3,2]:
-            return True
-        if (len(a)>333 and len(set(a[:333]))==1 and a[0]==26) or len(a)>9926 or len(set(a))<3:
-            return False
-        for i in range(len(a)):
-            for j in range(i+1,len(a)):
-                if a[i]<a[j] and [k for k in a[j:] if k>a[i] and k<a[j]]:
-                    return True   
-        return len(a)==9926
-                
+    def alertNames(self,a,b):
+        r=[[a[i],int(b[i][:2]+b[i][3:])] for i in range(len(a))]
+        o={i:[] for i in set(a)}
+        res=[]
+        for i in r:
+            o[i[0]].append(i[1])
+        for i in o:
+            o[i].sort()
+            for j in range(len(o[i])-2):
+                if o[i][j+2]-o[i][j]<=100:
+                    res.append(i)
+                    break
+        return sorted(set(res))
+
+
 s=Solution()
 
-print(s.find132pattern([1,0,1,1,3,0,3,3,3,2,8,9,0,0]))
+print(s.alertNames(["a","a","a","a","b","b","b","b","b","b","c","c","c","c"],["01:35","08:43","20:49","00:01","17:44","02:50","18:48","22:27","14:12","18:00","12:38","20:40","03:59","22:24"]))
+
+print(s.alertNames(["a","a","a","a","a","b","b","b","b","b","b"],["23:20","11:09","23:30","23:02","15:28","22:57","23:40","03:43","21:55","20:38","00:19"]))
