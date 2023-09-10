9dea4269a97b87020d45a28a4bb1471e669c4345
Jhonny <gonzodzen@mail.ru>
2023-09-10 18:15:59 +0300

    Defuse the Bomb

--- my.py
+++ my.py
@@ -1,12 +1,20 @@
 class Solution:
-    def isCircularSentence(self,s):
-        a=s.split(' ')
-        for i in range(len(a)-1):
-            if a[i][-1]!=a[i+1][0]:
-                return False
-        return s[0]==s[-1]
+    def decrypt(self,a,k):
+        if not k:
+            return [0]*len(a)
+        b=a+a+a
+        l=len(a)
+        r=[]
+        for i in range(l,l+l):
+            if k>0:
+                r.append(sum(b[i:i+k]))
+            else:
+                r.append(sum(b[i+k:i]))
+        if k>0:
+            r.append(r.pop(0))
+        return r
     
 s=Solution()
 
-print(s.isCircularSentence("leetcode exercises sound delightful"))
-print(s.isCircularSentence("Leetcode is cool"))
+print(s.decrypt([5,7,1,4],3))
+print(s.decrypt([2,4,9,3],-2))
