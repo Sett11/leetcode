03d895499a48704a87f70b3d54d9791fac7a0a39
Jhonny <gonzodzen@mail.ru>
2023-09-10 10:31:08 +0300

    Sum of Digits in Base K

--- my.py
+++ my.py
@@ -1,17 +1,16 @@
 class Solution:
-    def countSymmetricIntegers(self,l,r):
-        c=0
-        i=l
-        while i<=r:
-            if i==101:
-                i=1001
-            j=str(i)
-            l=len(j)//2
-            if sum([int(k) for k in j[l:]])==sum([int(k) for k in j[:l]]):
-                c+=1
-            i+=1
-        return c
+    def sumBase(self,n,k):
+        i=0
+        while n:
+            if n//k:
+                i+=n%k
+                n//=k
+            else:
+                i+=n
+                n=0
+        return i
+
     
 s=Solution()
 
-print(s.countSymmetricIntegers(1,100))
+print(s.sumBase(34,6))
