03c5638333f2f6d97e401385c520d675c45a7576
Jhonny <gonzodzen@mail.ru>
2023-09-10 18:03:04 +0300

    Circular Sentence

--- my.py
+++ my.py
@@ -1,21 +1,12 @@
 class Solution:
-    def alertNames(self,a,b):
-        r=[[a[i],int(b[i][:2]+b[i][3:])] for i in range(len(a))]
-        o={i:[] for i in set(a)}
-        res=[]
-        for i in r:
-            o[i[0]].append(i[1])
-        for i in o:
-            o[i].sort()
-            for j in range(len(o[i])-2):
-                if o[i][j+2]-o[i][j]<=100:
-                    res.append(i)
-                    break
-        return sorted(set(res))
-
-
+    def isCircularSentence(self,s):
+        a=s.split(' ')
+        for i in range(len(a)-1):
+            if a[i][-1]!=a[i+1][0]:
+                return False
+        return s[0]==s[-1]
+    
 s=Solution()
 
-print(s.alertNames(["a","a","a","a","b","b","b","b","b","b","c","c","c","c"],["01:35","08:43","20:49","00:01","17:44","02:50","18:48","22:27","14:12","18:00","12:38","20:40","03:59","22:24"]))
-
-print(s.alertNames(["a","a","a","a","a","b","b","b","b","b","b"],["23:20","11:09","23:30","23:02","15:28","22:57","23:40","03:43","21:55","20:38","00:19"]))
+print(s.isCircularSentence("leetcode exercises sound delightful"))
+print(s.isCircularSentence("Leetcode is cool"))
