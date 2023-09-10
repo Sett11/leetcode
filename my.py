a4d71df3973f77c7945f99f736731739cf7ac950
Jhonny <gonzodzen@mail.ru>
2023-09-10 19:41:47 +0300

    Non-decreasing Subsequences

--- my.py
+++ my.py
@@ -1,20 +1,14 @@
+from itertools import combinations
+
 class Solution:
-    def decrypt(self,a,k):
-        if not k:
-            return [0]*len(a)
-        b=a+a+a
-        l=len(a)
+    def findSubsequences(self,a):
         r=[]
-        for i in range(l,l+l):
-            if k>0:
-                r.append(sum(b[i:i+k]))
-            else:
-                r.append(sum(b[i+k:i]))
-        if k>0:
-            r.append(r.pop(0))
-        return r
+        for j in range(2,len(a)):
+            r.extend([list(i) for i in combinations(a,j)])
+        r.append(a)
+        return list(filter(lambda e:e==sorted(e) and len(e)>1,[[int(h) for h in k.split('&')] for k in set(['&'.join([str(j) for j in i]) for i in r])]))
     
 s=Solution()
 
-print(s.decrypt([5,7,1,4],3))
-print(s.decrypt([2,4,9,3],-2))
+print(s.findSubsequences([4,6,7,7]))
+print(s.findSubsequences([4,4,3,2,1]))
