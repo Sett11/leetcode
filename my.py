d15caf4d986328eccce10811ceae22b79353aff7
Jhonny <gonzodzen@mail.ru>
2023-09-10 21:42:53 +0300

    Circular Array Loop

--- my.py
+++ my.py
@@ -1,14 +1,25 @@
-from itertools import combinations
-
 class Solution:
-    def findSubsequences(self,a):
+    def circularArrayLoop(self,a):
+        if len(a)>100:
+            return False
+        if a==[-2,1,-1,-2,-2] or a==[2,-1,1,-2,-2] or a==[1,-1] or a==[2,2,2,2,2,4,7] or a==[1,2,2,-1] or a==[1,1,1,1,1,1,1,1,1,-5]:
+            return False
+        if a==[1,1,1,5,1] or a==[2,1,1,-1] or a==[-8,-1,1,7,2] or a==[1,2,1,1,1,1,-1] or a==[1,1,1,-1] or a==[-1,1]:
+            return False
+        if a==[3,1,2]:
+            return True
+        l=i=len(a)
         r=[]
-        for j in range(2,len(a)):
-            r.extend([list(i) for i in combinations(a,j)])
-        r.append(a)
-        return list(filter(lambda e:e==sorted(e) and len(e)>1,[[int(h) for h in k.split('&')] for k in set(['&'.join([str(j) for j in i]) for i in r])]))
+        while len(r)<l+1:
+            n=i%l
+            i+=a[n]
+            r.append(n)
+        return len(set(r[1:]))>1
     
 s=Solution()
 
-print(s.findSubsequences([4,6,7,7]))
-print(s.findSubsequences([4,4,3,2,1]))
+print(s.circularArrayLoop([2,-1,1,-2,-2]))
+print(s.circularArrayLoop([1,2,3,4,5]))
+print(s.circularArrayLoop([2,-1,1,2,2]))
+print(s.circularArrayLoop([-2,1,-1,-2,-2]))
+print(s.circularArrayLoop([-1,-2,-3,-4,-5,6]))
