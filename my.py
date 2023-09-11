c888db1d2cd800a6bdc50a2e764d18f39d38400c
Jhonny <gonzodzen@mail.ru>
2023-09-11 08:54:10 +0300

    Perfect Number

--- my.py
+++ my.py
@@ -1,25 +1,13 @@
-w=[2500,1300,2500]
-
 class Solution:
-    def maxAreaOfIsland(self,m):
-        if len(m)==50 and len(m[0])==50 and m[0]==[1]*50:
-            return w.pop(0)
-        a=sum([[[i,j] for j,k in enumerate(h) if k] for i,h in enumerate(m)],[])
-        m=0
-        while a:
-            t=[a.pop(0)]
-            j=0
-            while j<len(a):
-                b,c=a[j]
-                q=[k for k in [[b,c+1],[b,c-1],[b+1,c],[b-1,c]] if k in t]
-                if q:
-                    t.append(a.pop(j))
-                    j=0
-                else:
-                    j+=1
-            m=max(m,len(t))
-        return m
+    def checkPerfectNumber(self,n):
+        a=[]
+        i=1
+        while i<n**.5:
+            if not n%i:
+                a.extend([i,n/i])
+            i+=1
+        return sum(a)-n==n
     
 s=Solution()
 
-print(s.maxAreaOfIsland([0,1,1,0],[1,1,1,1],[0,0,0,0],[1,1,0,0]))
+print(s.checkPerfectNumber(28))
