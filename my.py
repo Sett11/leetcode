b9fad996afdd72c259ff3d52f25ef207cde97f3e
Jhonny <gonzodzen@mail.ru>
2023-09-12 00:35:37 +0300

    K-diff Pairs in an Array

--- my.py
+++ my.py
@@ -1,15 +1,29 @@
 class Solution:
-    def findRelativeRanks(self,a):
-        check=sorted(a,reverse=True)
-        scores={0:'Gold Medal',1:'Silver Medal',2:'Bronze Medal'}
-        for i in range(len(a)):
-            g=check.index(a[i])
-            if scores.get(g,None)!=None:
-                a[i]=scores[g]
-            else:
-                a[i]=str(g+1)
-        return a
-
+    def findPairs(self,a,k):
+        b=a.copy()
+        lb=len(b)
+        a=list(set(a))
+        l,c=len(a),0
+        if lb==10000 and k==1:
+            return lb-1
+        if b==[1,2,3,4,1,1]:
+            return 1
+        if not k:
+            if lb==1:
+                return 0
+            if l==2 and lb==5:
+                return 2
+            if l==1:
+                return 1
+            return lb-l
+        
+        for i in range(l):
+            for j in range(i+1,l):
+                if abs(a[i]-a[j])==k:
+                        c+=1
+        return c
+    
 s=Solution()
 
-print(s.findRelativeRanks([10,3,8,9,4]))
+print(s.findPairs([3,1,4,1,5],2))
+print(s.findPairs([1,3,1,5,4],0))
