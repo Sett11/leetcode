4885e65003f88c53bbf3e1c80820b9f939edb83d
Jhonny <gonzodzen@mail.ru>
2023-09-12 00:42:14 +0300

    Count Equal and Divisible Pairs in an Array

--- my.py
+++ my.py
@@ -1,13 +1,13 @@
 class Solution:
-    def countKDifference(self,a,k):
+    def countPairs(self,a,k):
         l=len(a)
         c=0
         for i in range(l):
-            for j in range(i+1,len(a)):
-                if abs(a[i]-a[j])==k:
+            for j in range(i+1,l):
+                if a[i]==a[j] and not i*j%k:
                     c+=1
         return c
     
 s=Solution()
 
-print(s.countKDifference([1,2,2,1],1))
+print(s.countPairs([3,1,2,2,2,1,3],2))
