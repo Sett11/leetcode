9309dee5c6c29e7d7776f36b4e34e414e0babbaa
Jhonny <gonzodzen@mail.ru>
2023-09-11 20:42:12 +0300

    Keyboard Row

--- my.py
+++ my.py
@@ -1,14 +1,8 @@
-from itertools import permutations as p
-
 class Solution:
-    def nextGreaterElement(self,n):
-        if n in [2147483486,1999999999,2147483647]:
-            return -1
-        r=sorted(set([int(''.join(i)) for i in p(str(n))]))
-        g=r.index(n)
-        return -1 if g==len(r)-1 or r[g+1]==n else r[g+1]
-    
+    def findWords(self,w):
+        a,b,c='qwertyuiop','asdfghjkl','zxcvbnm'
+        return [i for i in w if all(j.lower() in a for j in i) or all(j.lower() in b for j in i) or all(j.lower() in c for j in i)]
+
 s=Solution()
 
-print(s.nextGreaterElement(2167))
-print(s.nextGreaterElement(101))
+print(s.findWords(["Hello","Alaska","Dad","Peace"]))
