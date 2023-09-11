2bbedd32defb4215b64f1748daaf995ad1d92c27
Jhonny <gonzodzen@mail.ru>
2023-09-11 18:59:37 +0300

    Next Greater Element I

--- my.py
+++ my.py
@@ -1,7 +1,26 @@
 class Solution:
-    def digitCount(self,s):
-        return all(s.count(str(i))==int(s[i]) for i in range(len(s)))
-    
+    def nextGreaterElement(self,a,b):
+        r=[]
+        for i in a:
+            t=b.index(i)
+            if t!=len(b)-1:
+                q=b[t+1:]
+                m=i
+                j=0
+                while j<len(q):
+                    if q[j]>i:
+                        m=q[j]
+                        break
+                    j+=1
+                if m>i:
+                    r.append(m)
+                else:
+                    r.append(-1)
+            else:
+                r.append(-1)
+        return r
+
 s=Solution()
 
-print(s.digitCount('1210'))
+print(s.nextGreaterElement([4,1,2],[1,3,4,2]))
+print(s.nextGreaterElement([2,4],[1,2,3,4]))
