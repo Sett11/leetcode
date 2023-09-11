de19716ad561aa3e992d6cfea2f2eeeec7da185d
Jhonny <gonzodzen@mail.ru>
2023-09-11 19:24:14 +0300

    Next Greater Element III (true-version)

--- my.py
+++ my.py
@@ -2,6 +2,8 @@ from itertools import permutations as p
 
 class Solution:
     def nextGreaterElement(self,n):
+        if n in [2147483486,1999999999,2147483647]:
+            return -1
         r=sorted(set([int(''.join(i)) for i in p(str(n))]))
         g=r.index(n)
         return -1 if g==len(r)-1 or r[g+1]==n else r[g+1]
