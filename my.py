f7d877fdb2599abde58a5a4cce6b382a6cde9184
Jhonny <gonzodzen@mail.ru>
2023-09-12 20:37:37 +0300

    Random Flip Matrix

--- my.py
+++ my.py
@@ -1,9 +1,29 @@
-class Solution:
-    def smallerNumbersThanCurrent(self,a):
-        b=sorted(a)
-        return [b.index(i) for i in a]
-    
-s=Solution()
+from random import randint
 
-print(s.smallerNumbersThanCurrent([8,1,2,2,3]))
-print(s.smallerNumbersThanCurrent([6,5,4,8]))
+class Solution:
+    def __init__(self,m,n):
+        self.m=m
+        self.n=n
+        self.s=set()
+
+    def flip(self):
+        t=[randint(0,self.m-1),randint(0,self.n-1)]
+        c='&'.join([str(i) for i in t])
+        while c in self.s:
+            t=[randint(0,self.m-1),randint(0,self.n-1)]
+            c='&'.join([str(i) for i in t])
+        self.s.add(c)
+        return t
+
+    def reset(self):
+        self.s=set()
+    
+s=Solution(3,1)
+
+print(s.flip())
+print(s.flip())
+s.reset()
+
+print(s.flip())
+print(s.flip())
+print(s.flip())
