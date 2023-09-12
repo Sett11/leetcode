2a06855fd700730bbd8eac0d1a297112db4d1c80
Jhonny <gonzodzen@mail.ru>
2023-09-12 18:34:40 +0300

    Reverse Pairs

--- my.py
+++ my.py
@@ -1,8 +1,16 @@
+from bisect import insort,bisect_left
+
 class Solution:
-    def reverseWords(self,s):
-        return ' '.join([i[::-1] for i in s.split(' ')])
+    def reversePairs(self,a):
+        c=0
+        t=[a[-1]*2]
+        for i in range(len(a)-2,-1,-1):
+            n=a[i]
+            c+=bisect_left(t,n)
+            insort(t,n*2)
+        return c
     
 s=Solution()
 
-print(s.reverseWords("Let's take LeetCode contest"))
-print(s.reverseWords('God Ding'))
+print(s.reversePairs([1,3,2,3,1]))
+print(s.reversePairs([2,4,3,5,1]))
