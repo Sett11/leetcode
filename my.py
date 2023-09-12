a10059ebf07521f9ad50a02e5b748231fac8fa70
Jhonny <gonzodzen@mail.ru>
2023-09-13 00:33:17 +0300

    Convert 1D Array Into 2D Array

--- my.py
+++ my.py
@@ -1,17 +1,16 @@
 class Solution:
-    def matrixReshape(self,a,m,n):
-        b=sum(a,[])
-        if len(b)!=m*n:
-            return a
+    def construct2DArray(self,a,m,n):
         r=[]
-        while b:
+        if len(a)!=m*n:
+            return r
+        while a:
             t=[]
             while len(t)<n:
-                t.append(b.pop(0))
+                t.append(a.pop(0))
             r.append(t)
         return r
     
 s=Solution()
 
-print(s.matrixReshape([[1,2],[3,4],[5,6],[7,8]],2,4))
-print(s.matrixReshape([[1,2],[3,4]],1,4))
+print(s.construct2DArray([1,2,3,4,5,6,7,8],2,4))
+print(s.construct2DArray([1,2,3,4],2,2))
