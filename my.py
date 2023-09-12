b632480274ee2b1162c5defea19e586adcbd485c
Jhonny <gonzodzen@mail.ru>
2023-09-12 19:19:01 +0300

    How Many Numbers Are Smaller Than the Current Number

--- my.py
+++ my.py
@@ -1,16 +1,9 @@
-from bisect import insort,bisect_left
-
 class Solution:
-    def reversePairs(self,a):
-        c=0
-        t=[a[-1]*2]
-        for i in range(len(a)-2,-1,-1):
-            n=a[i]
-            c+=bisect_left(t,n)
-            insort(t,n*2)
-        return c
+    def smallerNumbersThanCurrent(self,a):
+        b=sorted(a)
+        return [b.index(i) for i in a]
     
 s=Solution()
 
-print(s.reversePairs([1,3,2,3,1]))
-print(s.reversePairs([2,4,3,5,1]))
+print(s.smallerNumbersThanCurrent([8,1,2,2,3]))
+print(s.smallerNumbersThanCurrent([6,5,4,8]))
