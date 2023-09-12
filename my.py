5936c33dfe7f02299a3f60050c21beb0a055af3f
Jhonny <gonzodzen@mail.ru>
2023-09-13 00:00:11 +0300

    Distinct Prime Factors of Product of Array

--- my.py
+++ my.py
@@ -1,29 +1,19 @@
-def d(n):
-    a=[]
-    for i in range(2,int(n**.5)+1):
-        if n%i==0:
-            a.extend([i,n//i])
-    return sorted(a)
+from functools import reduce as r
 
-def f(a,k):
-    m=int(1e9)
-    o={}
-    o[m]=m
-    for i in range(len(a)):
-        for j in range(i+1,len(a)):
-            if a[i]*a[j]==k:
-                m=min(abs(a[i]-a[j]),m)
-                o[m]=[a[i],a[j],[m]]
-    return o[m] if m!=1e9 else [0,0,[int(1e9)]]
+def d(n):
+    i=2
+    s=set()
+    while i<n*n:
+        while n%i==0:
+            n//=i
+            s.add(i)
+        i+=1
+    return list(s)
 
 class Solution:
-    def closestDivisors(self,n):
-        o={1:[1,2],2:[1,3],3:[2,2],4:[2,3],5:[2,3],6:[2,4]}
-        if o.get(n):
-            return o[n]
-        c,q=f(d(n+1),n+1),f(d(n+2),n+2)
-        return c[:2] if c[2][0]<q[2][0] else q[:2]
+    def distinctPrimeFactors(self,a):
+        return len(d(r(lambda e,c:e*c,a)))
     
 s=Solution()
 
-print(s.closestDivisors(9))
+print(s.distinctPrimeFactors([i for i in range(1,10000)]))
