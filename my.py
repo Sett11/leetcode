158735e87bbf61bc23c62707c9b86a482c4492bb
Jhonny <gonzodzen@mail.ru>
2023-09-14 00:10:32 +0300

    Set Mismatch

--- my.py
+++ my.py
@@ -1,13 +1,24 @@
 class Solution:
-    def findAndReplacePattern(self, a, t):
-        r = []
+    def findErrorNums(self,a):
+        t=k=0
+
         for i in a:
-            if len(i) == len(t) and all(i.count(i[j]) == t.count(t[j]) and i.index(i[j]) == t.index(t[j]) for j in range(len(t))):
-                r.append(i)
-        return r
+            if a.count(i)>1:
+                if i>0:
+                    t=i
+                    break
+                
+        for i in range(1,max(a)+2):
+            if i not in a:
+                k=i
+                break
+
+        return [t,k]
 
 
-s = Solution()
+s=Solution()
 
-print(s.findAndReplacePattern(
-    ["abc", "deq", "mee", "aqq", "dkd", "ccc"], 'add'))
+print(s.findErrorNums([1,2,2,4]))
+print(s.findErrorNums([1,1]))
+print(s.findErrorNums([2,2]))
+print(s.findErrorNums([1,2,3,4,5,6,7,8,9,10,11,11,13,14,15,16,17,18,19,20]))
