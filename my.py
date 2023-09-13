7cd9aada05f493546da8caf97b4b060fe8fec9f2
Jhonny <gonzodzen@mail.ru>
2023-09-13 18:10:18 +0300

    Shuffle the Array

--- my.py
+++ my.py
@@ -1,20 +1,10 @@
 class Solution:
-    def findUnsortedSubarray(self,a):
-        b=sorted(a)
-        if a==b:
-            return 0
-        for i in range(len(a)):
-            if a[i]!=b[i]:
-                for j in range(i,len(a)+1):
-                    if j==len(a):
-                        return j-i
-                    if a[j:]==b[j:]:
-                        return j-i
-        return len(a)
-
+    def shuffle(self,a,n):
+        b,c,r=a[:n],a[n:],[]
+        for i in range(n):
+            r.extend([b[i],c[i]])
+        return r
+    
 s=Solution()
 
-print(s.findUnsortedSubarray([2,6,4,8,10,9,15]))
-print(s.findUnsortedSubarray([2,3,3,2,4]))
-print(s.findUnsortedSubarray([1,3,2,2,2]))
-print(s.findUnsortedSubarray([1,2]))
+print(s.shuffle([2,5,1,3,4,7],3))
