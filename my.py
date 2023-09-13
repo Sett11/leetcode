aedb725f4044434699873ceea0485ce7223ec059
Jhonny <gonzodzen@mail.ru>
2023-09-13 18:56:28 +0300

    Check if Numbers Are Ascending in a Sentence

--- my.py
+++ my.py
@@ -1,11 +1,9 @@
+from re import sub
 class Solution:
-    def checkString(self,s):
-        return 'b' not in s or 'a' not in s[s.index('b'):]
+    def areNumbersAscending(self,s):
+        return eval('<'.join([i for i in sub(r'\D',' ',s).split(' ') if i]))
     
 s=Solution()
 
-print(s.checkString('aaabbb'))
-print(s.checkString('abab'))
-print(s.checkString('aaa'))
-print(s.checkString('bbb'))
-print(s.checkString(''))
+print(s.areNumbersAscending("1 box has 3 blue 4 red 6 green and 12 yellow marbles"))
+print(s.areNumbersAscending("hello world 5 x 5"))
