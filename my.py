8b8164541a502bb8d30bff216500d13b94663708
Jhonny <gonzodzen@mail.ru>
2023-09-13 20:26:59 +0300

    Largest Number After Digit Swaps by Parity

--- my.py
+++ my.py
@@ -1,7 +1,11 @@
 class Solution:
-    def getCommon(self,a,b):
-        return min(set(a).intersection(set(b)),default=-1)
+    def largestInteger(self,n):
+        s=str(n)
+        odd=sorted([i for i in s if not int(i)&1],reverse=True)
+        even=sorted([i for i in s if int(i)&1],reverse=True)
+        return int(''.join([odd.pop(0) if i==0 else even.pop(0) for i in [int(i)&1 for i in s]]))
     
 s=Solution()
 
-print(s.getCommon([1,2,3],[2,4]))
+print(s.largestInteger(65875))
+print(s.largestInteger(1234))
