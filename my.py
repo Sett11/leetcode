c2a28d1548704d59a2483456a86c2360e57cfaf8
Jhonny <gonzodzen@mail.ru>
2023-09-13 19:11:24 +0300

    Keep Multiplying Found Values by Two

--- my.py
+++ my.py
@@ -1,14 +1,9 @@
 class Solution:
-    def checkIfExist(self,a):
-        l=len(a)
-        for i in range(l):
-            t=a[i]*2
-            if t in a and a.index(t)!=i:
-                return True
-        return False
+    def findFinalValue(self,a,n):
+        while n in a:
+            n*=2
+        return n
     
 s=Solution()
 
-print(s.checkIfExist([10,2,5,3]))
-print(s.checkIfExist([3,1,7,11]))
-print(s.checkIfExist([-2,0,10,-19,4,6,-8]))
+print(s.findFinalValue([5,3,6,1,12],3))
