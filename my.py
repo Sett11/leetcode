05000f9df13d0c135fbca3c814f4ddb44733bf35
Jhonny <gonzodzen@mail.ru>
2023-09-13 21:43:34 +0300

    String to Integer (atoi)

--- my.py
+++ my.py
@@ -1,10 +1,24 @@
 class Solution:
-    def pivotArray(self,a,n):
-        b,c,d=[],[],[]
-        [b.append(i) if i<n else c.append(i) if i==n else d.append(i) for i in a]
-        return b+c+d
-    
-s=Solution()
+    def myAtoi(self, s):
+        c = ''
+        a = [-2**31, 2147483647]
+        v = False
+        for i in s.lstrip():
+            if (i not in ['-', '+'] and not i.isdigit()) or (not i.isdigit() and v):
+                break
+            c += i
+            v = True
+        try:
+            n = int(c)
+            return a[0] if n < a[0] else a[1] if n > a[1] else n
+        except:
+            return 0
 
-print(s.pivotArray([9,12,5,10,14,3,10],10))
-print(s.pivotArray([-3,4,3,2],2))
+
+s = Solution()
+
+print(s.myAtoi("4193 with words"))
+print(s.myAtoi("-419"))
+print(s.myAtoi("    with words 4193 "))
+print(s.myAtoi("+1"))
+print(s.myAtoi("-5-"))
