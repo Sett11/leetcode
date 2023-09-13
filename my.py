1feb06d1febc23218cf04132c64e759d2b8b7670
Jhonny <gonzodzen@mail.ru>
2023-09-14 00:16:14 +0300

    Palindromic Substrings

--- my.py
+++ my.py
@@ -1,24 +1,14 @@
 class Solution:
-    def findErrorNums(self,a):
-        t=k=0
-
-        for i in a:
-            if a.count(i)>1:
-                if i>0:
-                    t=i
-                    break
-                
-        for i in range(1,max(a)+2):
-            if i not in a:
-                k=i
-                break
-
-        return [t,k]
-
-
+    def countSubstrings(self,s):
+        l=len(s)
+        c=0
+        for i in range(l):
+            for j in range(i+1,l+1):
+                if s[i:j]==s[i:j][::-1]:
+                    c+=1
+        return c
+    
 s=Solution()
 
-print(s.findErrorNums([1,2,2,4]))
-print(s.findErrorNums([1,1]))
-print(s.findErrorNums([2,2]))
-print(s.findErrorNums([1,2,3,4,5,6,7,8,9,10,11,11,13,14,15,16,17,18,19,20]))
+print(s.countSubstrings('abc'))
+print(s.countSubstrings('aaa'))
