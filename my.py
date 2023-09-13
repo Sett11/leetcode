afecf3e25ef21ed35bf0a25fa000325d9ffddc5e
Jhonny <gonzodzen@mail.ru>
2023-09-13 20:38:52 +0300

    Sort Array By Parity

--- my.py
+++ my.py
@@ -1,11 +1,9 @@
 class Solution:
-    def largestInteger(self,n):
-        s=str(n)
-        odd=sorted([i for i in s if not int(i)&1],reverse=True)
-        even=sorted([i for i in s if int(i)&1],reverse=True)
-        return int(''.join([odd.pop(0) if i==0 else even.pop(0) for i in [int(i)&1 for i in s]]))
+    def sortArrayByParity(self,a):
+        b,c=[],[]
+        [c.append(i) if i&1 else b.append(i) for i in a]
+        return b+c
     
 s=Solution()
 
-print(s.largestInteger(65875))
-print(s.largestInteger(1234))
+print(s.sortArrayByParity([3,1,2,4]))
