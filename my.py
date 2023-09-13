69ef1382ab81cab6360ff3d8cab10d11cd5b2e76
Jhonny <gonzodzen@mail.ru>
2023-09-13 18:46:59 +0300

    Check if Array Is Sorted and Rotated

--- my.py
+++ my.py
@@ -1,10 +1,12 @@
 class Solution:
-    def shuffle(self,a,n):
-        b,c,r=a[:n],a[n:],[]
-        for i in range(n):
-            r.extend([b[i],c[i]])
-        return r
+    def check(self,a):
+        b=sorted(a)
+        for i in range(len(a)):
+            if a[i:]+a[:i]==b:
+                return True
+        return False
     
 s=Solution()
 
-print(s.shuffle([2,5,1,3,4,7],3))
+print(s.check([2,5,1,3,4,7]))
+print(s.check([3,4,5,1,2]))
