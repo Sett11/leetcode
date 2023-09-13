0e0f3fcea4a37f491497cba900f5a6665aa520e3
Jhonny <gonzodzen@mail.ru>
2023-09-13 21:58:34 +0300

    Jewels and Stones

--- my.py
+++ my.py
@@ -1,24 +1,10 @@
 class Solution:
-    def myAtoi(self, s):
-        c = ''
-        a = [-2**31, 2147483647]
-        v = False
-        for i in s.lstrip():
-            if (i not in ['-', '+'] and not i.isdigit()) or (not i.isdigit() and v):
-                break
-            c += i
-            v = True
-        try:
-            n = int(c)
-            return a[0] if n < a[0] else a[1] if n > a[1] else n
-        except:
-            return 0
+    def numJewelsInStones(self,s,t):
+        c=0
+        for i in s:
+            c+=t.count(i)
+        return c
+    
+s=Solution()
 
-
-s = Solution()
-
-print(s.myAtoi("4193 with words"))
-print(s.myAtoi("-419"))
-print(s.myAtoi("    with words 4193 "))
-print(s.myAtoi("+1"))
-print(s.myAtoi("-5-"))
+print(s.numJewelsInStones("aA","aAAbbbb"))
