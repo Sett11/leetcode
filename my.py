0c23c7ace836c354c582192826193af77eb0e4e6
Jhonny <gonzodzen@mail.ru>
2023-09-13 21:01:37 +0300

    Rearrange Array Elements by Sign

--- my.py
+++ my.py
@@ -1,11 +1,11 @@
 class Solution:
-    def sortArrayByParityII(self,a):
+    def rearrangeArray(self,a):
         b,c,r=[],[],[]
-        [c.append(a[i]) if a[i]&1 else b.append(a[i]) for i in range(len(a))]
+        [c.append(a[i]) if a[i]<0 else b.append(a[i]) for i in range(len(a))]
         [r.extend([b[i],c[i]]) for i in range(len(b))]
         return r
     
 s=Solution()
 
-print(s.sortArrayByParityII([4,2,5,7]))
-print(s.sortArrayByParityII([36,45,32,31,15,41,9,46,36,6,15,16,33,26,27,31,44,34]))
+print(s.rearrangeArray([3,1,-2,-5,2,-4]))
+print(s.rearrangeArray([-1,1]))
