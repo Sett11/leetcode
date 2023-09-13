136c7824f182f6c8c580c982828f96f97d7ac320
Jhonny <gonzodzen@mail.ru>
2023-09-13 22:11:51 +0300

    Number of Senior Citizens

--- my.py
+++ my.py
@@ -1,11 +1,11 @@
-shift=lambda e,c:chr(ord(e)+c)
-
 class Solution:
-    def replaceDigits(self,s):
-        l=len(s)
-        return ''.join([s[i-1]+shift(s[i-1],int(s[i])) for i in range(1,l,2)])+(s[-1] if l&1 else '')
+    def countSeniors(self,a):
+        c=0
+        for i in a:
+            if int(i[11:13])>60:
+                c+=1
+        return c
     
 s=Solution()
 
-print(s.replaceDigits('a1c1e1'))
-print(s.replaceDigits('a1b2c3d4e'))
+print(s.countSeniors(["7868190130M7522","5303914400F9211","9273338290F4010"]))
