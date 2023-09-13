95a52af7447ddbf4c2e479f78902e588ec239bb6
Jhonny <gonzodzen@mail.ru>
2023-09-13 20:58:15 +0300

    Sort Array By Parity II

--- my.py
+++ my.py
@@ -1,17 +1,11 @@
 class Solution:
-    def sortEvenOdd(self,a):
+    def sortArrayByParityII(self,a):
         b,c,r=[],[],[]
-        for i in range(len(a)):
-            if i&1:
-                c.append(a[i])
-                r.append('even')
-            else:
-                b.append(a[i])
-                r.append('odd')
-        b,c=sorted(b),sorted(c,reverse=True)
-        return [b.pop(0) if i=='odd' else c.pop(0) for i in r]
+        [c.append(a[i]) if a[i]&1 else b.append(a[i]) for i in range(len(a))]
+        [r.extend([b[i],c[i]]) for i in range(len(b))]
+        return r
     
 s=Solution()
 
-print(s.sortEvenOdd([4,1,2,3]))
-print(s.sortEvenOdd([36,45,32,31,15,41,9,46,36,6,15,16,33,26,27,31,44,34]))
+print(s.sortArrayByParityII([4,2,5,7]))
+print(s.sortArrayByParityII([36,45,32,31,15,41,9,46,36,6,15,16,33,26,27,31,44,34]))
