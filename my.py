f11bfe3024967dc5a6941b35cb266a39c47eb146
Jhonny <gonzodzen@mail.ru>
2023-09-13 18:01:54 +0300

    Shortest Unsorted Continuous Subarray

--- my.py
+++ my.py
@@ -1,20 +1,20 @@
 class Solution:
-    def minimumAbsDifference(self,a):
-        a.sort()
-        m=int(1e9)
-        o={}
-        for i in range(0,len(a)-1,1):
-            q=[a[i],a[i+1]]
-            s=abs(a[i]-a[i+1])
-            m=min(s,m)
-            if s in o:
-                o[s].append(q)
-            else:
-                o[s]=[q]
-        return o[m]
+    def findUnsortedSubarray(self,a):
+        b=sorted(a)
+        if a==b:
+            return 0
+        for i in range(len(a)):
+            if a[i]!=b[i]:
+                for j in range(i,len(a)+1):
+                    if j==len(a):
+                        return j-i
+                    if a[j:]==b[j:]:
+                        return j-i
+        return len(a)
 
 s=Solution()
 
-print(s.minimumAbsDifference([4,2,1,3]))
-print(s.minimumAbsDifference([3,8,-10,23,19,-4,-14,27]))
-print(s.minimumAbsDifference([1,3,6,10,15]))
+print(s.findUnsortedSubarray([2,6,4,8,10,9,15]))
+print(s.findUnsortedSubarray([2,3,3,2,4]))
+print(s.findUnsortedSubarray([1,3,2,2,2]))
+print(s.findUnsortedSubarray([1,2]))
