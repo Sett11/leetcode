47ea10d3043756338dda0bb4a639ecfffd6c882e
Jhonny <gonzodzen@mail.ru>
2023-09-14 16:14:00 +0300

    Non-decreasing Array

--- my.py
+++ my.py
@@ -1,20 +1,18 @@
 class Solution:
-    def imageSmoother(self,a):
-        m,n=len(a),len(a[0])
-        r=[[0]*n for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                q=[]
-                for k in range(i-1,i+2):
-                    for h in range(j-1,j+2):
-                        if 0<=k<m and 0<=h<n:
-                            q.append(a[k][h])
-                r[i][j]=sum(q)//len(q)
-        return r
+    def checkPossibility(self,a):
+        b=sorted(a)
+        for i in range(len(a)):
+            t=a.pop(i)
+            g=b.index(t)
+            b.pop(g)
+            if a==b:
+                return True
+            else:
+                a.insert(i,t)
+                b.insert(g,t)
+        return False
     
 s=Solution()
 
-print(s.imageSmoother([[100,200,100],
-                       [200,50,200],
-                       [100,200,100]]))
-print(s.imageSmoother([[1,1,1],[1,0,1],[1,1,1]]))
+print(s.checkPossibility([4,2,3]))
+print(s.checkPossibility([-1,4,2,3]))
