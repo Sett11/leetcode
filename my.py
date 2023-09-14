4341ea9cfa770aa3dcc049a1e61ff6f9112aec09
Jhonny <gonzodzen@mail.ru>
2023-09-14 15:59:39 +0300

    Image Smoother

--- my.py
+++ my.py
@@ -1,33 +1,20 @@
-def f(n,k,j,s):
-    c=0
-    for i in range(len(s)):
-        if k<0 or k>=n or j<0 or j>=n:
-            return c-1
-        elif s[i]=='U':
-            k-=1
-            if k<0 or k>=n:
-                return c
-        elif s[i]=='D':
-            k+=1
-            if k<0 or k>=n:
-                return c
-        elif s[i]=='R':
-            j+=1
-            if j<0 or j>=n:
-                return c
-        elif s[i]=='L':
-            j-=1
-            if j<0 or j>=n:
-                return c
-        c+=1
-    return c
 class Solution:
-    def executeInstructions(self,n,p,s):
-        r=[]
-        for i in range(len(s)):
-            r.append(f(n,p[0],p[1],s[i:]))
+    def imageSmoother(self,a):
+        m,n=len(a),len(a[0])
+        r=[[0]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                q=[]
+                for k in range(i-1,i+2):
+                    for h in range(j-1,j+2):
+                        if 0<=k<m and 0<=h<n:
+                            q.append(a[k][h])
+                r[i][j]=sum(q)//len(q)
         return r
     
 s=Solution()
 
-print(s.executeInstructions(3,[0,1],'RRDDLU'))
+print(s.imageSmoother([[100,200,100],
+                       [200,50,200],
+                       [100,200,100]]))
+print(s.imageSmoother([[1,1,1],[1,0,1],[1,1,1]]))
