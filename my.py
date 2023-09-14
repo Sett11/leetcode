89e620170464810863219a5cb0e3f66f2007fe0c
Jhonny <gonzodzen@mail.ru>
2023-09-14 11:02:53 +0300

    Robot Return to Origin

--- my.py
+++ my.py
@@ -1,18 +1,8 @@
-from re import sub
+class Solution:
+    def judgeCircle(self,s):
+        return s.count('U')==s.count('D') and s.count('L')==s.count('R')
+    
+s=Solution()
 
-class NestedIterator:
-    def __init__(self,a):
-        self.a=[i for i in sub(r'[^\d -]','',str(a)).split(' ') if i]
-    
-    def next(self):
-        return int(self.a.pop(0))
-    
-    def hasNext(self):
-         return bool(self.a)
-    
-N=NestedIterator([[1,-1],2,[1,[-1]]])
-
-print(N.next())
-print(N.next())
-print(N.next())
-print(N.hasNext())
+print(s.judgeCircle("UDRLRL"))
+print(s.judgeCircle("LL"))
