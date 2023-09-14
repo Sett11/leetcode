a18ead77365fd9642a5199e1b15377f6fc8c3fcd
Jhonny <gonzodzen@mail.ru>
2023-09-14 11:37:49 +0300

    Special Positions in a Binary Matrix

--- my.py
+++ my.py
@@ -1,8 +1,18 @@
 class Solution:
-    def judgeCircle(self,s):
-        return s.count('U')==s.count('D') and s.count('L')==s.count('R')
+    def numSpecial(self,a):
+        c=0
+        for i in range(len(a)):
+            for j in range(len(a[0])):
+                if a[i][j]==1:
+                    a[i][j]=0
+                    if 1 not in a[i]+[list(k) for k in zip(*a)][j]:
+                        c+=1
+                    a[i][j]=1
+        return c
     
 s=Solution()
 
-print(s.judgeCircle("UDRLRL"))
-print(s.judgeCircle("LL"))
+print(s.numSpecial([[1,0,0]
+                    ,[0,0,1]
+                    ,[1,0,0]]))
+print(s.numSpecial([[1,0,0],[0,1,0],[0,0,1]]))
