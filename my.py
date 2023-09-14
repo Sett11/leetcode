5f618316a4fa614abc3934e274b4272488bc8419
Jhonny <gonzodzen@mail.ru>
2023-09-15 00:02:05 +0300

    Design HashSet

--- my.py
+++ my.py
@@ -1,9 +1,13 @@
-class Solution:
-    def findThePrefixCommonArray(self,a,b):
-        return [len(set(a[:i]).intersection(set(b[:i]))) for i in range(1,len(a)+1)]
+class MyHashSet:
+    def __init__(self):
+        self.hash=set()
 
-    
-s=Solution()
+    def add(self,k):
+        self.hash.add(k)
 
-print(s.findThePrefixCommonArray([1,3,2,4],[3,1,2,4]))
-print(s.findThePrefixCommonArray([2,3,1],[3,1,2]))
+    def remove(self,k):
+        if k in self.hash:
+            self.hash.remove(k)
+
+    def contains(self,k):
+        return k in self.hash
