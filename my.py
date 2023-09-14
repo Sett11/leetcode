b481367f47ea792d1f950bd4b37d965553bfa3df
Jhonny <gonzodzen@mail.ru>
2023-09-14 13:41:32 +0300

    Longest Mountain in Array

--- my.py
+++ my.py
@@ -1,24 +1,32 @@
 class Solution:
-    def checkValidGrid(self, a):
-        m = len(a)
-        i = j = n = 0
-        while n != m*m-1:
-            t = [k for k in [[i+2, j+1], [i-2, j-1], [i-2, j+1], [i+2, j-1], [i+1, j+2], [i-1, j-2],
-                             [i-1, j+2], [i+1, j-2]] if 0 <= k[0] < m and 0 <= k[1] < m and a[k[0]][k[1]] == n+1]
-            if not t:
-                return False
-            n += 1
-            i, j = t[0]
-        return not a[0][0]
+    def longestMountain(self,a):
+        a.append(a[-1]+1)
+        l=len(a)
+        m=0
+        for i in range(l-1):
+            v=False
+            k=-1
+            if a[i]<a[i+1]:
+                for j in range(i+1,l):
+                    if j==l-1:
+                        if a[j]<=a[j-1]:
+                            m=max(len(a[i:j]),m)
+                        break
+                    if v and a[j]<=a[j+1]:
+                        t=a[i:j+1]
+                        if t.count(a[k])==1:
+                            m=max(m,len(t))
+                        i=j
+                        break
+                    if not v and a[j]>=a[j+1]:
+                        v=True
+                        k=j
+        return m
+    
+s=Solution()
 
-
-s = Solution()
-
-print(s.checkValidGrid([[0, 11, 16, 5, 20],
-                        [17, 4, 19, 10, 15],
-                        [12, 1, 8, 21, 6],
-                        [3, 18, 23, 14, 9],
-                        [24, 13, 2, 7, 22]]))
-print(s.checkValidGrid([[8,3,6],
-                        [5,0,1],
-                        [2,7,4]]))
+print(s.longestMountain([2,1,4,7,3,2,5]))
+print(s.longestMountain([2,3]))
+print(s.longestMountain([0,1,2,3,4,5,4,3,2,1,0]))
+print(s.longestMountain([0,2,0,2,1,2,3,4,4,1]))
+print(s.longestMountain([0,2,0,0,2,0,2,1,1,0,2,1,0,0,1,0,2,1,2,0,1,1,0,2,2,1,2,2,0,0,0,1,0,2,0,0,1,2,0,1,0,2,0,2,0,0,0,0,2,1,0,0,0,0,1,0,2,1,2,2,1,0,0,1,0,2,0,0,0,2,1,0,1,2,1,0,1,0,2,1,0,2,0,2,1,1,2,0,1,0,1,1,1,1,2,1,2,2,2,0]))
