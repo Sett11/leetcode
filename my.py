97b2ae1f52e614221121e4e7bc971434191c1fd1
Jhonny <gonzodzen@mail.ru>
2023-09-14 21:10:09 +0300

    Kth Largest Element in a Stream

--- my.py
+++ my.py
@@ -1,46 +1,18 @@
-class Solution:
-    def canReach(self,s,m,n):
-        if s=='01000110110' and m==2 and n==3:
-            return True
-        if s=='0000010000' and m==2 and n==5:
-            return True
-        if s=='0000000000' and m==2 and n==5:
-            return True
-        if len(set(s))==1 and s[0]=='0' and ((m==5 and n==99998) or (m==3 and n==4)or (m==3 and n==7)):
-            return True
-        if s=='000' and m==2 and n==2:
-            return True
-        if s=='011001110001000' and m==3 and n==5:
-            return True
-        if s=='0110110101011100' and m==3 and n==6:
-            return True
-        if s=='0101001110' and m==2 and n==4:
-            return True
-        if s=='0100100010' and m==3 and n==4:
-            return True
-        if s=='0110010000' and m==3 and n==4:
-            return True
-        if s=='0111000110' and m==2 and n==4:
-            return True
-        if s=='010011110' and m==2 and n==5:
-            return True
-        if s=='010011010' and m==2 and n==3:
-            return True
-        try:
-            i=0
-            while i<len(s):
-                if i>=len(s)-1:
-                    return True
-                g=s.index('0',i+1)
-                if i+m<=g<=min(i+n,len(s)-1):
-                    i=g
-                else:
-                    return False
-        except:
-            return False
-    
-s=Solution()
+import bisect
 
-print(s.canReach('01000110110',2,3))
-print(s.canReach('01101110',2,3))
-print(s.canReach('01',1,1))
+class KthLargest:
+    def __init__(self,k,a):
+        self.k=k
+        self.a=sorted(a)
+
+    def add(self,v):
+        bisect.insort(self.a,v)
+        return self.a[-self.k]
+    
+K=KthLargest(3,[4, 5, 8, 2])
+
+print(K.add(3))
+print(K.add(5))
+print(K.add(10))
+print(K.add(9))
+print(K.add(4))
