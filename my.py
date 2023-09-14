c3329147981fa0d6683053f81e7a62223b105b06
Jhonny <gonzodzen@mail.ru>
2023-09-14 12:45:36 +0300

    Check Knight Tour Configuration

--- my.py
+++ my.py
@@ -1,16 +1,24 @@
 class Solution:
-    def onesMinusZeros(self,a):
-        m,n=len(a),len(a[0])
-        b=[[i.count(0),i.count(1)] for i in zip(*a)]
-        a=[[i.count(0),i.count(1)] for i in a]
-        r=[[0]*n for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                r[i][j]=a[i][1]+b[j][1]-a[i][0]-b[j][0]
-        return r
-    
-s=Solution()
+    def checkValidGrid(self, a):
+        m = len(a)
+        i = j = n = 0
+        while n != m*m-1:
+            t = [k for k in [[i+2, j+1], [i-2, j-1], [i-2, j+1], [i+2, j-1], [i+1, j+2], [i-1, j-2],
+                             [i-1, j+2], [i+1, j-2]] if 0 <= k[0] < m and 0 <= k[1] < m and a[k[0]][k[1]] == n+1]
+            if not t:
+                return False
+            n += 1
+            i, j = t[0]
+        return not a[0][0]
 
-print(s.onesMinusZeros([[0,1,1],
-                        [1,0,1],
-                        [0,0,1]]))
+
+s = Solution()
+
+print(s.checkValidGrid([[0, 11, 16, 5, 20],
+                        [17, 4, 19, 10, 15],
+                        [12, 1, 8, 21, 6],
+                        [3, 18, 23, 14, 9],
+                        [24, 13, 2, 7, 22]]))
+print(s.checkValidGrid([[8,3,6],
+                        [5,0,1],
+                        [2,7,4]]))
