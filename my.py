b5a02768b324ebdabd3c26c778d36a4e09409d15
Jhonny <gonzodzen@mail.ru>
2023-09-15 19:40:58 +0300

    Find the Middle Index in Array

--- my.py
+++ my.py
@@ -1,5 +1,5 @@
 class Solution:
-    def pivotIndex(self,a):
+    def findMiddleIndex(self,a):
         c=0
         for i in range(1,len(a)+1):
             if c==sum(a[i:]):
@@ -9,5 +9,5 @@ class Solution:
         
 s=Solution()
 
-print(s.pivotIndex([1,7,3,6,5,6]))
-print(s.pivotIndex([-1,-1,-1,-1,-1,-1]))
+print(s.findMiddleIndex([1,7,3,6,5,6]))
+print(s.findMiddleIndex([2,3,-1, 8,4 ]))
