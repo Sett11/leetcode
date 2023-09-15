8dd924d86cb92d4b65dd1a3ffe2fb82a4f939102
Jhonny <gonzodzen@mail.ru>
2023-09-15 14:46:19 +0300

    Minimum Index Sum of Two Lists

--- my.py
+++ my.py
@@ -1,9 +1,15 @@
 class Solution:
-    def isOneBitCharacter(self,a):
-        s=''.join([str(i) for i in a[:-1]]).replace('11','').replace('10','').replace('0','')
-        return not s and not a[-1]
+    def findRestaurant(self,a,b):
+        s=list(set(a).intersection(set(b)))
+        m=int(1e9)
+        for i in range(len(s)):
+            t=a.index(s[i])+b.index(s[i])
+            m=min(t,m)
+            s[i]=[s[i]]+[t]
+        return [i[0] for i in s if i[1]==m]
     
 s=Solution()
 
-print(s.isOneBitCharacter([0,0]))
-print(s.isOneBitCharacter([1,1,0,0]))
+print(s.findRestaurant(["Shogun","Tapioca Express","Burger King","KFC"],["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]))
+print(s.findRestaurant(["Shogun","Tapioca Express","Burger King","KFC"],["KFC","Shogun","Burger King"]))
+print(s.findRestaurant(["happy","sad","good"],["sad","happy","good"]))
