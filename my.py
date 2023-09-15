7b8e1f919b2a57ab1512af204ba904278c61e491
Jhonny <gonzodzen@mail.ru>
2023-09-16 00:12:20 +0300

    Partition Array Into Three Parts With Equal Sum

--- my.py
+++ my.py
@@ -1,13 +1,28 @@
 class Solution:
-    def findMiddleIndex(self,a):
-        c=0
-        for i in range(1,len(a)+1):
-            if c==sum(a[i:]):
-                return i-1
-            c+=a[i-1]
-        return -1
+    def canThreePartsEqualSum(self,a):
+        s=sum(a)//3
+        r=[]
+        t=[]
+        i=0
+        while i<=len(a):
+            if len(r)==2 and r[0]==r[1]:
+                if sum(t+a[i:])==r[0]:
+                    return True
+            if t and sum(t)==s:
+                r.append(sum(t))
+                t=[]
+            t.append(a[i])
+            i+=1
+            if i==len(a):
+                if t:
+                    r.append(sum(t))
+                break
+        return len(r)==3 and len(set(r))==1
+
         
 s=Solution()
 
-print(s.findMiddleIndex([1,7,3,6,5,6]))
-print(s.findMiddleIndex([2,3,-1, 8,4 ]))
+print(s.canThreePartsEqualSum([0,2,1,-6,6,-7,9,1,2,0,1]))
+print(s.canThreePartsEqualSum([3,3,6,5,-2,2,5,1,-9,4]))
+print(s.canThreePartsEqualSum([0,2,1,-6,6,7,9,-1,2,0,1]))
+print(s.canThreePartsEqualSum([0,0,0,0]))
