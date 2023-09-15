bd8fc2c71cae39957cfffe3437e20816a9ecf87a
Jhonny <gonzodzen@mail.ru>
2023-09-15 16:17:09 +0300

    Baseball Game

--- my.py
+++ my.py
@@ -1,48 +1,18 @@
-class FrontMiddleBackQueue:
+class Solution:
+    def calPoints(self,a):
+        r=[]
+        for i in a:
+            if i.isdigit() or i[1:].isdigit():
+                r.append(int(i))
+            if i=='+':
+                r.append(r[-1]+r[-2])
+            if i=='C':
+                r.pop()
+            if i=='D':
+                r.append(r[-1]*2)
+        return sum(r)
+    
+s=Solution()
 
-    def __init__(self):
-        self.queue = []
-
-    def pushFront(self, v):
-        self.queue.insert(0, v)
-
-    def pushMiddle(self, v):
-        l = len(self.queue)//2
-        self.queue = self.queue[0:l]+[v]+self.queue[l:]
-        return self.queue
-
-    def pushBack(self, v):
-        self.queue.append(v)
-
-    def popFront(self):
-        try:
-            return self.queue.pop(0)
-        except:
-            return -1
-
-    def popMiddle(self):
-        l = len(self.queue)//2
-        try:
-            return self.queue.pop(l if len(self.queue)&1 else l-1)
-        except:
-            return -1
-
-    def popBack(self):
-        try:
-            return self.queue.pop()
-        except:
-            return -1
-
-
-f = FrontMiddleBackQueue()
-
-f.pushMiddle(3)
-f.pushFront(6)
-f.pushMiddle(6)
-f.pushMiddle(3)
-print(f.queue)
-print(f.popMiddle())
-print(f.popMiddle())
-# print(f.queue)
-# print(f.popMiddle())
-# print(f.popMiddle())
+print(s.calPoints(["5","2","C","D","+"]))
+print(s.calPoints(["5","-2","4","C","D","9","+","+"]))
