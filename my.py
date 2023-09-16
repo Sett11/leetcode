44f6878c9abf72a1fe63ca6a9cd710f7481bf9bd
Jhonny <gonzodzen@mail.ru>
2023-09-16 17:59:06 +0300

    My Calendar I

--- my.py
+++ my.py
@@ -1,26 +1,20 @@
-class Solution:
-    def accountsMerge(self,a):
-        a=sorted([i[:1]+sorted(i[1:]) for i in a])
-        r=[]
-        while a:
-            t=[]
-            t.extend(a.pop(0))
-            name=t.pop(0)
-            j=0
-            while j<len(a):
-                if set(t).intersection(a[j][1:]):
-                    t.extend(a.pop(j)[1:])
-                    j=-1
-                j+=1
-            r.append([name]+sorted(set(t)))
-        return r
+class MyCalendar:
 
-    
-s=Solution()
+    def __init__(self):
+        self.a = []
 
-print(s.accountsMerge([["John","johnsmith@mail.com","john_newyork@mail.com"],
-                       ["John","johnsmith@mail.com","john00@mail.com"],
-                       [ "Mary","mary@mail.com"],
-                       ["Jhon","johnnybravo@mail.com"]]))
+    def book(self, s, e):
+        for i in self.a:
+            if (s >= i[0] and e <= i[1]) or (s < i[1] and s >= i[0]) or (e > i[0] and s < i[0]):
+                return False
+        return bool(self.a.append([s, e]) or 1)
 
-print(s.accountsMerge([["David","David0@m.co","David1@m.co"],["David","David3@m.co","David4@m.co"],["David","David4@m.co","David5@m.co"],["David","David2@m.co","David3@m.co"],["David","David1@m.co","David2@m.co"]]))
+
+m = MyCalendar()
+
+arr = [47, 50], [33, 41], [39, 45], [33, 42], [25, 32], [
+    26, 35], [19, 25], [3, 8], [8, 13], [18, 27]
+
+for i in arr:
+    print(m.book(i[0], i[1]))
+    #print(m.a)
