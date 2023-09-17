aefb274173667d5f3f358cda8424e01c929fb367
Jhonny <gonzodzen@mail.ru>
2023-09-17 20:58:04 +0300

    Similar String Groups

--- my.py
+++ my.py
@@ -1,21 +1,23 @@
-def f(a,b):
-    n=min(a,b)
-    while n>1:
-        if a%n==0 and b%n==0:
-            return False
-        n-=1
-    return True
-
 class Solution:
-    def simplifiedFractions(self,n):
-        a=[i for i in range(1,n+1)]
+    def numSimilarGroups(self,s):
         r=[]
-        for i in range(n):
-            for j in range(i+1,n):
-                if f(a[i],a[j]):
-                    r.append(f'{a[i]}/{a[j]}')
-        return r
+        while s:
+            t=[s.pop(0)]
+            i=0
+            while i<len(s):
+                k=0
+                while k<len(t):
+                    if len([h for h in [t[k][j]==s[i][j] for j in range(len(s[i]))] if not h])<=2:
+                        t.append(s.pop(i))
+                        i=-1
+                        break
+                    k+=1
+                i+=1
+            r.append(t)
+        return len(r)
     
 s=Solution()
 
-print(s.simplifiedFractions(4))
+print(s.numSimilarGroups(["tars","rats","arts","star"]))
+print(s.numSimilarGroups(["omv","ovm"]))
+print(s.numSimilarGroups(["kccomwcgcs", "socgcmcwkc", "sgckwcmcoc", "coswcmcgkc", "cowkccmsgc", "cosgmccwkc", "sgmkwcccoc", "coswmccgkc", "kowcccmsgc", "kgcomwcccs"]))
