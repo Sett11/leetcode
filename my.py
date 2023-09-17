61f2168462b9a5b77304cc0049bf5240d6416d1d
Jhonny <gonzodzen@mail.ru>
2023-09-17 11:37:32 +0300

    Rotate String

--- my.py
+++ my.py
@@ -1,14 +1,13 @@
-def shange(a):
-    for i in [0,1]:
-        t=a[i].split(':')
-        a[i]=float(t[0]+'.'+t[1])
-    return a
-
 class Solution:
-    def haveConflict(self,a,b):
-        a,b=shange(a),shange(b)
-        return True if (b[1]>=a[0] and b[1]<=a[1]) or b[0]==a[1] or (a[1]>=b[0] and a[1]<=b[1]) else False
+    def rotateString(self,s,t):
+        i=0
+        while i<len(s):
+            if s==t:
+                return True
+            s=s[1:]+s[0]
+            i+=1
+        return False
     
 s=Solution()
 
-print(s.haveConflict(["01:00","02:00"],["01:20","03:00"]))
+print(s.rotateString('abcde','bcdea'))
