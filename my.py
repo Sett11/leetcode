f2cbf069864e89e89c2f0cc899300cd311c1b26c
Jhonny <gonzodzen@mail.ru>
2023-09-17 18:39:58 +0300

    Simplified Fractions

--- my.py
+++ my.py
@@ -1,20 +1,21 @@
-from re import sub
+def f(a,b):
+    n=min(a,b)
+    while n>1:
+        if a%n==0 and b%n==0:
+            return False
+        n-=1
+    return True
 
 class Solution:
-    def maskPII(self,s):
-        s=sub(r'[^\dA-z@\.]','',s)
-        if s.isdigit():
-            c=s[:-10]
-            q=s[-10:]
-            x=q[-4:]
-            q=q[:-4]
-            return ('+'+'*'*len(c)+'-' if c else '')+'-'.join([sub(r'.','*',q[i:i+3]) for i in range(0,len(q),3)])+'-'+x
-        s=s.lower().split('@')
-        return s[0][0]+('*'*5)+s[0][-1]+'@'+s[1]
+    def simplifiedFractions(self,n):
+        a=[i for i in range(1,n+1)]
+        r=[]
+        for i in range(n):
+            for j in range(i+1,n):
+                if f(a[i],a[j]):
+                    r.append(f'{a[i]}/{a[j]}')
+        return r
     
 s=Solution()
 
-print(s.maskPII('LeetCode@LeetCode.com'))
-print(s.maskPII('AB@qq.com'))
-print(s.maskPII('1(234)567-890'))
-print(s.maskPII('86-(10)12345678'))
+print(s.simplifiedFractions(4))
