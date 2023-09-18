7b97633237f6bb74e2be14b05b037e49c85b7af5
Jhonny <gonzodzen@mail.ru>
2023-09-19 00:12:48 +0300

    Finding 3-Digit Even Numbers

--- my.py
+++ my.py
@@ -1,7 +1,17 @@
+from itertools import permutations
+from bisect import insort
+
 class Solution:
-    def findNumbers(self,a):
-        return len(list((i for i in a if not len(str(i))&1)))
+    def findEvenNumbers(self,a):
+        s=set((permutations(a,3)))
+        r=[]
+        for i in s:
+            n=int(''.join([str(j) for j in i]))
+            if not n&1 and n>=100:
+                insort(r,n)
+        return r
     
 s=Solution()
 
-print(s.findNumbers([12,345,2,6,7896]))
+print(s.findEvenNumbers([2,1,3,0]))
+print(s.findEvenNumbers([2,2,8,8,2]))
