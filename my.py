f047940930e103c3d62c7d68324916a04461d1a6
Jhonny <gonzodzen@mail.ru>
2023-09-18 19:26:10 +0300

    Number of Lines To Write String

--- my.py
+++ my.py
@@ -1,11 +1,18 @@
 class Solution:
-    def binaryGap(self,n):
-        a=[i for i,j in enumerate(bin(n)[2:]) if j=='1']
-        m=0
-        for i in range(1,len(a)):
-            m=max(m,a[i]-a[i-1])
-        return m
+    def numberOfLines(self,a,s):
+        b=[a[ord(i)-97] for i in s]
+        c=i=k=0
+        while i<len(b):
+            if (c+b[i])<=100:
+                c+=b[i]
+            else:
+                k+=1
+                c=0
+                i-=1
+            i+=1
+        return [k+1,c]
     
 s=Solution()
 
-print(s.binaryGap(22))
+print(s.numberOfLines([4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10. ,10,10,10],'bbbcccdddaaa'))
+print(s.numberOfLines([10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10],'abcdefghijklmnopqrstuvwxyz'))
