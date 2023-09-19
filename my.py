c44686f65d88cafca6b7458b249ca7e3ed12fc20
Jhonny <gonzodzen@mail.ru>
2023-09-19 20:18:04 +0300

    Rotated Digits

--- my.py
+++ my.py
@@ -1,18 +1,16 @@
-from re import sub
-
 class Solution:
-    def minDiffInBST(self,a):
-        a=sub(r'[^\d]',' ',repr(a)).split(' ')
-        m=1e9
-        l=len(a)
-        for i in range(l):
-            if a[i]:
-                t=int(a[i])
-                for j in range(i+1,l):
-                    if a[j]:
-                        m=min(m,abs(t-int(a[j])))
-        return m
+    def rotatedDigits(self,n):
+        c=0
+        for i in range(2,n+1):
+            t=str(i)
+            if len([j for j in '347' if j in t]):
+                continue
+            else:
+                k=int(''.join(['2' if j=='5' else '5' if j=='2' else '6' if j=='9' else '9' if j=='6' else j for j in t]))
+                if k!=i:
+                    c+=1
+        return c
     
 s=Solution()
 
-print(s.minDiffInBST([4,2,6,1,3]))
+print(s.rotatedDigits(10000))
