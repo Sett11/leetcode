8c958553bbee5869db10dbf6f3c7c00937ca80f4
Jhonny <gonzodzen@mail.ru>
2023-09-20 00:41:22 +0300

    Duplicate Zeros

--- my.py
+++ my.py
@@ -1,7 +1,14 @@
+from re import sub
+
 class Solution:
-    def sortArray(self,a):
-        return sorted(a)
+    def duplicateZeros(self,a):
+        l=len(a)
+        b=[int(j) for j in list(sub(r'0','00',''.join([str(i) for i in a])))]
+        a.clear()
+        a.extend(b[:l])
     
 s=Solution()
-
-print(s.sortArray([2,5,3,1,4]))
+arr=[1,0,2,3,0,4,5,0]
+print(arr)
+s.duplicateZeros(arr)
+print(arr)
