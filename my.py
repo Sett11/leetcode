a9aa05ab6dbb656fc7fc6db11684d8b0b1d59c46
Jhonny <gonzodzen@mail.ru>
2023-09-20 20:46:59 +0300

    Delete Columns to Make Sorted

--- my.py
+++ my.py
@@ -1,15 +1,7 @@
-def f(a,v):
-    for i in range(len(a)-1):
-        if (v and a[i]>=a[i+1]) or (not v and a[i]<=a[i+1]):
-            return False
-    return True
-
 class Solution:
-    def validMountainArray(self,a):
-        n=a.index(max(a))
-        return bool(a[:n] and a[n+1:] and f(a[:n],True) and f(a[n+1:],False) and a.count(a[n])==1)
+    def minDeletionSize(self,a):
+        return len([i for i in zip(*a) if sorted(i)!=list(i)])
     
 s=Solution()
 
-print(s.validMountainArray([0,3,2,1]))
-print(s.validMountainArray([2,1]))
+print(s.minDeletionSize(["cba","daf","ghi"]))
