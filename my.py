4c9d5f1ebf3442e6267b2880f13023971bea3993
Jhonny <gonzodzen@mail.ru>
2023-10-01 01:48:25 +0300

    Rank Transform of an Array

--- my.py
+++ my.py
@@ -1,13 +1,14 @@
 class Solution:
-    def maxSum(self,a,n,k):
-        m=0
+    def arrayRankTransform(self,a):
+        o={}
+        l=list(set(a))
+        l.sort()
+        for i in range(len(l)):
+            o[l[i]]=i
         for i in range(len(a)):
-            t=a[i:i+k]
-            if len(set(t))>=n:
-                m=max(m,sum(t))
-        return m
+            a[i]=o[a[i]]+1
+        return a
 
 s=Solution()
 
-print(s.maxSum([2,6,7,3,1,7],3,4))
-print(s.maxSum([5,9,9,2,4,5,4],1,3))
+print(s.arrayRankTransform([40,10,20,30]))
