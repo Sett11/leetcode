28ec0840487397f1e13a96c14314d64c83a77691
Jhonny <gonzodzen@mail.ru>
2023-09-30 16:20:37 +0300

    Verifying an Alien Dictionary

--- my.py
+++ my.py
@@ -1,11 +1,8 @@
 class Solution:
-    def uncommonFromSentences(self,s1,s2):
-        s1,s2=s1.split(' '),s2.split(' ')
-        a,b=[i for i in s1 if s1.count(i)==1 and i not in s2],[i for i in s2 if s2.count(i)==1 and i not in s1]
-        return a+b
+    def isAlienSorted(self,w,s):
+        a=[[s.index(j) for j in i] for i in w]
+        return sorted(a)==a
     
 s=Solution()
 
-print(s.uncommonFromSentences("this apple is sweet","this apple is sour"))
-print(s.uncommonFromSentences("apple apple","banana"))
-print(s.uncommonFromSentences("s z z z s","s z ejt"))
+print(s.isAlienSorted(["hello","leetcode"],"hlabcdefgijkmnopqrstuvwxyz"))
