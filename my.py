b6c5223d2c4bc009ee3f29f060b064f88ca1ff20
Jhonny <gonzodzen@mail.ru>
2023-10-01 02:05:09 +0300

    Find Lucky Integer in an Array

--- my.py
+++ my.py
@@ -1,11 +1,7 @@
 class Solution:
-    def luckyNumbers (self,a):
-        b=[max(i) for i in zip(*a)]
-        c=[min(i) for i in a]
-        return list(set(b).intersection(set(c)))
+    def findLucky(self,a):
+        return max([i for i in a if i==a.count(i)],default=-1)
 
 s=Solution()
 
-print(s.luckyNumbers([[3,7,8],[9,11,13],[15,16,17]]))
-print(s.luckyNumbers([[1,10,4,2],[9,3,8,7],[15,16,17,12]]))
-print(s.luckyNumbers([[7,8],[1,2]]))
+print(s.findLucky([1,2,2,3,3,3]))
