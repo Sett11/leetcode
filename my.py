130e53027932dc56b06133994521b54f25162525
Jhonny <gonzodzen@mail.ru>
2023-09-30 22:45:37 +0300

    Split Strings by Separator

--- my.py
+++ my.py
@@ -1,22 +1,8 @@
 class Solution:
-    def balancedStringSplit(self,s):
-        r=[]
-        i=0
-        while i<len(s):
-            c=s[i]
-            j=i+1
-            while j<=len(s):
-                if c.count('L')==c.count('R'):
-                    r.append(c)
-                    i=j-1
-                    break
-                else:
-                    c+=s[j]
-                    j+=1
-            i+=1
-        return len(r)
+    def splitWordsBySeparator(self,w,s):
+        return [j for j in sum([i.split(s) for i in w],[]) if j]
     
 s=Solution()
 
-print(s.balancedStringSplit('RLRRLLRLRL'))
-print(s.balancedStringSplit('RLRRRLLRLL'))
+print(s.splitWordsBySeparator(["one.two.three","four.five","six"],'.'))
+print(s.splitWordsBySeparator(["$easy$","$problem$"],'$'))
