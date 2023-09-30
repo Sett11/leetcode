027bc1deb508d07fa9d4510fb2c340ae2156b537
Jhonny <gonzodzen@mail.ru>
2023-10-01 00:14:59 +0300

    Maximum Sum of Almost Unique Subarray

--- my.py
+++ my.py
@@ -1,7 +1,13 @@
 class Solution:
-    def countDistinctIntegers(self,a):
-        return len(set(list(map(lambda e:int(str(e)[::-1]),a))+a))
+    def maxSum(self,a,n,k):
+        m=0
+        for i in range(len(a)):
+            t=a[i:i+k]
+            if len(set(t))>=n:
+                m=max(m,sum(t))
+        return m
 
 s=Solution()
 
-print(s.countDistinctIntegers([1,13,10,12,31]))
+print(s.maxSum([2,6,7,3,1,7],3,4))
+print(s.maxSum([5,9,9,2,4,5,4],1,3))
