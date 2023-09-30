3d9ee3983cfaf5d13c52d0548c472cc716ae1665
Jhonny <gonzodzen@mail.ru>
2023-09-30 22:41:02 +0300

    Split a String in Balanced Strings

--- my.py
+++ my.py
@@ -1,8 +1,22 @@
 class Solution:
-    def uniqueOccurrences(self,a):
-        b=[a.count(i) for i in set(a)]
-        return len(b)==len(set(b))
+    def balancedStringSplit(self,s):
+        r=[]
+        i=0
+        while i<len(s):
+            c=s[i]
+            j=i+1
+            while j<=len(s):
+                if c.count('L')==c.count('R'):
+                    r.append(c)
+                    i=j-1
+                    break
+                else:
+                    c+=s[j]
+                    j+=1
+            i+=1
+        return len(r)
     
 s=Solution()
 
-print(s.uniqueOccurrences([1,1,1,2,2,3]))
+print(s.balancedStringSplit('RLRRLLRLRL'))
+print(s.balancedStringSplit('RLRRRLLRLL'))
