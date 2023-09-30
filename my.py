8c2a7a48a099f0ba5fe15cd1dbe8c9aacb631917
Jhonny <gonzodzen@mail.ru>
2023-09-30 22:24:44 +0300

    Unique Number of Occurrences

--- my.py
+++ my.py
@@ -1,17 +1,8 @@
-b=[1165529,18688525]
 class Solution:
-    def numEquivDominoPairs(self,a):
-        l=len(a)
-        if l>5000:
-            return b.pop(0)
-        c=0
-        for i in range(l):
-            for j in range(i+1,l):
-                if sorted(a[i])==sorted(a[j]):
-                    c+=1
-        return c
+    def uniqueOccurrences(self,a):
+        b=[a.count(i) for i in set(a)]
+        return len(b)==len(set(b))
     
 s=Solution()
 
-print(s.numEquivDominoPairs([[1,2],[1,2],[1,1],[1,2],[2,2]]))
-print(s.numEquivDominoPairs([[1,2],[2,1],[3,4],[5,6]]))
+print(s.uniqueOccurrences([1,1,1,2,2,3]))
