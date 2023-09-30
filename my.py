d5e9033f89103f6110577fe6ac259a5f99a9dfc8
Jhonny <gonzodzen@mail.ru>
2023-09-30 17:57:50 +0300

    Find the Maximum Divisibility Score

--- my.py
+++ my.py
@@ -1,9 +1,17 @@
 class Solution:
-    def averageValue(self,a):
-        a=[i for i in a if not i&1 and not i%3]
-        return int(sum(a)/len(a)) if len(a) else 0
+    def maxDivScore(self,a,d):
+        m=0
+        r=[]
+        for i in d:
+            t=len([j for j in a if j%i==0])
+            m=max(m,t)
+            r.append([t,i])
+        return min([i[1] for i in r if i[0]==m])
             
     
 s=Solution()
 
-print(s.averageValue([1,3,6,10,12,15]))
+print(s.maxDivScore([20,14,21,10],[5,7,5]))
+print(s.maxDivScore([4,7,9,3,9],[5,2,3]))
+print(s.maxDivScore([73,13,20,6],
+[56,75,83,26,24,53,56,61]))
