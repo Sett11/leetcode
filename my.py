a0b8cb5245d3c24bbb29907a75402d9510a9554e
Jhonny <gonzodzen@mail.ru>
2023-10-01 22:57:06 +0300

    Count Square Sum Triples

--- my.py
+++ my.py
@@ -1,33 +1,16 @@
-class Solution:
-    def fourSum(self,a,d):
-        a.sort()
-        l=len(a)
-        s=set()
-        for i in range(l):
-            o=a[i]
-            for j in range(i+1,l):
-                t=a[j]
-                k=j+1
-                h=l-1
-                while k<h:
-                    th=a[k]
-                    f=a[h]
-                    p=o+t+th+f
-                    if p==d:
-                        s.add((o,t,th,f))
-                        k+=1
-                        h-=1
-                    elif p>d:
-                        h-=1
-                    elif p<d:
-                        k+=1
-                    else:
-                        h-=1
-                        k+=1
-        return s
-    
+from math import sqrt
 
+class Solution:
+    def countTriples(self,n):
+        a=[i for i in range(1,n+1)]
+        c=0
+        for i in range(len(a)):
+            for j in range(len(a)):
+                q=sqrt(a[i]**2+a[j]**2)
+                if q in a:
+                    c+=1
+        return c
+    
 s=Solution()
 
-print(s.fourSum([1,0,-1,0,-2,2],0))
-print(s.fourSum([2,2,2,2,2],0))
+print(s.countTriples(300))
