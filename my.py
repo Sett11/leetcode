4cf46ac9dc3d4cf98f8e016db0dafaaf76617428
Jhonny <gonzodzen@mail.ru>
2023-10-01 21:47:48 +0300

    3Sum

--- my.py
+++ my.py
@@ -1,13 +1,25 @@
 class Solution:
-    def pivotInteger(self,n):
-        c=n-1
-        if n==1:
-            return 1
-        while True:
-            if c*(c+1)/2==sum([i for i in range(c,n+1)]) or c==-1:
-                return c
-            c-=1
+    def threeSum(self,a):
+        a.sort()
+        s=set()
+        for i in range(len(a)-2):
+            f=a[i]
+            j=i+1
+            k=len(a)-1
+            while j<k:
+                x=a[j]
+                y=a[k]
+                p=f+x+y
+                if p>0:
+                    k-=1
+                elif p<0:
+                    j+=1
+                else:
+                    s.add((f,x,y))
+                    j+=1
+                    k-=1
+        return s
     
 s=Solution()
 
-print(s.pivotInteger(1))
+print(s.threeSum([-1,0,1,2,-1,-4]))
