465817c80e5cf2b790826bf1224863850c962e0e
Jhonny <gonzodzen@mail.ru>
2023-10-01 18:00:43 +0300

    Number of Good Pairs

--- my.py
+++ my.py
@@ -1,15 +1,13 @@
-from re import sub
-
 class Solution:
-    def reformatDate(self,s):
-        mounth={"Jan":'01', "Feb":'02', "Mar":'03', "Apr":'04', "May":'05', "Jun":'06', "Jul":'07', "Aug":'08', "Sep":'09', "Oct":'10', "Nov":'11', "Dec":'12'}
-        s=s.split(' ')
-        n=sub(r'\D','',s[0])
-        if len(n)==1:
-            n='0'+n
-        return f'{s[-1]}-{mounth[s[1]]}-{n}'
+    def numIdenticalPairs(self,a):
+        l=len(a)
+        c=0
+        for i in range(l):
+            for j in range(i+1,l):
+                if a[i]==a[j]:
+                    c+=1
+        return c
     
 s=Solution()
 
-print(s.reformatDate("6th Jun 1933"))
-print(s.reformatDate("20th Oct 2052"))
+print(s.numIdenticalPairs([1,2,3,1,1,3]))
