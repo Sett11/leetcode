19308e7ae18b7a3203be25feb1624d0501f06206
Jhonny <gonzodzen@mail.ru>
2023-10-01 20:08:52 +0300

    Count Good Triplets

--- my.py
+++ my.py
@@ -1,10 +1,13 @@
 class Solution:
-    def restoreString(self,s,a):
-        r=[]
+    def countGoodTriplets(self,a,b,c,d):
+        q=0
         for i in range(len(a)):
-            r.append([a[i],s[i]])
-        return ''.join(list(map(lambda e:e[1],sorted(r))))
+            for j in range(i+1,len(a)):
+                for k in range(j+1,len(a)):
+                    if abs(a[i]-a[j])<=b and abs(a[j]-a[k])<=c and abs(a[i]-a[k])<=d:
+                        q+=1
+        return q
     
 s=Solution()
 
-print(s.restoreString("codeleet",[4,5,6,7,0,2,1,3]))
+print(s.countGoodTriplets([3,0,1,1,9,7],7,2,3))
