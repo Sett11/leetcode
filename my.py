a5b7084ed3ccb06c5e3e7f35c2c46995d9331382
Jhonny <gonzodzen@mail.ru>
2023-10-01 15:53:09 +0300

    Arithmetic Subarrays

--- my.py
+++ my.py
@@ -1,9 +1,16 @@
+def canMakeArithmeticProgression(a):
+    a.sort()
+    n=a[1]-a[0]
+    return all(a[i+1]-a[i]==n for i in range(len(a)-1))
+
 class Solution:
-    def canMakeArithmeticProgression(self,a):
-        a.sort()
-        return all(a[i+1]-a[i]==a[1]-a[0] for i in range(len(a)-1))
+    def checkArithmeticSubarrays(self,a,b,c):
+        r=[]
+        for i in range(len(b)):
+            r.append(canMakeArithmeticProgression(a[b[i]:c[i]+1]))
+        return r
         
     
 s=Solution()
 
-print(s.canMakeArithmeticProgression([3,1,5]))
+print(s.checkArithmeticSubarrays([4,6,5,9,3,7],[0,0,2],[2,3,5]))
