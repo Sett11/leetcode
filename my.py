dbc74412554bd20b8eed3844445078ac203b9a98
Jhonny <gonzodzen@mail.ru>
2023-10-01 14:22:13 +0300

    Running Sum of 1d Array

--- my.py
+++ my.py
@@ -1,20 +1,12 @@
 class Solution:
-    def maxPower(self,s):
-        if len(set(s))==1:
-            return len(s)
-        s+='&&'
+    def runningSum(self,a):
         r=[]
-        for i in range(len(s)):
-            c=s[i]
-            for j in range(i+1,len(s)):
-                if len(set(c))==1:
-                    c+=s[j]
-                else:
-                    r.append(c[:-1])
-                    break
-        return len(sorted(r,key=len,reverse=True)[0])
-
+        c=0
+        for i in range(len(a)):
+            c+=a[i]
+            r.append(c)
+        return r
+    
 s=Solution()
 
-print(s.maxPower('abbcccddddeeeeedcba'))
-print(s.maxPower('aabbbbbccccdddddddeffffffggghhhhhiiiiijjjkkkkkllllmmmmmnnnnnoopppqrrrrsssttttuuuuvvvvwwwwwwwxxxxxyyyzzzzzzzz'))
+print(s.runningSum([1,2,3,4]))
