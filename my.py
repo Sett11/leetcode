31ae399b0273a01fb7b9877e664bd4dd44a1ea42
Jhonny <gonzodzen@mail.ru>
2023-10-02 01:18:33 +0300

    Check if Every Row and Column Contains All Numbers

--- my.py
+++ my.py
@@ -1,15 +1,7 @@
 class Solution:
-    def diagonalSum(self,a):
-        return sum([a[i][len(a[0])-1-i]+a[i][i] for i in range(len(a))])-(a[len(a)//2][len(a[0])//2] if len(a)&1 else 0)
+    def checkValid(self,a):
+        return all(sorted(i)==[x for x in range(1,len(a)+1)] for i in a+[list(j) for j in zip(*a)])
     
 s=Solution()
 
-print(s.diagonalSum([
-            [ 1 ,2, 3 ],
-              [4, 5,6 ],
-              [ 7 ,8, 9 ]]))
-print(s.diagonalSum([
-            [ 1 ,1,1,1],
-              [1,1,1,1],
-              [1,1,1,1],
-              [1,1,1,1 ]]))
+print(s.checkValid([[1,2,3],[3,1,2],[2,3,1]]))
