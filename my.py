dea4c2109987a5f0388ee44ffe95f7c4c78b9850
Jhonny <gonzodzen@mail.ru>
2023-10-02 01:48:25 +0300

    Maximum Product of Two Elements in an Array

--- my.py
+++ my.py
@@ -1,11 +1,12 @@
 class Solution:
-    def trimMean(self,a):
+    def maxProduct(self,a):
         l=len(a)
-        while l-len(a)<l/100*10:
-            a.remove(max(a))
-            a.remove(min(a))
-        return round(sum(a)/len(a),5)
+        m=0
+        for i in range(l):
+            for j in range(i+1,l):
+                m=max((a[i]-1)*(a[j]-1),m)
+        return m
     
 s=Solution()
 
-print(s.trimMean([6,0,7,0,7,5,7,8,3,4,0,7,8,1,6,8,1,1,2,4,8,1,9 ,5,4,3,8,5,10,8,6,6,1,0,6,10,8,2,3,4]))
+print(s.maxProduct([3,4,5,2]))
