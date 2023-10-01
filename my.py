fe5bbbcc59f762d26dab80062f2630c6d3f49935
Jhonny <gonzodzen@mail.ru>
2023-10-01 20:11:45 +0300

    Number of Unequal Triplets in Array

--- my.py
+++ my.py
@@ -1,13 +1,13 @@
 class Solution:
-    def countGoodTriplets(self,a,b,c,d):
+    def unequalTriplets(self,a):
         q=0
         for i in range(len(a)):
             for j in range(i+1,len(a)):
                 for k in range(j+1,len(a)):
-                    if abs(a[i]-a[j])<=b and abs(a[j]-a[k])<=c and abs(a[i]-a[k])<=d:
+                    if a[i]!=a[j] and a[j]!=a[k] and a[i]!=a[k]:
                         q+=1
         return q
     
 s=Solution()
 
-print(s.countGoodTriplets([3,0,1,1,9,7],7,2,3))
+print(s.unequalTriplets([4,4,2,4,3]))
