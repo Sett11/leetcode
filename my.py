fe22bebcea26c7c8065ba42a4505e23995ca7ae5
Jhonny <gonzodzen@mail.ru>
2023-10-01 12:59:16 +0300

    Consecutive Characters

--- my.py
+++ my.py
@@ -1,7 +1,20 @@
 class Solution:
-    def findLucky(self,a):
-        return max([i for i in a if i==a.count(i)],default=-1)
+    def maxPower(self,s):
+        if len(set(s))==1:
+            return len(s)
+        s+='&&'
+        r=[]
+        for i in range(len(s)):
+            c=s[i]
+            for j in range(i+1,len(s)):
+                if len(set(c))==1:
+                    c+=s[j]
+                else:
+                    r.append(c[:-1])
+                    break
+        return len(sorted(r,key=len,reverse=True)[0])
 
 s=Solution()
 
-print(s.findLucky([1,2,2,3,3,3]))
+print(s.maxPower('abbcccddddeeeeedcba'))
+print(s.maxPower('aabbbbbccccdddddddeffffffggghhhhhiiiiijjjkkkkkllllmmmmmnnnnnoopppqrrrrsssttttuuuuvvvvwwwwwwwxxxxxyyyzzzzzzzz'))
