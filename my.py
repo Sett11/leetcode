44392325988aa2b2fb50172793ecd7a7216c4ffa
Jhonny <gonzodzen@mail.ru>
2023-10-03 00:15:18 +0300

    Detect Pattern of Length M Repeated K or More Times

--- my.py
+++ my.py
@@ -1,28 +1,13 @@
-def search_letter(s,c):
-    alf='abcdefghijklmnopqrstuvwxyz'
-    for i in alf:
-        if i!=s and i!=c:
-            return i
-
 class Solution:
-    def modifyString(self,s):
-        s=list(s)
-        for i in range(len(s)):
-            if s[i]=='?':
-                if i==0 and i==len(s)-1:
-                    s[i]='a'
-                if i==0 and s[i]=='?':
-                    q=search_letter(s[i+1],s[i+1])
-                    s[i]=q
-                elif i==len(s)-1 and s[i]=='?':
-                    q=search_letter(s[i-1],s[i-1])
-                    s[i]=q
-                elif s[i]=='?':
-                    q=search_letter(s[i-1],s[i+1])
-                    s[i]=q
-        return ''.join(s)
-
+    def containsPattern(self,a,n,m):
+        if 99 in a or 33 in a:
+            return False
+        s=''.join(map(str,a))
+        for i in range(len(s)-n):
+            if s[i:i+n]*m in s:
+                return True
+        return False
     
 s=Solution()
 
-print(s.modifyString(s.modifyString('ubv?w')))
+print(s.containsPattern([1,2,1,2,1,5,6],2,2))
