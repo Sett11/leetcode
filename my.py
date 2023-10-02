4c40919a8bd3697476e435541b3ce913652c71f4
Jhonny <gonzodzen@mail.ru>
2023-10-03 01:00:57 +0300

    Maximum Units on a Truck

--- my.py
+++ my.py
@@ -1,17 +1,16 @@
-from re import sub
-
 class Solution:
-    def reformatNumber(self,s):
-        s=sub(r'\D','',s)
-        r=[]
-        while len(s)>4:
-            r.append(s[:3])
-            s=s[3:]
-        r='-'.join(r)
-        if len(s)==4:
-            return r+('-' if r else '')+s[0:2]+'-'+s[2:4]
-        return r+('-' if r else '')+s
+    def maximumUnits(self,a,n):
+        a=sorted(a,key=lambda e:e[1],reverse=True)
+        i=c=0
+        while i<len(a) and n:
+            if n:
+                while a[i][0] and n:
+                    a[i][0]-=1
+                    c+=a[i][1]
+                    n-=1
+                i+=1
+        return c
     
 s=Solution()
 
-print(s.reformatNumber('123 4-567'))
+print(s.maximumUnits([[5,10],[2,5],[4,7],[3,9]],10))
