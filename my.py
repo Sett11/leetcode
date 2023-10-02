8d703ede8f3516bed71e5800c66b7541df70ae62
Jhonny <gonzodzen@mail.ru>
2023-10-02 20:52:45 +0300

    Kids With the Greatest Number of Candies

--- my.py
+++ my.py
@@ -1,24 +1,10 @@
-from re import sub
-
 class Solution:
-    def reformat(self,s):
-        a,b=sub(r'\d','',s),sub(r'\D','',s)
-        if abs(len(a)-len(b))>1 or (not a and len(s)>1) or (not b and len(s)>1):
-            return ''
-        if len(a)>len(b):
-            a,b=b,a
-        if len(b)>len(a):
-            a,b=b,a
-        c=''
-        j=0
-        for i in a:
-            c+=i
-            if j<len(b):
-                c+=b[j]
-            j+=1
-        return c
-        
+    def kidsWithCandies(self,a,k):
+        r=[]
+        [r.append(True) if i+k>=max(a) else r.append(False) for i in a]
+        return r
     
 s=Solution()
 
-print(s.reformat('a'))
+print(s.kidsWithCandies([2,3,5,1,3],3))
+print(s.kidsWithCandies([4,2,1,1,2],1))
