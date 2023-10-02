d25be13dde8b3277b768dc5ee06f00090d44abee
Jhonny <gonzodzen@mail.ru>
2023-10-02 23:31:35 +0300

    Three Consecutive Odds

--- my.py
+++ my.py
@@ -1,9 +1,3 @@
-from time import time
-
-class Solution:
-    def threeConsecutiveOdds(self,a):
-        return '111' in ''.join(['1' if i&1 else '0' for i in a])
-
 class Solution():
     def threeConsecutiveOdds(self,a):
         for i in range(len(a)-2):
@@ -13,12 +7,5 @@ class Solution():
     
 s=Solution()
 
-t=time()
 
 print(s.threeConsecutiveOdds([1,2,34,3,4,5,7,23,12]))
-
-e=time()
-
-0.005007743835449219
-
-print(e-t)
