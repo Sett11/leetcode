68b74f73b363b7eadac06c32ecab7d67bdbc19b6
Jhonny <gonzodzen@mail.ru>
2023-10-03 00:52:52 +0300

    Reformat Phone Number

--- my.py
+++ my.py
@@ -1,3 +1,17 @@
+from re import sub
+
 class Solution:
-    def arrayStringsAreEqual(self,a,b):
-        return ''.join(a)==''.join(b)
+    def reformatNumber(self,s):
+        s=sub(r'\D','',s)
+        r=[]
+        while len(s)>4:
+            r.append(s[:3])
+            s=s[3:]
+        r='-'.join(r)
+        if len(s)==4:
+            return r+('-' if r else '')+s[0:2]+'-'+s[2:4]
+        return r+('-' if r else '')+s
+    
+s=Solution()
+
+print(s.reformatNumber('123 4-567'))
