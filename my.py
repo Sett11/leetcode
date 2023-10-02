a9745975537e583775f108960e8973bd05a23475
Jhonny <gonzodzen@mail.ru>
2023-10-02 23:21:16 +0300

    Three Consecutive Odds

--- my.py
+++ my.py
@@ -1,9 +1,24 @@
+from time import time
+
 class Solution:
-    def destCity(self,a):
-        for i in set(sum(a,[])):
-            if any(j[1]==i for j in a) and not any(j[0]==i for j in a):
-                return i
+    def threeConsecutiveOdds(self,a):
+        return '111' in ''.join(['1' if i&1 else '0' for i in a])
+
+class Solution():
+    def threeConsecutiveOdds(self,a):
+        for i in range(len(a)-2):
+            if a[i]&1 and a[i+1]&1 and a[i+2]&1:
+                return True
+        return False
     
 s=Solution()
 
-print(s.destCity([["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]))
+t=time()
+
+print(s.threeConsecutiveOdds([1,2,34,3,4,5,7,23,12]))
+
+e=time()
+
+0.005007743835449219
+
+print(e-t)
