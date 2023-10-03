3937ed22b12f93939354a4dbb7eea0619b58a0a2
Jhonny <gonzodzen@mail.ru>
2023-10-04 01:17:18 +0300

    Maximum Ascending Subarray Sum

--- my.py
+++ my.py
@@ -1,11 +1,15 @@
 class Solution:
-    def secondHighest(self,s):
-        try:
-            return sorted({int(i) for i in s if i.isdigit()},reverse=True)[1]
-        except:
-            return -1
+    def maxAscendingSum(self,a):
+        a.append(a[-1]-1)
+        m=max(a,default=0)
+        for i in range(len(a)-1):
+            if a[i]<a[i+1]:
+                for j in range(i+1,len(a)-1):
+                    if a[j]>=a[j+1]:
+                        m=max(m,sum(a[i:j+1]))
+                        break
+        return m
 
 s=Solution()
 
-print(s.secondHighest('dfa12321afd'))
-print(s.secondHighest('ck077'))
+print(s.maxAscendingSum([100,10,1]))
