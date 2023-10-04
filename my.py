6b3af6350109058997f930f444b9f4975560b013
Jhonny <gonzodzen@mail.ru>
2023-10-05 01:03:38 +0300

    Find Closest Number to Zero

--- my.py
+++ my.py
@@ -1,3 +1,23 @@
 class Solution:
-    def sum(self,a,b):
-        return a+b
+    def findClosestNumber(self,a):
+        if 0 in a:
+            return 0
+        if len(set(a))==1:
+            return a[0]
+        a.append(0)
+        a=sorted(set(a))
+        i=a.index(0)
+        if i==0:
+            return a[i+1]
+        if i==len(a)-1:
+            return a[i-1]
+        if len(a)==2:
+            return [i for i in a if i][0]
+        a=sorted([i for i in a if i][i-1:i+1],key=abs)
+        if abs(a[0])==abs(a[1]):
+            return abs(a[0])
+        return a[0]
+    
+s=Solution()
+
+print(s.findClosestNumber([-10,-12,-54,-12,-544,-10000]))
