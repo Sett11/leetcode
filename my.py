f3660897e13944492d09949faabc1800236b45b0
Jhonny <gonzodzen@mail.ru>
2023-10-05 00:03:50 +0300

    Find First Palindromic String in the Array

--- my.py
+++ my.py
@@ -1,10 +1,10 @@
-from functools import reduce as r
-
 class Solution:
-    def arraySign(self,a):
-        s=r(lambda e,u:e*u,a)
-        return 1 if s>0 else 0 if not s else -1
-
+    def firstPalindrome(self,a):
+        try:
+            return next(i for i in a if i==i[::-1])
+        except:
+            return ''
+    
 s=Solution()
 
-print(s.arraySign([-1,-2,-3,-4,3,2,1]))
+print(s.firstPalindrome(["def","ghi"]))
