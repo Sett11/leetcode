e08253199400785567cd83529cdf2863d7340ee9
Jhonny <gonzodzen@mail.ru>
2023-10-05 00:38:49 +0300

    Number of Valid Words in a Sentence

--- my.py
+++ my.py
@@ -1,7 +1,26 @@
+from re import sub
+
+def check(s):
+    if any(i.isdigit() or i.isupper() for i in s):
+        return False
+    if any(i in s for i in '.,!') and (s[-1].isalpha() or len(sub(r'[^.,!]','',s))>1):
+        return False
+    if '-' in s:
+        if s.count('-')>1:
+            return False
+        if len(s)<3:
+            return False
+        i=s.index('-')
+        if not i or i==len(s)-1:
+            return False
+        if not (s[i-1].isalpha() and s[i+1].isalpha()):
+            return False
+    return True
+
 class Solution:
-    def mostWordsFound(self,a):
-        return max((len(i.split(' ')) for i in a))
+    def countValidWords(self,a):
+        return len([i for i in a.split(' ') if i and check(i)])
     
 s=Solution()
 
-print(s.mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]))
+print(s.countValidWords("alice and  bob are playing stone-game10"))
