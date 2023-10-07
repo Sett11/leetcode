6eff377cbbfc5b229c1d760546083f93cc1c4ad2
Jhonny <gonzodzen@mail.ru>
2023-10-08 00:33:10 +0300

    Smallest Even Multiple

--- my.py
+++ my.py
@@ -1,22 +1,7 @@
-from bisect import insort_left
-
 class Solution:
-    def mergeSimilarItems(self,a,b):
-        a=a+b
-        i=0
-        r=[]
-        while i<len(a):
-            j=i+1
-            while j<len(a):
-                if a[i][0]==a[j][0]:
-                    a[i][1]+=a[j][1]
-                    a.pop(j)
-                    j-=1
-                j+=1
-            insort_left(r,a[i])
-            i+=1
-        return r
+    def smallestEvenMultiple(self,n):
+        return next(i for i in range(n,10000000) if not i%2 and not i%n)
 
 s=Solution()
 
-print(s.mergeSimilarItems([[1,3],[2,2]],[[7,1],[2,2],[1,4]]))
+print(s.smallestEvenMultiple(5))
