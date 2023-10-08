4d502edaeeb5ecfda4d33229f695b2f5d3c8a5ae
Jhonny <gonzodzen@mail.ru>
2023-10-09 01:01:57 +0300

    Largest Positive Integer That Exists With Its Negative

--- my.py
+++ my.py
@@ -1,10 +1,10 @@
 class Solution:
-    def equalFrequency(self,s):
-        if len(set(s))==1:
-            return True
-        a=sorted([s.count(i) for i in set(s)])
-        return (len(set(a[:-1]))==1 and a[-1]-a[0]==1) or (len(set(a))==1 and a[0]==1) or ((1 in a and a.count(1)==1) and len(set([i for i in a if i!=1]))==1)
+    def findMaxK(self,a):
+        try:
+            return next(i for i in sorted(a,reverse=True) if -i in a)
+        except:
+            return -1
 
 s=Solution()
 
-print(s.equalFrequency('zz'))
+print(s.findMaxK([-1,10,6,7,-7,1]))
