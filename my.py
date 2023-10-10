fddfa1c643785f19ad01abd6b4bccf1a832fa9ad
Jhonny <gonzodzen@mail.ru>
2023-10-11 01:08:50 +0300

    Equal Row and Column Pairs

--- my.py
+++ my.py
@@ -1,13 +1,13 @@
 class Solution:
-    def deleteGreatestValue(self,a):
-        m=0
-        while all(i for i in a):
-            n=0
-            for i in range(len(a)):
-                n=max(n,a[i].pop(a[i].index(max(a[i]))))
-            m+=n
-        return m
+    def equalPairs(self,a):
+        b=[list(i) for i in zip(*a)]
+        c=0
+        for i in a:
+            for j in b:
+                if i==j:
+                    c+=1
+        return c
     
 s=Solution()
 
-print(s.deleteGreatestValue([[1,2,4],[3,3,1]]))
+print(s.equalPairs([[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]))
