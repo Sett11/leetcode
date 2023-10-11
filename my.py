c0ea3c906f5c8cdc556ef982e806cd262eb0a218
Jhonny <gonzodzen@mail.ru>
2023-10-12 00:18:32 +0300

    Find the Array Concatenation Value

--- my.py
+++ my.py
@@ -1,7 +1,14 @@
 class Solution:
-    def countConsistentStrings(self,s,a):
-        return len([i for i in a if all(j in s for j in i)])
+    def findTheArrayConcVal(self,a):
+        l=len(a)//2
+        b,c=a[:l],a[l:][::-1]
+        if len(b)<len(c):
+            b+=[0]
+        if len(c)<len(b):
+            c+=[0]
+        return sum([int(str(b[i])+str(c[i])) for i in range(len(b))])
     
 s=Solution()
 
-print(s.countConsistentStrings("ab",["ad","bd","aaab","baa","badab"]))
+print(s.findTheArrayConcVal([7,52,2,4]))
+print(s.findTheArrayConcVal([5,14,13,8,12]))
