6c5d841e4d9ea8c5bbb97e1a7ecb1ddf5d97ccc5
Jhonny <gonzodzen@mail.ru>
2023-10-14 10:22:34 +0300

    Flatten Deeply Nested Array

--- my.js
+++ my.js
@@ -1,11 +1,3 @@
-const inorderTraversal = function*(a){
-    a=a.flat(100000)
-    for(let i=-1;++i<a.length;)yield a[i]
-}
+const flat=(a,n)=>a.flat(n)
 
-const g=inorderTraversal([[[6]],[1,3],[]])
-
-console.log(g.next().value)
-console.log(g.next().value)
-console.log(g.next().value)
-console.log(g.next().value)
+console.log(flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]],1))
