50da2946375a900f5c10fe9585459fe8e127fb71
Jhonny <gonzodzen@mail.ru>
2023-10-14 10:15:05 +0300

    Nested Array Generator

--- my.js
+++ my.js
@@ -1,3 +1,11 @@
-argumentsLength=(..._)=>_.length
+const inorderTraversal = function*(a){
+    a=a.flat(100000)
+    for(let i=-1;++i<a.length;)yield a[i]
+}
 
-console.log(argumentsLength(1,2,3))
+const g=inorderTraversal([[[6]],[1,3],[]])
+
+console.log(g.next().value)
+console.log(g.next().value)
+console.log(g.next().value)
+console.log(g.next().value)
