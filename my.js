fc99307ec01fc95f3699aa89cd0b95f0a1130471
Jhonny <gonzodzen@mail.ru>
2023-10-15 09:46:24 +0300

    Counter

--- my.js
+++ my.js
@@ -1,3 +1,5 @@
-const flat=(a,n)=>a.flat(n)
-
-console.log(flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]],1))
+const createCounter = function(n) {
+    return function() {
+        return n++
+    }
+}
