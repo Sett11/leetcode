835875526bb0d9f0edc7dd3cc3c242242453ef3a
Jhonny <gonzodzen@mail.ru>
2023-10-16 00:26:34 +0300

    Form Smallest Number From Two Digit Arrays

--- my.py
+++ my.py
@@ -1,7 +1,9 @@
 class Solution:
-    def kItemsWithMaximumSum(self,a,b,c,k):
-        return sum(([1]*a+[0]*b+[-1]*c)[:k])
+    def minNumber(self,a,b):
+        c=set(a).intersection(b)
+        return sorted(c)[0] if c else int(''.join([str(i) for i in sorted([sorted(a)[0]]+[sorted(b)[0]])]))
     
 s=Solution()
 
-print(s.kItemsWithMaximumSum(3,2,0,4))
+print(s.minNumber([4,1,3],[5,7]))
+print(s.minNumber([3,5,2,6],[3,1,7]))
