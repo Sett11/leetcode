56c5b119c11615861b8020288bc0ee4429b8d92a
Jhonny <gonzodzen@mail.ru>
2023-10-16 01:12:59 +0300

    Apply Transform Over Each Element in Array

--- my.js
+++ my.js
@@ -1,3 +1,7 @@
-const reduce = function(nums, fn, init) {
-    return nums.reduce(fn,init)
+const map = function(a,f){
+    r=[]
+    a.forEach((e,i)=>r.push(f(e,i)))
+    return r
 }
+
+console.log(map([1,2,3,4],e=>e+1))
