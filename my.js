2273097129eb89a18339fb9c15c0b134e111bdf7
Jhonny <gonzodzen@mail.ru>
2023-10-16 21:18:21 +0300

    Memoize

--- my.js
+++ my.js
@@ -1,5 +1,34 @@
-const compose=a=>x=>a.reverse().map(e=>x=e(x)).reverse()[0]||x
+function memoize(f) {
+  const o = {};
+  return function (...a) {
+    let c = typeof a === "number" ? a + "" : a.join`,`,
+      r = null;
+    if (c in o) return o[c];
+    r = f(...a);
+    o[c] = r;
+    return o[c];
+  };
+}
 
-f=compose([x=>x+1,x=>x**2,x=>x*2])
+let callCount = 0;
+let c = 0;
+const memoizedFn = memoize(function (a, b) {
+  callCount += 1;
+  return a + b;
+});
 
-console.log(f(4))
+console.log(memoizedFn(2, 3));
+console.log(memoizedFn(2, 3));
+
+function f(x) {
+  c += 1;
+  return x <= 1 ? 1 : f(x - 1) + f(x - 2);
+}
+
+q = memoize(f);
+
+console.log(q(9));
+console.log(q(9));
+console.log(callCount);
+
+console.log(c);
