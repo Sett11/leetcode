aa84dcb82397cca139e8f7ba4634e6c30e5910c9
Jhonny <gonzodzen@mail.ru>
2023-10-16 21:23:52 +0300

    Find the Width of Columns of a Grid

--- my.py
+++ my.py
@@ -1,19 +1,8 @@
-from collections import Counter
-from bisect import insort
-
-
 class Solution:
-    def findWinners(self, a):
-        s = set(sum(a, []))
-        b = Counter([i[1] for i in a])
-        r = []
-        for i in b:
-            if b[i] == 1:
-                insort(r, i)
-        return [sorted(s.difference(b)), r]
+    def findColumnWidth(self,a):
+        return [max([len(str(j)) for j in i]) for i in zip(*a)]
 
 
 s = Solution()
 
-print(s.findWinners([[1, 3], [2, 3], [3, 6], [5, 6], [
-      5, 7], [4, 5], [4, 8], [4, 9], [10, 4], [10, 9]]))
+print(s.findColumnWidth([[-15,1,3],[15,7,12],[5,6,-2]]))
