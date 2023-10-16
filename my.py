493d9c2b7e87499ed2a4f34b3659e294fa43afc9
Jhonny <gonzodzen@mail.ru>
2023-10-16 22:53:24 +0300

    Number of Substrings Containing All Three Characters

--- my.py
+++ my.py
@@ -1,16 +1,18 @@
-from re import sub
-
 class Solution:
-    def checkZeroOnes(self,s):
-        a=[i for i in sub(r'(.)\1*',lambda e:' '+e.group()+' ',s).split(' ') if i]
-        b=c=0
-        for i in a:
-            if i[0]=='1':
-                b=max(len(i),b)
+    def numberOfSubstrings(self, s):
+        a = b = c = -1
+        n = 0
+        for i in range(len(s)):
+            if s[i] == 'a':
+                a = i
+            elif s[i] == 'b':
+                b = i
             else:
-                c=max(len(i),c)
-        return b>c
+                c = i
+            n += min(a, b, c) + 1
+        return n
+
 
 s = Solution()
 
-print(s.checkZeroOnes('11101000'))
+print(s.numberOfSubstrings('aaaabc'))
