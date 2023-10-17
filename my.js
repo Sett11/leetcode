c78f35299b584ceca9883857dbccdd8b701fda74
Jhonny <gonzodzen@mail.ru>
2023-10-17 11:57:48 +0300

    Chunk Array

--- my.js
+++ my.js
@@ -1,14 +1,8 @@
-const once = function(f){
-  let c=1
-	return function(...a){
-        return c--===1?f(...a):undefined
-    }
+const chunk=(a,n)=>{
+  const r=[]
+  for(let i=0;i<a.length;i+=n)r.push(a.slice(i,i+n))
+  return r
 }
 
-
-let fn = (a,b,c) => (a + b + c)
- let onceFn = once(fn)
-
-  console.log(onceFn(1,2,3))
-  console.log(onceFn(2,3,6))
+console.log(chunk([1,2,3,4,5],3))
  
