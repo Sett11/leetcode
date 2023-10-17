804f21dfb6844d7e5b2656023a6acd8216095e85
Jhonny <gonzodzen@mail.ru>
2023-10-17 12:33:59 +0300

    Check if The Number is Fascinating

--- my.py
+++ my.py
@@ -1,18 +1,11 @@
+from collections import Counter as c
+
 class Solution:
-    def circularGameLosers(self,n,k):
-        a=[i for i in range(1,n+1)]
-        r=[1]
-        i=k
-        j=2
-        while True:
-            i+=j*k
-            j+=1
-            c=a[i%n]
-            if c in r:
-                break
-            r.append(c)
-        return list(set(a).difference(r))
+    def isFascinating(self,n):
+        s=str(n)+str(n*2)+str(n*3)
+        r=set(c(s).values())
+        return all(i in s for i in '123456789') and len(r)==1 and 1 in r and '0' not in s
     
 s=Solution()
 
-print(s.circularGameLosers(3,1))
+print(s.isFascinating(192))
