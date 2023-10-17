d65461d86b9fdff93f40158f0838c32042194a7f
Jhonny <gonzodzen@mail.ru>
2023-10-17 23:09:33 +0300

    Longest Even Odd Subarray With Threshold

--- my.py
+++ my.py
@@ -1,16 +1,22 @@
-def gcd(x,y):
-    return  [i for i in range(1,min(x,y)+1) if x%i==0 and y%i==0]==[1]
+def check(a, n):
+    return all(a[i] % 2 != a[i+1] % 2 and a[i] <= n and a[i+1] <= n for i in range(len(a)-1))
+
 
 class Solution:
-    def countBeautifulPairs(self,a):
-        c=0
-        l=len(a)
+    def longestAlternatingSubarray(self, a, n):
+        m = 0
+        l = len(a)
         for i in range(l):
-            for j in range(i+1,l):
-                if gcd(int(str(a[i])[0]),int(str(a[j])[-1])):
-                    c+=1
-        return c
-    
-s=Solution()
+            if a[i] % 2 == 0 and a[i] <= n:
+                for j in range(i+1, l+1):
+                    t = a[i:j]
+                    if check(t, n):
+                        m = max(m, len(t))
+                    else:
+                        break
+        return m
 
-print(s.countBeautifulPairs([31,25,72,79,74]))
+
+s = Solution()
+
+print(s.longestAlternatingSubarray([3, 2, 5, 4], 5))
