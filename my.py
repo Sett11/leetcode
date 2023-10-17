5403f144a32062df775e39018d26a842dddf7a80
Jhonny <gonzodzen@mail.ru>
2023-10-17 23:19:37 +0300

    Maximum Average Subarray I

--- my.py
+++ my.py
@@ -1,22 +1,12 @@
-def check(a, n):
-    return all(a[i] % 2 != a[i+1] % 2 and a[i] <= n and a[i+1] <= n for i in range(len(a)-1))
-
-
 class Solution:
-    def longestAlternatingSubarray(self, a, n):
-        m = 0
-        l = len(a)
-        for i in range(l):
-            if a[i] % 2 == 0 and a[i] <= n:
-                for j in range(i+1, l+1):
-                    t = a[i:j]
-                    if check(t, n):
-                        m = max(m, len(t))
-                    else:
-                        break
-        return m
+    def findMaxAverage(self, a, k):
+        m=c=sum(a[:k])
+        for i in range(k,len(a)):
+            c+=a[i]-a[i-k]
+            m=max(c,m)
+        return m/k
 
 
 s = Solution()
 
-print(s.longestAlternatingSubarray([3, 2, 5, 4], 5))
+print(s.findMaxAverage([1,12,-5,-6,50,3], 4))
