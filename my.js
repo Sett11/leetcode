ef4caa6bff65099379469583c863beace8d3f98e
Jhonny <gonzodzen@mail.ru>
2023-10-18 20:52:40 +0300

    To Be Or Not To Be

--- my.js
+++ my.js
@@ -1,4 +1,14 @@
-isEmpty = (x, y = JSON.stringify(x)) => y === "{}" || y === "[]"
+const expect = v => {
+  return {
+    toBe: function(e){
+        if(e===v)return true
+        throw Error('Not Equal')
+    },
+    notToBe: function(e){
+        if(e!==v)return true
+        throw Error('Equal')
+    },
+  }
+}
 
-console.log(isEmpty({}));
-console.log(isEmpty({ x: 9 }));
+console.log(expect(5).notToBe(5));
