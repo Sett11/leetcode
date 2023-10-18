090873329d775cacee4681ed346dc4ab7d970f9a
Jhonny <gonzodzen@mail.ru>
2023-10-18 22:11:30 +0300

    Semi-Ordered Permutation

--- my.py
+++ my.py
@@ -1,12 +1,22 @@
 class Solution:
-    def buyChoco(self,a,n):
-        q=min(a)
-        a.remove(q)
-        s=n-(min(a)+q)
-        return s if s>=0 else n
-
+    def semiOrderedPermutation(self,a):
+        one=a.index(1)
+        m=max(a)
+        n=a.index(m)
+        c=0
+        while one!=0 or n!=len(a)-1:
+            while one!=0:
+                a[one],a[one-1]=a[one-1],a[one]
+                one=a.index(1)
+                c+=1
+            n=a.index(m)
+            while n!=len(a)-1:
+                a[n],a[n+1]=a[n+1],a[n]
+                n=a.index(m)
+                c+=1
+        return c
 
 s = Solution()
 
-print(s.buyChoco([1,2,2],3))
-print(s.buyChoco([98,54,6,34,66,63,52,39],62))
+print(s.semiOrderedPermutation([2,1,3]))
+print(s.semiOrderedPermutation([2,4,1,3]))
