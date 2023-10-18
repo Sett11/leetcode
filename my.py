eb70bf519f1c0ecc6ac3d0476f656172602f957d
Jhonny <gonzodzen@mail.ru>
2023-10-18 23:02:26 +0300

    Check if Array is Good

--- my.py
+++ my.py
@@ -1,23 +1,18 @@
-def check(a):
-    r=[]
-    for i in range(len(a)-1):
-        r.append(a[i+1]-a[i])
-    return r
+from collections import Counter as c
+
 
 class Solution:
-    def alternatingSubarray(self,a):
-        m=-1
-        l=len(a)
-        for i in range(l):
-            for j in range(i+2,l+1):
-                t=a[i:j]
-                n=len(t)
-                if ([1,-1]*n)[:n-1]==check(t):
-                    m=max(m,n)
-                else:
-                    break
-        return m
+    def isGood(self, a):
+        b = c(a)
+        m = max(a)
+        if len(a)-1 != m:
+            return False
+        for i in range(1, m+1):
+            if (i != m and b[i] != 1) or (i == m and b[i] != 2):
+                return False
+        return True
+
 
 s = Solution()
 
-print(s.alternatingSubarray([2,3,4,3,4]))
+print(s.isGood([1, 2, 3, 3]))
