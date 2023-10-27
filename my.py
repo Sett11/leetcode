206cb8e0b055d994dcd0eec30ef26ebc7ca571e5
Jhonny <gonzodzen@mail.ru>
2023-10-27 20:19:00 +0300

    Single Element in a Sorted Array

--- my.py
+++ my.py
@@ -1,17 +1,12 @@
+from collections import Counter
+
 class Solution:
-    def lengthOfLIS(self,a):
-        s=0
-        n=len(a)
-        r=[0]*(n+1)
-        for i in range(1,n+1):
-            m=0
-            for j in range(i):
-                if a[i-1]>a[j-1] and r[j]>m:
-                    m=r[j]
-            r[i]=m+1
-            s=max(s,r[i])
-        return s
+    def singleNonDuplicate(self,a):
+        a=Counter(a)
+        for i in a:
+            if a[i]==1:
+                return i
 
 s=Solution()
 
-print(s.lengthOfLIS([1,3,6,7,9,4,10,5,6]))
+print(s.singleNonDuplicate([3,3,7,7,10,11,11]))
