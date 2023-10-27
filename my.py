05e7fdb62ae47edb2e2731aae179d593ece37454
Jhonny <gonzodzen@mail.ru>
2023-10-27 12:13:51 +0300

    Longest Common Subsequence

--- my.py
+++ my.py
@@ -1,14 +1,15 @@
-class NumArray:
+class Solution:
+    def longestCommonSubsequence(self,a,b):
+        n,m=len(a),len(b)
+        r=[[0]*(m+1) for i in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if a[i-1]==b[j-1]:
+                    r[i][j]=1+r[i-1][j-1]
+                else:
+                    r[i][j]=max(r[i-1][j],r[i][j-1])
+        return max(sum(r,[]))
 
-    def __init__(self, a):
-        self.a = a
+s=Solution()
 
-    def sumRange(self, l, r):
-        return sum(self.a[l:r+1])
-
-
-s = NumArray([-2, 0, 3, -5, 2, -1])
-
-print(s.sumRange(0, 2))
-print(s.sumRange(2, 5))
-print(s.sumRange(0, 5))
+print(s.longestCommonSubsequence('abcde','ace'))
