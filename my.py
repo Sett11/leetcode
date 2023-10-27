537c299cf476c036276ae7484320d46498c57781
Jhonny <gonzodzen@mail.ru>
2023-10-27 15:59:03 +0300

    Longest Increasing Subsequence

--- my.py
+++ my.py
@@ -1,16 +1,17 @@
 class Solution:
-    def longestPalindromeSubseq(self,a):
-        b=a[::-1]
-        n,m=len(a),len(b)
-        r=[[0]*(m+1) for _ in range(n+1)]
+    def lengthOfLIS(self,a):
+        s=0
+        n=len(a)
+        r=[0]*(n+1)
         for i in range(1,n+1):
-            for j in range(1,m+1):
-                if a[i-1]==b[j-1]:
-                    r[i][j]=1+r[i-1][j-1]
-                else:
-                    r[i][j]=max(r[i-1][j],r[i][j-1])
-        return r[-1][-1]
+            m=0
+            for j in range(i):
+                if a[i-1]>a[j-1] and r[j]>m:
+                    m=r[j]
+            r[i]=m+1
+            s=max(s,r[i])
+        return s
 
 s=Solution()
 
-print(s.longestPalindromeSubseq('abcddce'))
+print(s.lengthOfLIS([1,3,6,7,9,4,10,5,6]))
