93f587600b184ae39494848e7e1685598eff9a51
Jhonny <gonzodzen@mail.ru>
2023-10-27 15:34:56 +0300

    Longest Palindromic Subsequence

--- my.py
+++ my.py
@@ -1,15 +1,16 @@
 class Solution:
-    def longestCommonSubsequence(self,a,b):
+    def longestPalindromeSubseq(self,a):
+        b=a[::-1]
         n,m=len(a),len(b)
-        r=[[0]*(m+1) for i in range(n+1)]
+        r=[[0]*(m+1) for _ in range(n+1)]
         for i in range(1,n+1):
             for j in range(1,m+1):
                 if a[i-1]==b[j-1]:
                     r[i][j]=1+r[i-1][j-1]
                 else:
                     r[i][j]=max(r[i-1][j],r[i][j-1])
-        return max(sum(r,[]))
+        return r[-1][-1]
 
 s=Solution()
 
-print(s.longestCommonSubsequence('abcde','ace'))
+print(s.longestPalindromeSubseq('abcddce'))
