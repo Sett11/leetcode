80288f94830179d69875231a9310d4ef77e84eb8
Jhonny <gonzodzen@mail.ru>
2023-11-01 00:01:01 +0300

    Insertion Sort List

--- my.py
+++ my.py
@@ -3,14 +3,21 @@ class ListNode:
         self.val = val
         self.next = next
 
+def insert_sort(a):
+    for i in range(1,len(a)):
+        k=i
+        while k and a[k]>a[k-1]:
+            a[k],a[k-1]=a[k-1],a[k]
+            k-=1
+
 class Solution:
-    def sortList(self,h):
+    def insertionSortList(self,h):
         r=[]
         c=h
         while c:
             r.append(c.val)
             c=c.next
-        r.sort(reverse=True)
+        insert_sort(r)
         c=h
         while c:
             c.val=r.pop()
