872cead42519f82b93cd9f457001be435cb656e6
Jhonny <gonzodzen@mail.ru>
2023-11-02 00:00:18 +0300

    Double a Number Represented as a Linked List

--- my.py
+++ my.py
@@ -1,3 +1,6 @@
+from sys import set_int_max_str_digits
+set_int_max_str_digits(20000)
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -12,21 +15,17 @@ def list_convert_to_array(h):
     return r
 
 class Solution:
-    def reverseEvenLengthGroups(self,h):
-        a=list_convert_to_array(h)
-        r=[a.pop(0)]
-        i=1
-        while a:
-            t=a[i-1:i+i]
-            r.extend(t if len(t)&1 else t[::-1])
-            del a[0:i]
-            i+=1
-        c=h
-        for i in r:
-            c.val=i
+    def doubleIt(self,h):
+        L=ListNode()
+        c=L
+        n=list(map(int,str(int(''.join(map(str,list_convert_to_array(h))))*2)))
+        l=len(n)
+        for i in range(l):
+            c.val=n[i]
+            c.next=ListNode() if i!=l-1 else None
             c=c.next
-        return h
+        return L
 
 s=Solution()
 
-print(s.reverseEvenLengthGroups(ListNode(7,ListNode(2,ListNode(4,ListNode(3,ListNode(8,ListNode(9,ListNode(10,ListNode(23,ListNode(76,ListNode(98))))))))))))
+print(s.doubleIt(ListNode(7,ListNode(2,ListNode(4,ListNode(3,ListNode(8,ListNode(9))))))))
