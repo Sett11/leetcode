b60d2dfceba17929483d0298cf0a7355ffddf00a
Jhonny <gonzodzen@mail.ru>
2023-11-01 19:44:25 +0300

    Rotate List

--- my.py
+++ my.py
@@ -3,15 +3,27 @@ class ListNode:
         self.val = val
         self.next = next
 
+def list_convert_to_array(h):
+    r=[]
+    c=h
+    while c:
+        r.append(c.val)
+        c=c.next
+    return r
+
 class Solution:
-    def pairSum(self,h):
-        c,r,l=h,[],0
-        while c:
-            r.append(c.val)
+    def rotateRight(self,h,k):
+        r=list_convert_to_array(h)
+        if not r:
+            return h
+        k%=len(r)
+        r=(r[-k:]+r[0:-k])[::-1]
+        c=h
+        while len(r):
+            c.val=r.pop()
             c=c.next
-            l+=1
-        return max([r[i]+r[l-i-1] for i in range(l//2)])
+        return h
 
 s=Solution()
 
-print(s.pairSum(ListNode(3,ListNode(1,ListNode(4,ListNode(11,ListNode(9,ListNode(5,ListNode(15,ListNode(2))))))))))
+print(s.rotateRight(ListNode(1,ListNode(2,ListNode(3,ListNode(4,ListNode(5))))),10**9+2))
