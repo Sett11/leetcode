72abf4d65c8a007d9bdcf18770bb465a4c6ec84f
Jhonny <gonzodzen@mail.ru>
2023-11-02 00:16:10 +0300

    Merge Nodes in Between Zeros

--- my.py
+++ my.py
@@ -1,6 +1,3 @@
-from sys import set_int_max_str_digits
-set_int_max_str_digits(20000)
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -14,18 +11,22 @@ def list_convert_to_array(h):
         c=c.next
     return r
 
-class Solution:
-    def doubleIt(self,h):
-        L=ListNode()
-        c=L
-        n=list(map(int,str(int(''.join(map(str,list_convert_to_array(h))))*2)))
-        l=len(n)
-        for i in range(l):
-            c.val=n[i]
+def array_convert_to_list(a):
+    L=ListNode()
+    c=L
+    l=len(a)
+    for i in range(l):
+            c.val=a[i]
             c.next=ListNode() if i!=l-1 else None
             c=c.next
-        return L
+    return L
+
+class Solution:
+    def mergeNodes(self,h):
+        a=list_convert_to_array(h)
+        ind=[i for i,j in enumerate(a) if not j]
+        return array_convert_to_list([sum(a[ind[i]+1:ind[i+1]]) for i in range(len(ind)-1)])
 
 s=Solution()
 
-print(s.doubleIt(ListNode(7,ListNode(2,ListNode(4,ListNode(3,ListNode(8,ListNode(9))))))))
+print(s.mergeNodes(array_convert_to_list([0,14,13,0,40,0])))
