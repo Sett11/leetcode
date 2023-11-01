34f5fef66323dfe261c130faec8bd6819aa3abfd
Jhonny <gonzodzen@mail.ru>
2023-11-01 22:59:13 +0300

    Swap Nodes in Pairs

--- my.py
+++ my.py
@@ -12,15 +12,20 @@ def list_convert_to_array(h):
     return r
 
 class Solution:
-    def swapNodes(self,h,k):
+    def swapPairs(self,h):
         a=list_convert_to_array(h)
-        a[k-1],a[-k]=a[-k],a[k-1]
+        n=len(a)
+        if n<2:
+            return h
+        r=sum([[a[i+1],a[i]] for i in range(0,n-1,2)],[])
+        if n&1:
+            r.append(a[-1])
         c=h
-        for i in a:
+        for i in r:
             c.val=i
             c=c.next
         return h
 
 s=Solution()
 
-print(s.swapNodes(ListNode(7,ListNode(2,ListNode(4,ListNode(3,ListNode(8,ListNode(9)))))),2))
+print(s.swapPairs(ListNode(7,ListNode(2,ListNode(4,ListNode(3,ListNode(8)))))))
