ba933dd1c2f9a01eb40ba03131b3b08cf56e21e0
Jhonny <gonzodzen@mail.ru>
2023-11-01 20:12:16 +0300

    Reverse Linked List

--- my.py
+++ my.py
@@ -12,16 +12,14 @@ def list_convert_to_array(h):
     return r
 
 class Solution:
-    def reorderList(self,h):
-        r=list_convert_to_array(h)
-        n=len(r)
-        a=sum([[r[i],r[n-i-1]] for i in range(n//2)]+([[r[n//2]]] if n&1 else []),[])
+    def reverseList(self,h):
+        r=list_convert_to_array(h)[::-1]
         c=h
-        for i in a:
+        for i in r:
             c.val=i
             c=c.next
         return h
 
 s=Solution()
 
-print(s.reorderList(ListNode(1,ListNode(2,ListNode(3,ListNode(4,ListNode(5)))))))
+print(s.reverseList(ListNode(1,ListNode(2,ListNode(3,ListNode(4,ListNode(5)))))))
