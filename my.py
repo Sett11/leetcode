420833313eeca373abad74168d83fdd583fec349
Jhonny <gonzodzen@mail.ru>
2023-11-17 13:03:48 +0300

    Maximum Star Sum of a Graph

--- my.py
+++ my.py
@@ -1,7 +1,36 @@
+from collections import defaultdict
+from heapq import heappush,heappop
+
+def convert_two_dimensional_array_to_graph(a):
+    d=defaultdict(list)
+
+    for i,j in a:
+        d[i].append(j)
+        d[j].append(i)
+
+    return d
+
 class Solution:
-    def findCenter(self,a):
-        return list(set(a[0])&set(a[1]))[0]
+    def maxStarSum(self,v,r,k):
+        g=convert_two_dimensional_array_to_graph(r)
+        if not r:
+            return max(v)
+        m=float('-inf')
+
+        for i in g:
+            t=[]
+            for j in g[i]:
+                heappush(t,v[j])
+                while len(t)>k:
+                    heappop(t)
+            p=v[i]
+            for x in range(len(t)):
+                p=max(p,p+t[x])
+                m=max(m,p)
+
+        return max(m,max(v))
     
 S=Solution()
 
-print(S.findCenter([[1,2],[2,3],[4,2]]))
+print(S.maxStarSum([1,2,3,4,10,-10,-20],[[0,1],[1,2],[1,3],[3,4],[ 3,5],[3,6]],2))
+print(S.maxStarSum([-2,-1,-2],[[0,2]],1))
