f5130ca4e4caf79116195bca3e73e7da5411396a
Jhonny <gonzodzen@mail.ru>
2023-12-03 11:17:48 +0300

    Linked List Cycle

--- my.py
+++ my.py
@@ -1,22 +1,15 @@
 class Solution:
-    def longestPalindrome(self,s):
-        T='#'.join('^{}$'.format(s))
-        n=len(T)
-        P=[0]*n
-        C=R=0
+    def hasCycle(self,h):
+        s=set()
 
-        for i in range(1,n-1):
-            P[i]=(R>i) and min(R-i,P[2*C-i])
-            while T[i+1+P[i]]==T[i-1-P[i]]:
-                P[i]+=1
-            if i+P[i]>R:
-                C,R=i,i+P[i]
+        while h:
+            if h.next in s:
+                return True
+            s.add(h)
+            h=h.next
         
-        m,c=max((n,i) for i,n in enumerate(P))
-
-        return s[(c-m)//2:(c+m)//2]
-
+        return False
 
 S=Solution()
 
-print(S.longestPalindrome('ababbab'))
+print(S.hasCycle())
