3ef71e273cd5206ffc337d9a89ce9e39c6d5c10f
Jhonny <gonzodzen@mail.ru>
2023-12-04 22:18:09 +0300

    Generate Parentheses

--- my.py
+++ my.py
@@ -1,17 +1,24 @@
-from re import sub
-
-def f(s,n):
-    k=str(round(float(s[1:])/100*(100-n),2))
-    return ' $'+k+('0' if len(k.split('.')[1])<2 else '')+' '
-
 class Solution:
-    def discountPrices(self,s,n):
-        s=' '+s+' '
-        s=sub(r'\s\$\d+\s',lambda x:' '+x.group()+' ',s)
-        return sub(r'\s\$\d+\s',lambda x:f(x.group().strip(),n),s).strip().replace('  ',' ')
+    def generateParenthesis(self,n):
+        r=[]
 
+        def f(s,n,m):
+            if [m,n]==[0,0]:
+                r.append(s)
+                return
+            if not n:
+                r.append(s+')'*m)
+                return
+            if n<m:
+                f(s+')',n,m-1)
+            f(s+'(',n-1,m)
+        
+        f('',n,n)
+
+        return r
+
+
+    
 S=Solution()
 
-print(S.discountPrices("1 2 $3 4 $5 $6 7 8$ $9 $10$",100))
-print(S.discountPrices("ka3caz4837h6ada4 r1 $602",9))
-print(S.discountPrices("there are $1 $2 and 5$ candies in the shop",50))
+print(S.generateParenthesis(8))
