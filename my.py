a3bc28e535f9ea00054c6fb72f6b1bffcd0dc5b2
Jhonny <gonzodzen@mail.ru>
2023-12-04 21:49:22 +0300

    Apply Discount to Prices

--- my.py
+++ my.py
@@ -1,8 +1,17 @@
+from re import sub
+
+def f(s,n):
+    k=str(round(float(s[1:])/100*(100-n),2))
+    return ' $'+k+('0' if len(k.split('.')[1])<2 else '')+' '
+
 class Solution:
-    def getMinDistance(self,a,k,n):
-        return min([abs(i-n) for i,j in enumerate(a) if j==k])
-    
+    def discountPrices(self,s,n):
+        s=' '+s+' '
+        s=sub(r'\s\$\d+\s',lambda x:' '+x.group()+' ',s)
+        return sub(r'\s\$\d+\s',lambda x:f(x.group().strip(),n),s).strip().replace('  ',' ')
 
 S=Solution()
 
-print(S.getMinDistance([1,2,3,4,5],5,3))
+print(S.discountPrices("1 2 $3 4 $5 $6 7 8$ $9 $10$",100))
+print(S.discountPrices("ka3caz4837h6ada4 r1 $602",9))
+print(S.discountPrices("there are $1 $2 and 5$ candies in the shop",50))
