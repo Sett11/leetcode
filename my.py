28edbc5bceee5a98bb3a10e22779b2e52f0368fc
Jhonny <gonzodzen@mail.ru>
2023-12-05 20:39:55 +0300

    Longest Arithmetic Subsequence

--- my.py
+++ my.py
@@ -1,21 +1,22 @@
-from math import gcd
-
-def lcm(a,b):
-    return a*b/gcd(a,b)
+from collections import defaultdict
 
 class Solution:
-    def nthMagicalNumber(self,n,a,b):
-        k,l,r=lcm(a,b),2,10**14
+    def longestArithSeqLength(self,a):
+        o=defaultdict(dict)
+        c=1
 
-        while l<r:
-            m=(l+r)//2
-            if m//a+m//b-m//k<n:
-                l=m+1
-            else:
-                r=m
-        
-        return l%(10**9+7)
+        for i in range(len(a)):
+            for j in range(i):
+                n=a[i]-a[j]
+                if n not in o[j]:
+                    o[j][n]=1
+                if n not in o[i]:
+                    o[i][n]=0
+                o[i][n]=o[j][n]+1
+                c=max(c,o[i][n])
+
+        return c
     
 S=Solution()
 
-print(S.nthMagicalNumber(4,2,3))
+print(S.longestArithSeqLength([20,1,15,3,10,5,8]))
