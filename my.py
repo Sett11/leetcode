559a45948460dc0b987377ff49079a3f3c13dc3a
Jhonny <gonzodzen@mail.ru>
2023-12-05 16:22:55 +0300

    Rotting Oranges

--- my.py
+++ my.py
@@ -1,16 +1,32 @@
 class Solution:
-    def kthGrammar(self,n,k):
-        v=True
-        n=1<<(n-1)
+    def orangesRotting(self,a):
+        n,m=len(a),len(a[0])
+        v,c=n*m,0
 
-        while n!=1:
-            n//=2
-            if k>n:
-                k-=n
-                v=not v
+        while v:
+            r=[]
+            w=False
+            for i in range(n):
+                for j in range(m):
+                    if a[i][j]==1:
+                        w=True
+                    if a[i][j]==2:
+                        p=[[k,h] for k,h in [[i+1,j],[i,j+1],[i-1,j],[i,j-1]] if -1<k<n and -1<h<m and a[k][h]==1]
+                        r.extend(p)
+            
+            for x,y in r:
+                a[x][y]=2
+
+            if not w:
+                return c
+            
+            c+=1
+            v-=1
         
-        return 0 if v else 1
+        return -1
     
+
 S=Solution()
 
-print(S.kthGrammar(2,2))
+print(S.orangesRotting([[2,1,1],[1,1,0],[0,1,1]]))
+print(S.orangesRotting([[2,1,1],[0,1,1],[1,0,1]]))
