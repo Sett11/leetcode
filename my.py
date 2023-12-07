ed579443da688f66260713a864612ab36122998a
Jhonny <gonzodzen@mail.ru>
2023-12-08 00:00:10 +0300

    Largest Odd Number in String

--- my.py
+++ my.py
@@ -1,29 +1,6 @@
-from itertools import permutations as p
-
 class Solution:
-    def largestTimeFromDigits(self,a):
-        a=list(map(lambda x:''.join(map(str,x)),p(a)))
-        m=24
-        o={}
-
-        for i in a:
-            n=float(i[:2]+'.'+i[2:])
-            t=24-n
-            if t>0 and int(i[2:])<60:
-                m=min(m,t)
-                o[t]=str(n)
-        
-        r=(o[m][:2]+':'+o[m][2:]).replace('.','') if m in o else ''
-        c=r.split(':')
-
-        if r and len(c[0])<2:
-            r='0'+r
-        
-        if r and len(c[1])<2:
-            r+='0'
-        
-        return r
-    
-S=Solution()
-
-print(S.largestTimeFromDigits([2,0,6,6]))
+    def largestOddNumber(self,s):
+        for i in range(len(s)-1,-1,-1):
+          if s[i] in '13579':
+            return s[:i+1]
+        return ''
