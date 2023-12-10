0dfaade2d5a5f5aaeca0303bbdce855c4f99636f
Jhonny <gonzodzen@mail.ru>
2023-12-10 17:32:57 +0300

    Prime Pairs With Target Sum

--- my.py
+++ my.py
@@ -1,18 +1,44 @@
+from math import ceil
+
+def er():
+    n=10**6
+    r=[True]*n
+    r[0]=r[1]=False
+    for i in range(n):
+        if r[i]:
+            r[2*i:n:i]=[False]*ceil((n/i)-2)
+    return [i for i,j in enumerate(r) if j]
+
+def right(a,n):
+    l,r=0,len(a)
+    while r-l>1:
+        m=(r+l)//2
+        if a[m]<=n:
+            l=m
+        else:
+            r=m
+    return r
+
+
+primes=er()
+hash=set(primes)
+
+
 class Solution:
-    def sequentialDigits(self,a,b):
-        n,m,t,r=len(str(a)),len(str(b))+1,'123456789',[]
-
-        while n<m:
-            for i in range(10-n):
-                x=int(t[i:i+n])
-                if a<=x<=b:
-                    r.append(x)
-            n+=1
-
+    def findPrimePairs(self,n):
+        a=primes[:right(primes,n)]
+        r=[]
+        for i in a:
+            t=n-i
+            if i>t:
+                break
+            if t in hash:
+                r.append([i,t])
         return r
+
 
     
 
 S=Solution()
 
-print(S.sequentialDigits(1000,10**9))
+print(S.findPrimePairs(10000))
