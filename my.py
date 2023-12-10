bb0800409941ce27c32b48e70e086123be8a44e2
Jhonny <gonzodzen@mail.ru>
2023-12-11 01:27:59 +0300

    The kth Factor of n

--- my.py
+++ my.py
@@ -1,12 +1,18 @@
 class Solution:
-    def replaceElements(self,a):
-        m=-1
-        for i in range(len(a)-1,-1,-1):
-            a[i],m=m,max(a[i],m)
-        return a
+    def kthFactor(self,n,k):
+        r=[1]
+        for i in range(2,n//2):
+            if n%i==0:
+                r.append(i)
+                if len(r)==k:
+                    return r[-1]
+        if n%2==0 and n//2 not in r:
+            r.append(n//2)
+        r.append(n)
+        return r[k-1] if k<=len(r) else -1
 
 
     
 S=Solution()
     
-print(S.replaceElements([17,18,5,4,6,1]))
+print(S.kthFactor(2,2))
