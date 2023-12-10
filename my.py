adedf789ba0be327342c9fbd6b90929f5eba3f46
Jhonny <gonzodzen@mail.ru>
2023-12-10 20:02:27 +0300

    Element Appearing More Than 25% In Sorted Array

--- my.py
+++ my.py
@@ -1,44 +1,12 @@
-from math import ceil
-
-def er():
-    n=10**6
-    r=[True]*n
-    r[0]=r[1]=False
-    for i in range(n):
-        if r[i]:
-            r[2*i:n:i]=[False]*ceil((n/i)-2)
-    return [i for i,j in enumerate(r) if j]
-
-def right(a,n):
-    l,r=0,len(a)
-    while r-l>1:
-        m=(r+l)//2
-        if a[m]<=n:
-            l=m
-        else:
-            r=m
-    return r
-
-
-primes=er()
-hash=set(primes)
-
+from collections import Counter
 
 class Solution:
-    def findPrimePairs(self,n):
-        a=primes[:right(primes,n)]
-        r=[]
-        for i in a:
-            t=n-i
-            if i>t:
-                break
-            if t in hash:
-                r.append([i,t])
-        return r
-
-
+    def findSpecialInteger(self,a):
+        n,c,s=len(a)//4,Counter(a),set(a)
+        for i in s:
+            if c[i]>n:
+                return i
     
-
 S=Solution()
 
-print(S.findPrimePairs(10000))
+print(S.findSpecialInteger([1,2,2,6,6,6,6,7,10]))
