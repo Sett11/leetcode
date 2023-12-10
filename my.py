90b2e8c2e0f76e5e629626165044d33ff098c8d7
Jhonny <gonzodzen@mail.ru>
2023-12-10 14:53:51 +0300

    Subtract the Product and Sum of Digits of an Integer

--- my.py
+++ my.py
@@ -1,46 +1,12 @@
-from math import ceil
-
-def e(n):
-    n+=1
-    r=[True]*n
-    r[0]=r[1]=False
-    for i in range(2,int(n**.5+1)):
-        if r[i]:
-            r[2*i:n:i]=[False]*ceil((n/i)-2)
-    return [i for i,j in enumerate(r) if j]
-
-p=e(10**6+100)
-
-
-def left(n):
-    l,r=0,len(p)
-    while r-l>1:
-        m=(r+l)//2
-        if p[m]<n:
-            l=m
-        else:
-            r=m
-    return l
+from functools import reduce
+from operator import mul
 
 class Solution:
-    def closestPrimes(self,l,r):
-        a=p[left(l):]
-        if a[0]<l:
-            a=a[1:]
-        m=10**6
-        i=0
-        o={}
-        while a[i+1]<=r:
-            s=a[i+1]-a[i]
-            if s in [1,2]:
-                return [a[i],a[i+1]]
-            m=min(s,m)
-            if s not in o:
-                o[s]=[a[i],a[i+1]]
-            i+=1
-        return o[m] if o else [-1,-1]
+    def subtractProductAndSum(self,n):
+        a=list(map(int,str(n)))
+        return reduce(mul,a)-sum(a)
+    
 
 S=Solution()
 
-print(S.closestPrimes(1,10))
-print(S.closestPrimes(19,31))
+print(S.subtractProductAndSum(4421))
