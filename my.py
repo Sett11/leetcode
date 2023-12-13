6b86fe618bfef37c347ef231addc1ac27ce903cc
Jhonny <gonzodzen@mail.ru>
2023-12-13 15:40:38 +0300

    Sort Colors

--- my.py
+++ my.py
@@ -1,16 +1,6 @@
-from random import choice
-
 class Solution:
     def sortColors(self,a):
-        def hs(a):
-            if len(a)<2:
-                return a
-            l,m,r,n=[],[],[],choice(a)
-            {l.append(i) if i<n else r.append(i) if i>n else m.append(i) for i in a}
-            return hs(l)+m+hs(r)
-        b=hs(a)
-        for i in range(len(b)):
-            a[i]=b[i]
+        a.sort()
     
 S=Solution()
 
