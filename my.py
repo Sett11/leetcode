fa6e140d555f943efbe8b48d8842568f5e6c09a4
Jhonny <gonzodzen@mail.ru>
2023-12-14 19:40:14 +0300

    Reconstruct Original Digits from English

--- my.py
+++ my.py
@@ -1,7 +1,20 @@
+from collections import Counter
+from bisect import insort
+
 class Solution:
-    def superPow(self,a,b):
-        return pow(a,int(''.join(map(str,b))),1337)
+    def originalDigits(self,s):
+        o=Counter(s)
+        d={'w': ('two', '2'), 'u': ('four', '4'), 'x': ('six', '6'), 'f': ('five', '5'), 'z': ('zero', '0'), 'r': ('three', '3'), 't': ('eight', '8'), 's': ('seven', '7'), 'i': ('nine', '9'), 'n': ('one', '1')}
+        r=[]
+        for i,(j,k) in d.items():
+            c=o[i]
+            insort(r,k*c)
+            for h in j:
+                o[h]-=c
+        return ''.join(r)
+
+
     
 S=Solution()
 
-print(S.superPow(2,[1,0]))
+print(S.originalDigits("owoztneoer"))
