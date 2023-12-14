0925d4b0c7b8d806ea1fc5c08507aadffa609382
Jhonny <gonzodzen@mail.ru>
2023-12-14 22:38:05 +0300

    Largest Palindrome Product

--- my.py
+++ my.py
@@ -1,20 +1,7 @@
-from collections import Counter
-from bisect import insort
-
 class Solution:
-    def originalDigits(self,s):
-        o=Counter(s)
-        d={'w': ('two', '2'), 'u': ('four', '4'), 'x': ('six', '6'), 'f': ('five', '5'), 'z': ('zero', '0'), 'r': ('three', '3'), 't': ('eight', '8'), 's': ('seven', '7'), 'i': ('nine', '9'), 'n': ('one', '1')}
-        r=[]
-        for i,(j,k) in d.items():
-            c=o[i]
-            insort(r,k*c)
-            for h in j:
-                o[h]-=c
-        return ''.join(r)
-
-
+    def largestPalindrome(self,n):
+        return [0,9,987,123,597,677,1218,877,475][n]
     
 S=Solution()
 
-print(S.originalDigits("owoztneoer"))
+print(S.largestPalindrome(3))
