418e9b51f6288911921fe0e221c6dc793b4c16a5
Jhonny <gonzodzen@mail.ru>
2023-12-14 12:12:25 +0300

    Maximum Consecutive Floors Without Special Floors

--- my.py
+++ my.py
@@ -1,10 +1,13 @@
 class Solution:
-    def sumOfThree(self,n):
-        m=n//3
-        s=m-1+m+m+1
-        return [m-1,m,m+1] if s==n else []
+    def maxConsecutive(self,l,r,b):
+        s=set(b)
+        a=[l]+sorted(b)+[r]
+        m=0
+        for i in range(1,len(a)):
+            m=max(a[i]-a[i-1]-(1 if a[i] in s and a[i-1] in s else 0),m)
+        return m if m>1 else 0
     
 S=Solution()
 
-print(S.sumOfThree(33))
-print(S.sumOfThree(43))
+print(S.maxConsecutive(2,9,[4,6]))
+print(S.maxConsecutive(28,50,[35,48]))
