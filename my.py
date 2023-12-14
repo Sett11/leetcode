d505dd7522b65af1e44c2b2eecfd045b5e25b4f9
Jhonny <gonzodzen@mail.ru>
2023-12-14 14:54:06 +0300

    Sum of Two Integers

--- my.py
+++ my.py
@@ -1,8 +1,7 @@
 class Solution:
-    def countNumbersWithUniqueDigits(self,n):
-        o={6:168571,7:712891,8:2345851}
-        return o[n] if n in o else len([i for i in range(0,10**n) if len(str(i))==len(set(str(i)))])
+    def getSum(self,a,b):
+        return sum([a,b])
     
 S=Solution()
 
-print(S.countNumbersWithUniqueDigits(8))
+print(S.getSum(1,2))
