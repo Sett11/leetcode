3671dc6a21ba3339dd673e59c54a89e312280fd4
Jhonny <gonzodzen@mail.ru>
2023-12-14 11:21:21 +0300

    Longest Consecutive Sequence

--- my.py
+++ my.py
@@ -1,12 +1,19 @@
 class Solution:
-    def minDistance(self,a,b):
-        n,m=len(a),len(b)
-        r=[[i+j if i*j==0 else 0 for j in range(m+1)] for i in range(n+1)]
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                r[i][j]=r[i-1][j-1] if a[i-1]==b[j-1] else 1+min(r[i-1][j],r[i][j-1],r[i-1][j-1])
-        return r[-1][-1]
+    def longestConsecutive(self,b):
+        a=['&']+sorted(set(b))+['&']
+        m=1
+        r=m
+        for i in range(2,len(a)-1):
+            if a[i]-a[i-1]==1:
+                m+=1
+                r=max(r,m)
+            else:
+                m=1
+        return r if b else 0
+
     
 S=Solution()
 
-print(S.minDistance("horse","ros"))
+print(S.longestConsecutive([100,4,200,1,3,2]))
+print(S.longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
+print(S.longestConsecutive([4,0,-4,-2,2,5,2,0,-8,-8,-8,-8,-1,7,4,5,5,-4,6,6,- 3]))
