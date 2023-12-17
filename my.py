5cc295255e7e142759d6181ec861e106df661066
Jhonny <gonzodzen@mail.ru>
2023-12-17 15:45:03 +0300

    Number of Steps to Reduce a Number to Zero

--- my.py
+++ my.py
@@ -1,69 +1,14 @@
-def f(a,x,y):
-    i,j=x,y
-    r=[]
-    while j>=0:
-        if a[i][j]==1:
-            r.append([i,j])
-            break
-        j-=1
-    i,j=x,y
-    while j<8:
-        if a[i][j]==1:
-            r.append([i,j])
-            break
-        j+=1
-    i,j=x,y
-    while i>=0:
-        if a[i][j]==1:
-            r.append([i,j])
-            break
-        i-=1
-    i,j=x,y
-    while i<8:
-        if a[i][j]==1:
-            r.append([i,j])
-            break
-        i+=1
-    i,j=x,y
-    while i<8 and j>=0:
-        if a[i][j]==1:
-            r.append([i,j])
-            break
-        i+=1
-        j-=1
-    i,j=x,y
-    while i>=0 and j>=0:
-        if a[i][j]==1:
-            r.append([i,j])
-            break
-        i-=1
-        j-=1
-    i,j=x,y
-    while i<8 and j<8:
-        if a[i][j]==1:
-            r.append([i,j])
-            break
-        i+=1
-        j+=1
-    i,j=x,y
-    while i>=0 and j<8:
-        if a[i][j]==1:
-            r.append([i,j])
-            break
-        i-=1
-        j+=1
-    return r
-
 class Solution:
-    def queensAttacktheKing(self,a,b):
-        c=[[0]*8 for _ in range(8)]
-        c[b[0]][b[1]]=2
-        for i in range(8):
-            for j in range(8):
-                if [i,j] in a:
-                    c[i][j]=1
-        return f(c,b[0],b[1])
-    
+    def numberOfSteps(self,n):
+        c=0
+        while n:
+            if n&1:
+                n-=1
+            else:
+                n//=2
+            c+=1
+        return c
+
 S=Solution()
 
-print(S.queensAttacktheKing([[0,0],[1,1],[2,2],[3,4],[3,5],[ 4,4],[4,5]],[3,3]))
+print(S.numberOfSteps(14))
