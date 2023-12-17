4fc67eb658a66570dae4c8294ff5e1868b92f1a5
Jhonny <gonzodzen@mail.ru>
2023-12-17 17:13:39 +0300

    Abbreviating the Product of a Range

--- my.py
+++ my.py
@@ -1,14 +1,17 @@
+from functools import reduce
+from operator import mul
+from re import sub
+from sys import set_int_max_str_digits
+set_int_max_str_digits(100000)
+
 class Solution:
-    def countOperations(self,a,b):
-        c=0
-        while a and b:
-            if a>=b:
-                a-=b
-            else:
-                b-=a
-            c+=1
-        return c
+    def abbreviateProduct(self,l,r):
+        s=str(reduce(mul,range(l,r+1)))
+        a=sub(r'0+$','',s)
+        l=str(len(s)-len(a))
+        k=f'e{l}'
+        return a+k if len(a)<11 else a[0:5]+'...'+a[-5:]+k
 
 S=Solution()
 
-print(S.countOperations(2,3))
+print(S.abbreviateProduct(44,9556))
