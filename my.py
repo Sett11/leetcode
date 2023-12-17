64a8cd7545496a152dcccdb29395e5c1a02af9c4
Jhonny <gonzodzen@mail.ru>
2023-12-17 19:01:28 +0300

    Sort Integers by The Power Value

--- my.py
+++ my.py
@@ -1,17 +1,14 @@
-from functools import reduce
-from operator import mul
-from re import sub
-from sys import set_int_max_str_digits
-set_int_max_str_digits(100000)
+def f(n):
+    c=0
+    while n>1:
+        n=3*n+1 if n&1 else n//2
+        c+=1
+    return c
 
 class Solution:
-    def abbreviateProduct(self,l,r):
-        s=str(reduce(mul,range(l,r+1)))
-        a=sub(r'0+$','',s)
-        l=str(len(s)-len(a))
-        k=f'e{l}'
-        return a+k if len(a)<11 else a[0:5]+'...'+a[-5:]+k
+    def getKth(self,l,r,n):
+        return sorted([[f(i),i] for i in range(l,r+1)],key=lambda x:(x[0],x[1]))[n-1][1]
 
 S=Solution()
 
-print(S.abbreviateProduct(44,9556))
+print(S.getKth(12,15,2))
