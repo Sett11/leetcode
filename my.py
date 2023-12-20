2a5840d1704ffd72afc96ec1e7a675ef0bc4ed7f
Jhonny <gonzodzen@mail.ru>
2023-12-20 13:14:54 +0300

    Cells with Odd Values in a Matrix

--- my.py
+++ my.py
@@ -1,32 +1,18 @@
 class Solution:
-    def generateMatrix(self,n):
-        a=list(range(1,n**2+1))
-        r,a,k=[a[:n]],a[n:],n-1
-        while a:
-            v=2
-            while v:
-                r.append(a[:k])
-                a=a[k:]
-                v-=1
-            k-=1
-        q,w,i=['l','d','r','u'],[r.pop()],0
-        while r:
-            v=r.pop()
-            if q[i%4]=='l':
-                for j in range(len(w)-1,-1,-1):
-                    w[j].insert(0,v.pop(0))
-            elif q[i%4]=='d':
-                w.append(v[::-1])
-            elif q[i%4]=='r':
-                for j in range(len(w)-1,-1,-1):
-                    w[j].append(v.pop())
-            else:
-                w.insert(0,v)
-            i+=1
-        if n%2==0:
-            w=[p[::-1] for p in w][::-1]
-        return w
+    def oddCells(self,n,m,a):
+        r=[[0]*m for _ in range(n)]
+        def row(a,k):
+            for i in range(len(a[k])):
+                a[k][i]+=1
+        def column(a,k):
+            for i in range(len(a)):
+                a[i][k]+=1
+        for i,j in a:
+            row(r,i)
+            column(r,j)
+        return len([i for i in sum(r,[]) if i&1])
+    
 
 S=Solution()
 
-print(S.generateMatrix(5))
+print(S.oddCells(2,3,[[0,1],[1,1]]))
