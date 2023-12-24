24eff00be697bc6387149dd8b9f0913d47afade1
Jhonny <gonzodzen@mail.ru>
2023-12-24 12:46:53 +0300

    Number of Steps to Reduce a Number in Binary Representation to One

--- my.py
+++ my.py
@@ -1,18 +1,12 @@
-from collections import defaultdict
-
 class Solution:
-    def countLargestGroup(self,n):
-        d,m=defaultdict(dict),0
-        for i in range(1,n+1):
-            t=sum(map(int,str(i)))
-            if t not in d:
-                d[t]=1
-            else:
-                d[t]+=1
-            m=max(d[t],m)
-        return len(list(filter(lambda x:x[1]==m,d.items())))
+    def numSteps(self,s):
+        n,c=int(s,2),0
+        while n!=1:
+            n=n+1 if n&1 else n//2
+            c+=1
+        return c
 
     
 S=Solution()
 
-print(S.countLargestGroup(13))
+print(S.numSteps('1101'))
