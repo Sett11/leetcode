e9680ae3a69968aea8a5867e13a8dba8e8a63d33
Jhonny <gonzodzen@mail.ru>
2023-12-24 14:44:17 +0300

    The k Strongest Values in an Array

--- my.py
+++ my.py
@@ -1,12 +1,9 @@
 class Solution:
-    def numSteps(self,s):
-        n,c=int(s,2),0
-        while n!=1:
-            n=n+1 if n&1 else n//2
-            c+=1
-        return c
-
+    def getStrongest(self,a,k):
+        n=sorted(a)[(len(a)-1)//2]
+        return sorted(a,key=lambda x:(abs(x-n),x),reverse=True)[:k]
     
 S=Solution()
 
-print(S.numSteps('1101'))
+print(S.getStrongest([1,2,3,4,5],2))
+print(S.getStrongest([-7,22,17,3],2))
